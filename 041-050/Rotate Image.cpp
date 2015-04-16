efc96d7f4e376c90e42fc18298d94e0e37e51695
Louis1992 <303542123@qq.com>
2015-04-16 18:14:54 +0800

    48

---
+++ 041-050/Rotate Image.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int> > &matrix) {
+        int n = matrix[0].size() -1;
+        for(int i = 0;i <= n - i;i++){
+            for(int j = i;j <= n - i - 1;j++)
+            {
+                int tmp = matrix[j][n-i];
+                matrix[j][n-i] = matrix[i][j];
+                matrix[i][j] = matrix[n-j][i];
+                matrix[n-j][i] = matrix[n-i][n-j];
+                matrix[n-i][n-j] = tmp;
+            }
+        }
+    }
+    
+};
+
+int main()
+{
+    Solution s;
+    
+    return 0;
+}
