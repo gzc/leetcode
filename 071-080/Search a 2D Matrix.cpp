38fb1e86056fdb5c497631ec25c7179025933f14
Zhenchao Gan <303542123@qq.com>
2015-04-30 13:01:07 +0800

    Create Search a 2D Matrix.cpp

---
+++ 071-080/Search a 2D Matrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int> > &matrix, int target) {
+        
+        int row = matrix.size();
+        int col = matrix[0].size();
+        
+        int i(0),j(row*col-1);
+        
+        while(i <= j)
+        {
+            int mid = (i+j) >> 1;
+            int v = matrix[mid/col][mid%col];
+            if(target < v) j = mid  - 1;
+            else if(target > v) i = mid + 1;
+            else if(target == v) return true;
+        }
+        
+        return false;
+        
+    }
+};
