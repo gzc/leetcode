7106057a36be878a358cf278b113c62c0f9c2728
Zhenchao Gan <303542123@qq.com>
2015-05-13 13:05:25 +0800

    89

---
+++ 081-090/Gray Code.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        
+        vector<int> v;
+        v.push_back(0);
+        
+        for(int i = 0;i < n;i++)
+        {
+            int len = v.size();
+            for(int j = len-1;j >= 0;j--)
+                v.push_back(v[j] | (1 << i) );
+        }
+        
+        return v;
+    }
+};
