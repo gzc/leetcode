0bdf971aa3f82259f2f0d20e677ace5ed62d82fc
Zhenchao Gan <303542123@qq.com>
2015-05-14 19:15:06 +0800

    104

---
+++ 101-110/Course Schedule II.cpp
@@ -0,0 +1,61 @@
+struct Node {
+    int state;
+    vector<int> edges;
+    Node(int x) : state(x){}
+};
+
+class Solution {
+    
+    vector<struct Node>  Nodes;
+    
+    bool DFS_VISIT(int u, vector<int>& v)
+    {
+        if(Nodes[u].state == 2) return true;
+        Nodes[u].state = 1;
+        for(int i = 0;i < Nodes[u].edges.size();i++)
+        {
+            int num = Nodes[u].edges[i];
+            if(Nodes[num].state == 0)
+            {
+                bool fff = DFS_VISIT(num, v);
+                if(!fff) return false;
+            } else if(Nodes[num].state == 1)
+            {
+                return false;
+            }
+        }
+        Nodes[u].state = 2;
+        v.push_back(u);
+        return true;
+    }
+    
+public:
+    vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
+        
+        vector<int>v;
+        vector<int>empty;
+        
+        for(int i = 0;i < numCourses;i++)
+            Nodes.push_back(Node(0));
+        
+        for(int i = 0;i < prerequisites.size();i++)
+            Nodes[prerequisites[i].first].edges.push_back(prerequisites[i].second);
+        
+        for(int i = 0;i < numCourses;i++)
+        {
+            vector<int> edges = Nodes[i].edges;
+            if(Nodes[i].state != 0) continue;
+            if(edges.empty()) {
+                v.push_back(i);
+                Nodes[i].state = 2;
+                continue;
+            }
+            bool fff = DFS_VISIT(i, v);
+            if(!fff) return empty;
+        }
+        
+        return v;
+    }
+    
+    
+};
