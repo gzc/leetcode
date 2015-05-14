c81e397030ff5d5152317e0a3b36fa3b6d0294ed
Zhenchao Gan <303542123@qq.com>
2015-05-14 19:35:44 +0800

    108

---
+++ 101-110/Convert Sorted Array to Binary Search Tree.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    
+    TreeNode *help(vector<int>::iterator it1, vector<int>::iterator it2)
+    {
+        if(it1 == it2)
+            return new TreeNode(*it1);
+        else if(it1 > it2)
+            return NULL;
+        
+        vector<int>::iterator midit = it1+(it2-it1)/2;
+        TreeNode *node = new TreeNode(*midit);
+        node->left = help(it1,midit-1);
+        node->right = help(midit+1, it2);
+        return node;
+    }
+    
+public:
+    TreeNode *sortedArrayToBST(vector<int> &num) {
+        
+        if(num.size() == 0)
+            return NULL;
+        
+        return help(num.begin(), num.end()-1);
+        
+    }
+};
