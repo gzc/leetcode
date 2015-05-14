22ad19a4b40620fede1293266cf3a79709694f55
Zhenchao Gan <303542123@qq.com>
2015-05-15 07:12:07 +0800

    112

---
+++ 111-120/Path Sum.cpp
@@ -0,0 +1,9 @@
+class Solution {
+    
+public:
+    bool hasPathSum(TreeNode *root, int sum) {
+        if(!root) return false;
+        if(!root->left && !root->right && sum == root->val) return true;
+        return hasPathSum(root->left,sum - root->val)||hasPathSum(root->right,sum - root->val);
+    }
+};
