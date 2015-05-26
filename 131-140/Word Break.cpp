6354015b514e61bbf4e755b173188df505ff09ce
Zhenchao Gan <303542123@qq.com>
2015-05-26 12:35:16 +0800

    139

---
+++ 131-140/Word Break.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool wordBreak(string s, unordered_set<string> &dict) {
+        
+        vector<bool> p(s.length()+1, false);
+        p[0]=true;
+        
+        for(int i = 0;i < s.length();i++)
+        {
+            for(int j = 0;j <= i;j++)
+            {
+                if(p[j] == true)
+                {
+                    string tmp = s.substr(j,i+1-j);
+                    if ( dict.find (tmp) != dict.end() )
+                    {
+                        p[i+1] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return p[s.length()];
+        
+    }
+    
+};
