5a08c2654ef483bcbdba3aad09432fa436342719
Zhenchao Gan <303542123@qq.com>
2015-06-06 01:24:15 +0800

    166

---
+++ 161-170/Fraction to Recurring Decimal.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    string fractionToDecimal(int64_t numerator, int64_t denominator) {
+        bool flag = false;
+        
+        if(numerator > 0 && denominator < 0)
+        {
+            flag = true;
+            denominator = -1*denominator;
+        } else if(numerator < 0 && denominator > 0)
+        {
+            numerator = -1 * numerator;
+            flag = true;
+        } else {
+            numerator = abs(numerator);
+            denominator = abs(denominator);
+        }
+        
+        int64_t remain = numerator % denominator;
+        int64_t d = numerator/denominator;
+        string result = to_string(d);
+        if(flag) result = "-"+result;
+        
+        if(remain == 0)
+            return result;
+        
+        unordered_map<int, int>p;
+        
+        string small = "";
+        int index = 0;
+        while(remain)
+        {
+            p[remain] = index;
+            int64_t tmp = remain*10;
+            int64_t digit = tmp/denominator;
+            remain = tmp%denominator;
+            small += to_string(digit);
+            
+            if(p.count(remain) > 0)
+            {
+                small.insert(p[remain], "(");
+                small += ")";
+                break;
+            }
+
+            index++;
+        }
+
+        
+        return result+"."+small;
+        
+    }
+    
+    
+};
