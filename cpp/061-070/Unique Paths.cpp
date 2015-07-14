e61585c7b692edb077a3c233e8528004e4d2c77e
Zhenchao Gan <303542123@qq.com>
2015-07-15 06:42:42 +0800

    Update Unique Paths.cpp

--- cpp/061-070/Unique Paths.cpp
+++ cpp/061-070/Unique Paths.cpp
@@ -5,12 +5,8 @@ public:
         p[1][1] = 1;
         
         for(int i = 1;i <= m;i++)
-        {
             for(int j = 1;j <= n;j++)
-            {
                 p[i][j] = p[i-1][j] + p[i][j-1];
-            }
-        }
         return p[m][n];
     }
 };
