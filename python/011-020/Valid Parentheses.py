b1d45d488c685ebbb655f216605b321de7eba0c8
Louis1992 <303542123@qq.com>
2015-07-21 10:17:33 +0800

    20.Valid Parentheses

---
+++ python/011-020/Valid Parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    # @param {string} s
+    # @return {boolean}
+    def isValid(self, s):
+        stack = []
+        dict = {"]":"[", "}":"{", ")":"("}
+        for char in s:
+            if char in dict.values():
+                stack.append(char)
+            elif char in dict.keys():
+                if stack == [] or dict[char] != stack.pop():
+                    return False
+            else:
+                return False
+        return not stack
