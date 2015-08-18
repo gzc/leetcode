2fa490635cf160c0eb11a444961458aa750e0a92
Louis1992 <303542123@qq.com>
2015-08-19 06:31:34 +0800

    263. Ugly Number

---
+++ cpp/261-270/Ugly Number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isUgly(int num) {
+        if(num <= 0) return false;
+        if(num == 1) return true;
+        while(num % 2 == 0) num /= 2;
+        while(num % 3 == 0) num /= 3;
+        while(num % 5 == 0) num /= 5;
+        return num == 1;
+    }
+};
