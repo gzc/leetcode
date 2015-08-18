a404a40b88b7f34af5605530aa4830c36b20b03e
Louis1992 <303542123@qq.com>
2015-08-19 07:46:46 +0800

    264. Ugly Number II

---
+++ cpp/261-270/Ugly Number II.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        deque<int> v = {1};
+        int index2 = 0;
+        int index3 = 0;
+        int index5 = 0;
+        int val = 1;
+        
+        while(--n)
+        {
+            val = min(v[index2]*2, min(v[index3]*3, v[index5]*5));
+            v.push_back(val);
+            if (val == v[index2]*2)
+                ++index2;
+            if (val == v[index3]*3)
+                ++index3;
+            if (val == v[index5]*5)
+            {
+                v.pop_front();
+                index2--;
+                index3--;
+            }
+        }
+        return val;
+    }
+};
