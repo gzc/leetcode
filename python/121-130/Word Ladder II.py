48b9938654c0711901ef54a31d4c9bc971be433d
Louis1992 <303542123@qq.com>
2015-08-20 10:21:47 +0800

    126. Word Ladder II

---
+++ python/121-130/Word Ladder II.py
@@ -0,0 +1,22 @@
+class Solution:
+    # @param start, a string
+    # @param end, a string
+    # @param dict, a set of string
+    # @return a list of lists of string
+    def findLadders(self, start, end, dict):
+        level = {start}
+        parents = collections.defaultdict(set)
+        while level and end not in parents:
+            next_level = collections.defaultdict(set)
+            for node in level:
+                for char in string.ascii_lowercase:
+                    for i in range(len(start)):
+                        n = node[:i]+char+node[i+1:]
+                        if n in dict and n not in parents:
+                            next_level[n].add(node)
+            level = next_level
+            parents.update(next_level)
+        res = [[end]]
+        while res and res[0][0] != start:
+            res = [[p]+r for r in res for p in parents[r[0]]]
+        return res
