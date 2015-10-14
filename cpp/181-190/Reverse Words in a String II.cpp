21d81bdb2e44d097068b5bfe02538237e518462f
Louis1992 <303542123@qq.com>
2015-10-14 16:56:47 -0700

    186. Reverse Words in a String II

---
+++ cpp/181-190/Reverse Words in a String II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseWords(string &s) {
+        stringstream ss(s);
+        string result = "";
+        string temp;
+        while(ss >> temp)
+        {
+            if(temp.size() == 0) break;
+            result = temp + " " + result;
+        }
+        result = result.substr(0, result.length()-1);
+        s = result;
+    }
+};
