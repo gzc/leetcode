84977777e243d3aa2c60ebe0d4b67a0bf41128cf
Louis1992 <303542123@qq.com>
2015-10-14 19:11:11 -0700

    293. Flip Game

---
+++ cpp/291-300/Flip Game.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> generatePossibleNextMoves(string s) {
+        vector<string> result;
+        
+        for(int i = 1;i < s.length();i++)
+        {
+            if(s[i-1] == '+' && s[i] == '+')
+            {
+                s[i-1] = s[i] = '-';
+                result.push_back(s);
+                s[i-1] = s[i] = '+';
+            }
+        }
+        
+        return result;
+    }
+};
