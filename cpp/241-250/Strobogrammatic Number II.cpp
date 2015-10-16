18ecb9a2c4d74465148fdfda43e1238c8f0e743b
Louis1992 <303542123@qq.com>
2015-10-16 09:40:52 -0700

    247. Strobogrammatic Number II

---
+++ cpp/241-250/Strobogrammatic Number II.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    
+    map<char, char> m;
+    char chs[5] = {'1','6','9','8','0'};
+    
+    void help(vector<string>& result, string temp, int cur, int &n)
+    {
+        if(n >= 2 && temp[0] == '0') return;
+        if(cur == (n+1)/2)
+        {
+            cout << temp << endl;
+            result.push_back(temp);
+            return;
+        }
+        
+        for(int i = 0;i <= 4;i++)
+        {
+            temp[cur] = chs[i];
+            temp[n - cur - 1] = m[chs[i]];
+            if(n % 2 == 1 && cur == n/2)
+            {
+                if(chs[i] == m[chs[i]])
+                    help(result, temp, cur+1, n);
+            }
+            else help(result, temp, cur+1, n);
+        }
+    }
+    
+public:
+    vector<string> findStrobogrammatic(int n) {
+        m['6'] = '9';
+        m['9'] = '6';
+        m['1'] = '1';
+        m['8'] = '8';
+        m['0'] = '0';
+        
+        vector<string> result;
+        string temp(n, '1');
+        help(result, temp, 0, n);
+        return result;
+    }
+    
+};
