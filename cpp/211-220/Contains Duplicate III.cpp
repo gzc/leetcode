796695e8cb0939f7c2c7adc94843d7e8513d1a93
Louis1992 <303542123@qq.com>
2015-10-20 22:15:41 -0700

    220. Contains Duplicate III

--- cpp/211-220/Contains Duplicate III.cpp
+++ cpp/211-220/Contains Duplicate III.cpp
@@ -2,27 +2,14 @@ class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
         
-        if(nums.size() < 2 || k < 0) return false;
-        
-        deque<int> dq;
-        multiset<long> window;
-        
-        for(auto e : nums)
-        {
-            if(dq.size() > k)
-            {
-                int item = dq.front();
-                dq.pop_front();
-                window.erase(window.find(item));
-            }
-            
-            auto it = window.lower_bound((long)e - (long)t);
-            if(it == window.end() || (*it) > ((long)e + (long)t))
-            {
-                dq.push_back(e);
-                window.insert(e);
-            }
-            else return true;
+        set<int> window; // set is ordered automatically
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > k) window.erase(nums[i-k-1]); // keep the set contains nums i j at most k
+            // -t <= x - nums[i] <= t;
+            auto pos = window.lower_bound(nums[i] - t); // x >= nums[i] - t
+            if (pos != window.end() && *pos - nums[i] <= t) // x <= nums[i] + t
+                return true;
+            window.insert(nums[i]);
         }
         return false;
     }
