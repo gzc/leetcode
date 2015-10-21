03193b6db6933cd91e7587eff1cde8c6b9d4fa1a
Louis1992 <303542123@qq.com>
2015-10-20 20:46:06 -0700

    122. Best Time to Buy and Sell Stock II

--- cpp/121-130/Best Time to Buy and Sell Stock II.cpp
+++ cpp/121-130/Best Time to Buy and Sell Stock II.cpp
@@ -4,22 +4,11 @@ public:
         
         if(prices.size() < 2) return 0;
         
-        int start(0), end(0), profit(0);
-        while(end < prices.size() - 1)
-        {
-            while(start < prices.size() - 1 && prices[start+1] <= prices[start])
-                ++start;
-                
-            if(start == prices.size()-1)
-                    return profit;
-            
-            end = start+1;
-            while(end < prices.size()-1 && prices[end+1] >= prices[end])
-                ++end;
-                
-            profit += prices[end] - prices[start];
-            start = end+1;
-        }
+        int profit(0);
+        
+        for(int i = 0;i < prices.size()-1;i++)
+            if(prices[i+1] > prices[i])
+                profit += (prices[i+1] - prices[i]);
         
         return profit;
     }
