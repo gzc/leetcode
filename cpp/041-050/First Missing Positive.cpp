4738e8b23fca7e12be99fffc6138e0836acf8f32
Louis1992 <303542123@qq.com>
2015-10-23 19:54:45 -0700

    41. First Missing Positive

--- cpp/041-050/First Missing Positive.cpp
+++ cpp/041-050/First Missing Positive.cpp
@@ -1,47 +1,21 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <stack>
-#include <queue>
-
-using namespace std;
-
 class Solution {
 public:
-    int firstMissingPositive(int A[], int n) {
+    int firstMissingPositive(vector<int>& A) {
         
+        if(A.empty()) return 1;
         int i(0);
         
-        while(i != n)
-        {
-            if(A[i] != i+1)
-            {
-                if(A[i] > 0 && A[i] <= n)
-                {
-                    if(A[i] != A[A[i]-1])
-                    {
-                        swap(A[i], A[A[i]-1]);
-                        continue;
-                    }
-                }
+        while(i < A.size()) {
+            if(A[i] != i+1 && A[i] > 0 && A[i] <= A.size() && A[i] != A[A[i]-1]) {
+                swap(A[i], A[A[i]-1]);
+                continue;
             }
             i++;
         }
         
-        for(int i = 0;i < n;i++)
+        for(int i = 0;i < A.size();i++)
             if(A[i] != i+1)
                 return i+1;
-        return A[n-1]+1;
-        
+        return A.back()+1;
     }
-    
 };
-
-int main()
-{
-    Solution s;
-    int a[] = {2,1};
-    cout << s.firstMissingPositive(a, 2);
-
-    return 0;
-}
