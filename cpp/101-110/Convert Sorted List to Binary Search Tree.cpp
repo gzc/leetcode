d5803869b5968c8ce0bf779bfeb1c12f3362a749
Louis1992 <303542123@qq.com>
2015-10-24 23:17:42 -0700

    109. Convert Sorted List to Binary Search Tree

--- cpp/101-110/Convert Sorted List to Binary Search Tree.cpp
+++ cpp/101-110/Convert Sorted List to Binary Search Tree.cpp
@@ -1,32 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
     
-    TreeNode *help(vector<int>::iterator it1, vector<int>::iterator it2)
+    ListNode *mylist;
+    
+    int len(ListNode *head)
     {
-        if(it1 == it2)
-            return new TreeNode(*it1);
-        else if(it1 > it2)
-            return NULL;
-        
-        vector<int>::iterator midit = it1+(it2-it1)/2;
-        TreeNode *node = new TreeNode(*midit);
-        node->left = help(it1,midit-1);
-        node->right = help(midit+1, it2);
-        return node;
+        int l = 0;
+        while(head) {
+            l++;
+            head = head->next;
+        }
+        return l;
+    }
+    
+    TreeNode *help(int n)
+    {
+        if(n == 0) return nullptr;
+        TreeNode *l = help(n/2);
+        TreeNode *newnode = new TreeNode(mylist->val);
+        newnode->left = l;
+        mylist = mylist->next;
+        newnode->right = help(n - n/2 - 1);
+        return newnode;
     }
     
 public:
     TreeNode *sortedListToBST(ListNode *head) {
-        
-        if(!head) return NULL;
-        
-        vector<int> con;
-        while(head)
-        {
-            con.push_back(head->val);
-            head = head->next;
-        }
-        
-        return help(con.begin(), con.end()-1);
-        
+        this->mylist = head;
+        return help(len(head));
     }
 };
