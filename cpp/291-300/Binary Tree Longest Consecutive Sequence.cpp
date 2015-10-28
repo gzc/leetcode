9c83255bd1b54af279617cbff94f5f2873666298
Louis1992 <303542123@qq.com>
2015-10-28 12:21:36 -0700

    298. Binary Tree Longest Consecutive Sequence

---
+++ cpp/291-300/Binary Tree Longest Consecutive Sequence.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    
+    int result;
+    
+    void help(TreeNode* root, int h)
+    {
+        result = max(result, h);
+        
+        if(root->left) {
+            if(root->val == root->left->val -1)
+                help(root->left, h+1);
+            else
+                help(root->left, 1);
+        }
+        if(root->right) {
+            if(root->val == root->right->val -1)
+                help(root->right, h+1);
+            else
+                help(root->right, 1);
+        }
+    }
+    
+public:
+    int longestConsecutive(TreeNode* root) {
+        if(!root) return 0;
+        help(root, 1);
+        return result;
+    }
+};
