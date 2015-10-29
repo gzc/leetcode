28734a9d0e3c5e7286b88b38a6675e79ab84216a
Louis1992 <303542123@qq.com>
2015-10-28 20:33:33 -0700

    275. H-index II

--- cpp/271-280/H-Index II.cpp
+++ cpp/271-280/H-Index II.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int h = citations.size();
+        int i(0),j(citations.size()-1);
+        if(citations.size() == 0) return 0;
         
-        int i = 0;
-        for(int i = 0;i < citations.size();i++)
+        while(i < j)
         {
-            if(citations[i] >= h)
-                break;
-            h--;
+            int mid = (i+j) >> 1;
+            int citation = citations[mid];
+            if(citation >= (citations.size() - mid)) j = mid - 1;
+            else i = mid + 1;
         }
         
-        return h;
+        if(citations[i] >= citations.size() - i) return citations.size() - i;
+        else return citations.size() - i - 1;
     }
+    
 };
