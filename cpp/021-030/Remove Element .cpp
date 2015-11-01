765089fe1ce6e2f0ac71ac4e9dc36b81dde19271
Zhenchao Gan <303542123@qq.com>
2015-11-01 00:14:16 -0700

    Update Remove Element .cpp

--- cpp/021-030/Remove Element .cpp
+++ cpp/021-030/Remove Element .cpp
@@ -1,39 +1,11 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <stack>
-#include <queue>
-
-using namespace std;
-
 class Solution {
 public:
-    int removeElement(int A[], int n, int elem) {
-        
-        int i(0),j(n-1);
-        
-        while(i <= j)
-        {
-            if(A[i] == elem)
-            {
-                swap(A[i], A[j]);
-                j--;
-            }
-            if(A[i] != elem) i++;
+    int removeElement(vector<int>& nums, int val) {
+        int first(0);
+        for(int i = 0;i < nums.size();i++) {
+            if(nums[i] != val)
+                swap(nums[i], nums[first++]);
         }
-        
-        return j+1;
+        return first;
     }
-    
 };
-
-int main()
-{
-    Solution s;
-    
-    int a[3] = {1,1,1};
-    int c = s.removeElement(a, 3, 1);
-    cout << c;
-    
-    return 0;
-}
