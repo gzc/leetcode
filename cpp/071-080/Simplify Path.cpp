69d46d8ca199baed58b98ce4fae455188d76f568
Zhenchao Gan <303542123@qq.com>
2015-11-01 16:48:39 -0800

    Update Simplify Path.cpp

--- cpp/071-080/Simplify Path.cpp
+++ cpp/071-080/Simplify Path.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
     string simplifyPath(string path) {
-        
-        int pre = 0;
-        int cur = 0;
-        stack<string>p;
-        path += "/";
-        
-        while( (cur = path.find("/", pre+1)) != string::npos)
-        {
-            string s = path.substr(pre+1, cur-pre-1);
-            if(s == ".." && !p.empty()) p.pop();
-            else if(s == ".." && p.empty()) {;}
-            else if(s == ".") {;}
-            else if(s.length() == 0) {;}
-            else p.push(s);
-            pre = cur;
+        string res, tmp;
+        vector<string> stk;
+        stringstream ss(path);
+        while(getline(ss,tmp,'/')) {
+            if (tmp == "" or tmp == ".") continue;
+            if (tmp == ".." and !stk.empty()) stk.pop_back();
+            else if (tmp != "..") stk.push_back(tmp);
         }
-        
-        string result = "";
-        while(!p.empty())
-        {
-            result = "/" + p.top() + result;
-            p.pop();
-        }
-        if(result.length() == 0) return "/";
-        return result;
-        
+        for(auto str : stk) res += "/"+str;
+        return res.empty() ? "/" : res;
     }
 };
