3168ebf6de5438e59b23ff4407d365a2ff04b539
Zhenchao Gan <303542123@qq.com>
2015-11-09 13:53:54 -0800

    Update Implement Queue using Stacks.cpp

--- cpp/231-240/Implement Queue using Stacks.cpp
+++ cpp/231-240/Implement Queue using Stacks.cpp
@@ -1,42 +1,35 @@
 class Queue {
 public:
-    
+
     stack<int>s1;
     stack<int>s2;
-    
+
     // Push element x to the back of queue.
     void push(int x) {
         s1.push(x);
     }
-    
+
     // Removes the element from in front of queue.
     void pop(void) {
-        if(s2.empty())
-        {
-            while(!s1.empty())
-            {
-                int v = s1.top();
-                s1.pop();
-                s2.push(v);
-            }
-        }
+        peek();
         s2.pop();
     }
-    
+
     // Get the front element.
     int peek(void) {
-        if(s2.empty())
-        {
-            while(!s1.empty())
-            {
-                int v = s1.top();
-                s1.pop();
-                s2.push(v);
-            }
-        }
+        if(s2.empty()) transit();
         return s2.top();
     }
     
+    void transit() {
+        while(!s1.empty())
+        {
+            int v = s1.top();
+            s1.pop();
+            s2.push(v);
+        }
+    }
+
     // Return whether the queue is empty.
     bool empty(void) {
         return s1.empty() && s2.empty();
