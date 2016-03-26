d9e803ad01ed081279fe5b6b967efa890b4660a6
Louis1992 <303542123@qq.com>
2016-03-26 11:10:48 -0700

    330. Patching Array

---
+++ cpp/321-330/Patching Array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        
+        sort(nums.begin(), nums.end());
+        
+        int i(0), res(0);
+        long miss(0);
+        
+        while(miss < n) {
+            if(i < nums.size() && miss >= (nums[i] - 1) ) {
+                miss += nums[i];
+                i++;
+            } else {
+                res++;
+                miss += (miss+1);
+            }
+        }
+        
+        return res;
+    }
+};
