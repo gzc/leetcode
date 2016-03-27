61c893bfcd66682cfd0058becc2feef59e887dc9
Zhenchao Gan <303542123@qq.com>
2016-03-27 11:56:12 -0700

    158. Read N Characters Given Read4 II - Call multiple times

--- cpp/151-160/Read N Characters Given Read4 II - Call multiple times.cpp
+++ cpp/151-160/Read N Characters Given Read4 II - Call multiple times.cpp
@@ -3,42 +3,16 @@ int read4(char *buf);
 
 class Solution {
     
-    queue<char> buffer;
+    char buf4[4];
+    int i4 = 0, n4 = 0;
     
 public:
-    /**
-     * @param buf Destination buffer
-     * @param n   Maximum number of characters to read
-     * @return    The number of characters read
-     */
+
     int read(char *buf, int n) {
-        
-        int len = 0;
-        
-        while(buffer.size() > 0)
-        {
-            *buf = buffer.front();
-            buffer.pop();
-            buf++;
-            len++;
-            if(n == len){
-                return len;
-            }
-        }
-        
-        while(len < n)
-        {
-            int v = read4(buf);
-            if(v == 0) return len;
-            len += v;
-            buf += v;
-        }
-        
-        int diff = len - n;
-        for(char *temp = buf - diff; temp != buf; temp++)
-            buffer.push(*temp);
-        buf = buf - diff;
-        *buf = '\0';
-        return n;
+        int i = 0;
+        while (i < n && (i4 < n4 || (i4 = 0) < (n4 = read4(buf4))))
+            buf[i++] = buf4[i4++];
+        return i;
     }
+    
 };
