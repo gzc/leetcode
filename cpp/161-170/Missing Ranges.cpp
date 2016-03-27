b5847218bcd68706cbd038b01ac1d7ff62a56241
Zhenchao Gan <303542123@qq.com>
2016-03-27 12:38:30 -0700

    163. Missing Ranges

--- cpp/161-170/Missing Ranges.cpp
+++ cpp/161-170/Missing Ranges.cpp
@@ -1,58 +1,21 @@
 class Solution {
+    
+    string get_range(int start, int end) {
+        return start==end? to_string(start) : to_string(start)+"->"+to_string(end);
+    }
+    
 public:
+
     vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
-        
         vector<string> result;
-        
-        if(nums.size() == 0)
+        int pre = lower-1;
+        for(int i =0; i <= nums.size(); i++)
         {
-            if(lower == upper) {
-                string temp = to_string(lower);
-                result.push_back(temp);
-            } else {
-                string temp = to_string(lower) + "->" + to_string(upper);
-                result.push_back(temp);
-            }
+            int cur = (i==nums.size()? upper+1:nums[i]);
+            if(cur-pre>=2)
+                result.push_back(get_range(pre+1,cur-1));
+            pre = cur;
         }
-        
-        for(int i = 0;i < nums.size();i++)
-        {
-            if(i == 0) {
-                if(nums[i] - lower >= 2) {
-                    string temp = to_string(lower) + "->" + to_string(nums[i]-1);
-                    result.push_back(temp);
-                } else if(nums[i] - lower == 1) {
-                    string temp = to_string(nums[i] - 1);
-                    result.push_back(temp);
-                } else {
-                    
-                }
-            }
-            
-            if(i > 0) {
-                if(nums[i] - nums[i-1] > 2) {
-                    string temp = to_string(nums[i-1]+1) + "->" + to_string(nums[i]-1);
-                    result.push_back(temp);
-                } else if(nums[i] - nums[i-1] == 2) {
-                    string temp = to_string(nums[i]-1);
-                    result.push_back(temp);
-                }
-            }
-            
-            
-            if(i == nums.size() - 1) {
-                if(upper - nums[i] >= 2) {
-                    string temp = to_string(nums[i]+1) + "->" + to_string(upper);
-                    result.push_back(temp);
-                } else if(upper - nums[i] == 1) {
-                    string temp = to_string(nums[i] + 1);
-                    result.push_back(temp);
-                } else {
-                    
-                }
-            }
-        }
-        
         return result;
     }
 };
