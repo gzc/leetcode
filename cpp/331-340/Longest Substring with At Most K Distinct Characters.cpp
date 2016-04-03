90544b52f088b63bb4e712393b85e17567cf7acb
Zhenchao Gan <303542123@qq.com>
2016-04-03 15:15:03 -0700

    340. Longest Substring with At Most K Distinct Characters

---
+++ cpp/331-340/Longest Substring with At Most K Distinct Characters.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        int current(0);
+        int longest(0);
+        int previouspos(0);
+        map<char, int>m;
+        
+        for(int i = 0;i < s.length();i++)
+        {
+            if(current < k && m[s[i]] == 0) {
+                m[s[i]] = 1;
+                current++;
+            } else if(m[s[i]] > 0) {
+                m[s[i]]++;
+            } else {
+                longest = max(longest, i-previouspos);
+                m[s[i]] = 1;
+                while(1) {
+                    char ch = s[previouspos];
+                    previouspos++;
+                    m[ch]--;
+                    if(m[ch] == 0) break;
+                }
+            }
+        }
+        longest = max(longest, (int)s.length()-previouspos);
+        return longest;
+    }
+};
