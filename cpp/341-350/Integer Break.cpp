134d995276a2097b09425d03299986b83c78ff5b
Zhenchao Gan <303542123@qq.com>
2016-05-27 09:33:01 -0700

    343. Integer Break

---
+++ cpp/341-350/Integer Break.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    long long integerBreak(long long n) {
+        if(n == 2) return 1;
+        if(n == 3) return 2;
+        if(n == 4) return 4;
+        if(n == 5) return 6;
+        if(n == 6) return 9;
+        return 3 * integerBreak(n - 3);
+    }
+};
