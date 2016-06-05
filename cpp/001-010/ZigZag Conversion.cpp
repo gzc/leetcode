dc739d520aa6609caa5bb9573dc0a6d83655b349
Zhenchao Gan <303542123@qq.com>
2016-06-04 23:00:44 -0700

    6. ZigZag Conversion

--- cpp/001-010/ZigZag Conversion.cpp
+++ cpp/001-010/ZigZag Conversion.cpp
@@ -1,27 +1,13 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-
-using namespace std;
-
-/*
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
- */
-
 class Solution {
 public:
     string convert(string s, int nRows) {
         string v[nRows];
         int k(0), up(1);
         for(size_t i = 0;i < s.size();i++) {
-            v[k] = v[k] + s[i];
+            v[k] += s[i];
             k += up;
             if(k == (nRows-1) ) up = -1;
-            else if(k == 0) up = 1;
+            if(k == 0) up = 1;
             if(k == nRows) k = 0;
         }
         string res("");
@@ -29,13 +15,4 @@ public:
             res += v[i];
         return res;
     }
-    
 };
-
-int main()
-{
-    Solution s;
-    cout << s.convert("PAYPALISHIRING", 3);
-    
-    return 0;
-}
