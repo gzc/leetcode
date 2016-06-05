97844ab2ecc0ad584e048f1f038ab21a01ba6a5e
Zhenchao Gan <303542123@qq.com>
2016-06-04 22:52:03 -0700

    5. Longest Palindromic Substring

---
+++ cpp/001-010/Longest Palindromic Substring.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n = s.length();
+        int longestBegin = 0;
+        int maxLen = 1;
+        bool table[1000][1000] = {false};
+        for (int i = 0; i < n; i++)
+            table[i][i] = true;
+  
+        for (int i = 0; i < n-1; i++) {
+            if (s[i] == s[i+1]) {
+                table[i][i+1] = true;
+                longestBegin = i;
+                maxLen = 2;
+            }
+        }
+  
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i < n-len+1; i++) {
+                int j = i+len-1;
+                if (s[i] == s[j] && table[i+1][j-1]) {
+                    table[i][j] = true;
+                    longestBegin = i;
+                    maxLen = len;
+                }
+            }
+        }
+        return s.substr(longestBegin, maxLen);
+    }
+};
