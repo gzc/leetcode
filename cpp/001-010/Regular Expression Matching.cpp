db66211e303a72a4788ee3fb096b158d7b0dc127
Zhenchao Gan <303542123@qq.com>
2016-06-06 14:30:41 -0700

    10. Regular Expression Matching

--- cpp/001-010/Regular Expression Matching.cpp
+++ cpp/001-010/Regular Expression Matching.cpp
@@ -1,43 +1,15 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-
-using namespace std;
-
-class Solution {
-public:
-    bool isMatch(const char *s, const char *p) {
-        
-        if(*s == 0 && *p == 0) return true;
-        if(*s != 0 && *p == 0) return false;
-        
-        if(*p == '.')
-        {
-            if(*(p+1) == '*')
-            {
-                if(*s == 0) return isMatch(s, p+2);
-                else return isMatch(s+1, p) || isMatch(s, p+2);
-            }
-            else if(*s == 0) return false;
-            else return isMatch(s+1, p+1);
-        }
-        else
-        {
-            if(*(p+1) == '*' && *s == *p)
-                return isMatch(s, p+2) || isMatch(s+1, p);
-            else if(*(p+1) == '*' && *s != *p)
-                return isMatch(s, p+2);
-            else if(*s != *p) return false;
-            else return isMatch(s+1, p+1);
-        }
-        
+bool isMatch(const char *s, const char *p) {
+    if (*p == '\0') return *s == '\0';
+ 
+    if (*(p+1) != '*') {
+        return ((*p == *s) || (*p == '.' && *s != 0)) && isMatch(s+1, p+1);
     }
     
-};
-
-int main()
-{
-    Solution s;
+    // next char is '*'
+    while ((*p == *s) || (*p == '.' && *s != 0)) {
+        if (isMatch(s, p+2)) return true;
+        s++;
+    }
     
-    cout << s.isMatch("a", ".*..a*");
+    return isMatch(s, p+2);
 }
