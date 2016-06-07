662d68b62e5abdffee6593498eca69088e58fa2c
Zhenchao Gan <303542123@qq.com>
2016-06-07 11:21:05 -0700

    300. Longest Increasing Subsequence

--- cpp/291-300/Longest Increasing Subsequence.cpp
+++ cpp/291-300/Longest Increasing Subsequence.cpp
@@ -1,28 +1,13 @@
 class Solution {
-    
-    int binarySearch(vector<int> &myvec, int len, int target) {
-        int i(0),j(len);
-        while(i <= j && i < len) {
-            int mid = (i+j) >> 1;
-            if(myvec[mid] == target) return mid;
-            if(myvec[mid] > target) j = mid - 1;
-            else i = mid + 1;
-        }
-        return i;
-    }
-    
+
 public:
     int lengthOfLIS(vector<int>& nums) {
-        if(nums.size() < 2) return nums.size();
-        int len(1);
-        vector<int> myvec(nums.size(), 0);
-        myvec[0] = nums[0];
-        
-        for(int i = 1;i < nums.size();i++) {
-            int index = binarySearch(myvec, len, nums[i]);
-            myvec[index] = nums[i];
-            if(len == index) len++;
+        vector<int> res;
+        for(int i=0; i<nums.size(); i++) {
+            auto it = std::lower_bound(res.begin(), res.end(), nums[i]);
+            if(it == res.end()) res.push_back(nums[i]);
+            else *it = nums[i];
         }
-        return len;
+        return res.size();
     }
 };
