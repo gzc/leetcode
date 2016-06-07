9e156744320b70f78576c0e9d663fde8de55ecfe
Zhenchao Gan <303542123@qq.com>
2016-06-07 16:14:48 -0700

    354. Russian Doll Envelopes

--- cpp/351-360/Russian Doll Envelopes.cpp
+++ cpp/351-360/Russian Doll Envelopes.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int maxEnvelopes(vector<pair<int, int>>& envelopes) {
-        if(envelopes.size() < 2) return envelopes.size();
         auto Cmp = [](const pair<int, int> &a, const pair<int, int> &b) { 
             if(a.first < b.first) return true;
-            if(a.first == b.first && a.second <= b.second) return true;
+            if(a.first == b.first && a.second > b.second) return true;
             return false;
         };
+        auto Cmp2 = [](const pair<int, int> &a, const pair<int, int> &b) { 
+            return a.second < b.second;
+        };
         sort(envelopes.begin(), envelopes.end(), Cmp);
-        vector<int> myvec(envelopes.size(), 1);
-        
-        for(int i = 1;i < envelopes.size();i++) {
-            for(int j = 0;j < i;j++) {
-                if(envelopes[j].first < envelopes[i].first && envelopes[j].second < envelopes[i].second) {
-                    myvec[i] = max(myvec[i], myvec[j]+1);
-                }
-            }
+
+        vector<pair<int, int>> res;
+        for(int i=0; i<envelopes.size(); i++) {
+            auto it = std::lower_bound(res.begin(), res.end(), envelopes[i], Cmp2);
+            if(it == res.end()) res.push_back(envelopes[i]);
+            else *it = envelopes[i];
         }
-        return *max_element(myvec.begin(),myvec.end());
+        return res.size();
     }
 };
