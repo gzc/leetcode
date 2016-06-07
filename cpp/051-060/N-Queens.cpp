36703badb8eccaf7e1a30e957ab180721032a2b8
Zhenchao Gan <303542123@qq.com>
2016-06-07 10:09:18 -0700

    51. N-Queens

--- cpp/051-060/N-Queens.cpp
+++ cpp/051-060/N-Queens.cpp
@@ -1,77 +1,42 @@
-/*************************************************************************
-	> File Name: N-Queens.cpp
-	> Author: Louis1992
-	> Mail: zhenchaogan@gmail.com
-	> Blog: http://gzc.github.io
-	> Created Time: Fri Apr 17 23:37:12 2015
- ************************************************************************/
-#include<iostream>
-#include<cstdio>
-#include<list>
-#include<vector>
-#include<unordered_map>
-#include<climits>
-#include<unordered_set>
-#include<map>
-#include<set>
-#include<stack>
-#include<queue>
-using namespace std;
-
-
 class Solution {
     
     bool *rows;
     bool *cols;
     bool *add;
     bool *diff;
-    unsigned int n;
+    int n;
     
     bool valid(int i, int j)
     {
-        if(rows[i] == false && cols[j] == false && add[i+j] == false && diff[i-j+n] == false)
+        if(!rows[i] && !cols[j] && !add[i+j] && !diff[i-j+n])
             return true;
         return false;
     }
     
-    void help(vector<vector<string> > &result, vector<pair<int, int> > &tmp, int i, int j)
+    void help(vector<vector<string>> &result, vector<pair<int, int>> &tmp, int i)
     {
         if(i == n)
         {
             if(n == tmp.size())
             {
                 string s(n, '.');
-                vector<string>t(n,s);
+                vector<string>t{n,s};
                 for(int k = 0;k < tmp.size();k++)
-                {
                     t[tmp[k].first][tmp[k].second] = 'Q';
-                }
                 result.push_back(t);
-                return;
-            } else {
-                return;
             }
         } else {
-            
-            for(int k = 0;k < n;k++)
+            for(int j = 0;j < n;j++)
             {
-                j = k;
                 if(valid(i, j))
                 {
-                    rows[i] = true;
-                    cols[j] = true;
-                    add[i+j] = true;
-                    diff[i-j+n] = true;
+                    rows[i] = cols[j] = add[i+j] = diff[i-j+n] = true;
                     tmp.push_back(make_pair(i, j));
-                    help(result, tmp, i+1, 0);
+                    help(result, tmp, i+1);
                     tmp.pop_back();
-                    rows[i] = false;
-                    cols[j] = false;
-                    add[i+j] = false;
-                    diff[i-j+n] = false;
+                    rows[i] = cols[j] = add[i+j] = diff[i-j+n] = false;
                 }
             }
-            return;
         }
     }
     
@@ -84,29 +49,11 @@ public:
         diff = new bool[2*n];
         this->n = n;
         
-        vector<vector<string> > result;
-        vector<pair<int, int> > tmp;
+        vector<vector<string>> result;
+        vector<pair<int, int>> tmp;
         
-        help(result, tmp, 0, 0);
+        help(result, tmp, 0);
         
         return result;
-        
     }
 };
-
-int main() {
-    Solution s;
-    vector<vector<string> > v = s.solveNQueens(4);
-    for(int i = 0;i < v.size();i++)
-    {
-        vector<string>tmp = v[i];
-        for(int j = 0;j < tmp.size();j++)
-        {
-            cout << tmp[j] << endl;
-        }
-        cout << endl << endl;
-    }
-
-
-    return 0;
-}
