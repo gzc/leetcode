bb80052bcfecb4694b40b3d421955b94314f6533
Zhenchao Gan <303542123@qq.com>
2016-06-25 23:00:55 -0700

    367. Valid Perfect Square

---
+++ cpp/361-370/Valid Perfect Square.cpp
@@ -0,0 +1,28 @@
+class Solution {
+
+    long long mysqrt(long long x) {
+        long long i(0), j(x);
+        
+        while(i < j) {
+            long long mid = (i+j)/2;
+            long long v = mid*mid;
+            if (v == x) return mid;
+            else if(v < x) i = mid + 1;
+            else j = mid - 1;
+        }
+        return i;
+    }
+    
+public:
+    bool isPerfectSquare(int num) {
+        
+        if(num < 0) return false;
+        
+        int v = mysqrt(num);
+        
+        if(v*v == num) return true;
+        
+        return false;
+        
+    }
+};
