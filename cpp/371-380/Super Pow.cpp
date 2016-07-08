0ff16805b58bd2a603118d1691e376e971844c06
Zhenchao Gan <303542123@qq.com>
2016-07-07 22:07:02 -0700

    372. Super Pow

---
+++ cpp/371-380/Super Pow.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    
+    int help(int a, int b) {
+        int res(1);
+        a %= 1337;
+        for(int i = 0;i < b;i++) {
+            res = (res * a) % 1337;
+        }
+        return res;
+    }
+    
+public:
+    int superPow(int a, vector<int>& b) {
+        if(b.empty()) return 1;
+        int last_digit = b.back();
+        b.pop_back();
+        return help(superPow(a, b), 10) * help(a, last_digit) % 1337;
+    }
+};
