949401ed64c41de5d31ee9d07535ae9f6d4e1636
Zhenchao Gan <303542123@qq.com>
2016-08-01 21:06:55 -0700

    43. Multiply Strings

--- cpp/041-050/Multiply Strings.cpp
+++ cpp/041-050/Multiply Strings.cpp
@@ -1,56 +1,39 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <stack>
-#include <queue>
-
-using namespace std;
-
 class Solution {
     
 public:
     string multiply(string num1, string num2) {
         
-        string s(num1.length()+num2.length(),'0');
+        string result(num1.length()+num2.length(), '0');
         
         reverse(num1.begin(), num1.end());
         reverse(num2.begin(), num2.end());
         
-        for(int i = 0;i < num1.length();i++)
+        for (int i = 0;i < num1.length();i++)
         {
-            int flag = 0;
-            for(int j = 0;j < num2.length();j++)
+            int carry(0);
+            for (int j = 0;j < num2.length();j++)
             {
-                int digit = s[i+j] - '0';
+                int digit = result[i+j] - '0';
                 int num = (num1[i] - '0') * (num2[j] - '0');
-                int res = digit + num + flag;
-                s[i+j] = (res % 10) + '0';
-                flag = res / 10;
+                int res = digit + num + carry;
+                result[i+j] = (res % 10) + '0';
+                carry = res / 10;
             }
             
             int index = i + num2.size();
-            while(flag)
+            while (carry)
             {
-                int digit = s[index] - '0';
-                int res = digit + flag;
-                s[index] = (res % 10) + '0';
-                flag = res / 10;
+                int digit = result[index] - '0';
+                int res = digit + carry;
+                result[index] = (res % 10) + '0';
+                carry = res / 10;
             }
         }
         
-        
-        reverse(s.begin(), s.end());
-        while(s[0] == '0' && s.length() > 1)
-            s = s.substr(1);
-        return s;
+        reverse(result.begin(), result.end());
+        while(result[0] == '0' && result.length() > 1)
+            result = result.substr(1);
+        return result;
     }
     
 };
-
-int main()
-{
-    Solution s;
-    cout << s.multiply("999", "777");
-
-    return 0;
-}
