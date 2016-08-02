e2348e8e1e0a4a066897d4120d463fdd5f5c25f2
Zhenchao Gan <303542123@qq.com>
2016-08-01 19:50:45 -0700

    114. Flatten Binary Tree to Linked List

--- cpp/111-120/Flatten Binary Tree to Linked List.cpp
+++ cpp/111-120/Flatten Binary Tree to Linked List.cpp
@@ -1,16 +1,17 @@
 class Solution {
     
+    TreeNode* flatten(TreeNode* root, TreeNode* pre) {
+        if (root == nullptr) return pre;
+        pre = flatten(root->right, pre);
+        pre = flatten(root->left, pre);
+        root->right = pre;
+        root->left = nullptr;
+        pre = root;
+        return root;
+    }
+    
 public:
-    void flatten(TreeNode *root) {
-        while ( root ) {
-            if ( root->left ) {
-                TreeNode *ptr = root->left;
-                while ( ptr->right ) ptr = ptr->right;
-                ptr->right = root->right;
-                root->right = root->left;
-                root->left = NULL;
-            }
-            root = root->right;
-        }
+    void flatten(TreeNode* root) {
+        flatten(root, nullptr);
     }
 };
