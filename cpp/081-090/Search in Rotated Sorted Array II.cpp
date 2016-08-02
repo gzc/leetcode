c73741eefc974ba6950c2ddbace64d9aa9dbda07
Zhenchao Gan <303542123@qq.com>
2016-08-01 20:06:23 -0700

    81. Search in Rotated Sorted Array II

--- cpp/081-090/Search in Rotated Sorted Array II.cpp
+++ cpp/081-090/Search in Rotated Sorted Array II.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int key) {
-        int n = nums.size();
-        
-        int l = 0, r = n - 1;
+        int l(0), r(nums.size() - 1);
         while (l <= r) {
             int m = (l + r) >> 1;
             if (nums[m] == key) return true;
             if (nums[l] < nums[m]) {
-                if (nums[l] <= key && key < nums[m])
-                r = m - 1;
-                else
-                l = m + 1;
+                if (nums[l] <= key && key < nums[m]) r = m - 1;
+                else l = m + 1;
             } else if (nums[l] > nums[m]) {
-                if (nums[m] < key && key <= nums[r])
-                l = m + 1;
-                else
-                r = m - 1;
+                if (nums[m] < key && key <= nums[r]) l = m + 1;
+                else r = m - 1;
             } else l++;
         }
         return false;
-        
     }
 };
-}
