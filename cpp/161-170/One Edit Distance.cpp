02f7eeedb2b7af52adc87758527c23c70d85746f
Zhenchao Gan <303542123@qq.com>
2016-08-02 20:49:03 -0700

    161. One Edit Distance

--- cpp/161-170/One Edit Distance.cpp
+++ cpp/161-170/One Edit Distance.cpp
@@ -2,31 +2,31 @@ class Solution {
     
 public:
     bool isOneEditDistance(string s, string t) {
-        if(abs((int)s.length() - (int)t.length()) >= 2) return false;
-        if(s == t) return false;
-        if(s.length() == t.length()) {
+        if (abs((int)s.length() - (int)t.length()) >= 2) return false;
+        if (s == t) return false;
+        if (s.length() == t.length()) {
             int diff(0);
-            for(int i = 0;i < s.length();i++)
+            for (int i = 0;i < s.length();i++)
             {
-                if(s[i] != t[i]) diff++;
-                if(diff >= 2) return false;
+                if (s[i] != t[i]) diff++;
+                if (diff >= 2) return false;
             }
             return true;
         }
-        if(s.length() > t.length()) return isOneEditDistance(t, s);
+        if (s.length() > t.length()) return isOneEditDistance(t, s);
         
-        int i(0),j(0);
-        while(i < s.length() && j < t.length())
+        int i(0), j(0);
+        while (i < s.length() && j < t.length())
         {
-            if(s[i] == t[j]){
+            if (s[i] == t[j]){
                 i++;
                 j++;
             } else {
                 j++;
             }
         }
-        if(i == s.length() && j == t.length()) return true;
-        if(j == i) return true;
+        if (i == s.length() && j == t.length()) return true;
+        if (j == i) return true;
         return false;
     }
     
