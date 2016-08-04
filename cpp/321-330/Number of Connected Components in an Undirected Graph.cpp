df9648ff18c01a17e098325cd69e55f07f877d9b
Zhenchao Gan <303542123@qq.com>
2016-08-03 21:58:38 -0700

    323. Number of Connected Components in an Undirected Graph

--- cpp/321-330/Number of Connected Components in an Undirected Graph.cpp
+++ cpp/321-330/Number of Connected Components in an Undirected Graph.cpp
@@ -2,7 +2,7 @@ class Solution {
     
     int findroot(int c, vector<int> &u)
     {
-        while(c != u[c]) c = u[c];
+        while (c != u[c]) c = u[c];
         return c;
     }
     
@@ -10,17 +10,18 @@ public:
     int countComponents(int n, vector<pair<int, int>>& edges) {
         
         vector<int> u(n, 0);
-        for(int i = 0;i < n;i++)
+        for (int i = 0;i < n;i++) {
             u[i] = i;
+        }
         
-        for(int i = 0;i < edges.size();i++)
+        for (int i = 0;i < edges.size();i++)
         {
             int root1 = findroot(edges[i].first, u);
             int root2 = findroot(edges[i].second, u);
-            if(root1 == root2) continue;
-            
-            u[root2] = root1;
-            n--;
+            if(root1 != root2) {
+                u[root2] = root1;
+                n--;
+            }
         }
         
         return n;
