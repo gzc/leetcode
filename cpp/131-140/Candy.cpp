6a614a74ea9b114c89989360b846c7a3da46ecd3
Zhenchao Gan <303542123@qq.com>
2016-08-04 19:13:56 -0700

    135. Candy

--- cpp/131-140/Candy.cpp
+++ cpp/131-140/Candy.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
-    int candy(vector<int> &ratings) {
-        
+    int candy(vector<int>& ratings) {
         int len = ratings.size();
         vector<int>v(len, 1);
         
-        for(int i = 1;i < len;i++)
-            if(ratings[i] > ratings[i-1])
+        for (int i = 1;i < len;i++)
+            if (ratings[i] > ratings[i-1])
                 v[i] = v[i-1] + 1;
                 
-        for(int i = len-1;i > 0;i--)
-            if(ratings[i-1] > ratings[i])
+        for (int i = len-1;i > 0;i--)
+             if(ratings[i-1] > ratings[i])
                 v[i-1] = max(v[i]+1, v[i-1]);
                 
         int result(0);
-        for(auto e : v)
+        for (auto e : v)
             result += e;
         return result;
     }
-    
 };
