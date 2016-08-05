f09c0d6066df109c674be745b892e2a6c8b819e5
Zhenchao Gan <303542123@qq.com>
2016-08-04 21:41:16 -0700

    280. Wiggle Sort

--- cpp/271-280/Wiggle Sort.cpp
+++ cpp/271-280/Wiggle Sort.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
         
-        for(int i = 0;i < nums.size();i++) {
-            if(i % 2 == 0 && (i+1) < nums.size() && nums[i] > nums[i+1]) swap(nums[i], nums[i+1]);
-            if(i % 2 == 1 && (i+1) < nums.size() && nums[i] < nums[i+1]) swap(nums[i], nums[i+1]);
+        for (int i = 0;i < nums.size();i++) {
+            if (i % 2 == 0 && (i+1) < nums.size() && nums[i] > nums[i+1]) swap(nums[i], nums[i+1]);
+            if (i % 2 == 1 && (i+1) < nums.size() && nums[i] < nums[i+1]) swap(nums[i], nums[i+1]);
         }
         
     }
