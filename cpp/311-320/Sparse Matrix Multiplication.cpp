c32dce0be70069f53fd9205fc18a09378b1ca935
Zhenchao Gan <303542123@qq.com>
2016-08-04 20:12:54 -0700

    311. Sparse Matrix Multiplication

--- cpp/311-320/Sparse Matrix Multiplication.cpp
+++ cpp/311-320/Sparse Matrix Multiplication.cpp
@@ -9,17 +9,17 @@ public:
         vector<vector<int>> res(row1, vector<int>(col2, 0));
         vector<unordered_map<int,int>> CacheB(col2);
         
-        for(int i = 0;i < row2;i++)
-            for(int j = 0;j < col2;j++)
-                if(B[i][j] != 0)
+        for (int i = 0;i < row2;i++)
+            for (int j = 0;j < col2;j++)
+                if (B[i][j] != 0)
                     CacheB[j][i] = B[i][j];
         
-        for(int i = 0;i < row1;i++) {
-            for(int j = 0;j < col2;j++) {
+        for (int i = 0;i < row1;i++) {
+            for (int j = 0;j < col2;j++) {
                 unordered_map<int, int> right = CacheB[j];
-                if(right.size() == 0) continue;
-                for(int k = 0;k < col1;k++) {
-                    if(A[i][k] != 0 && right.count(k) > 0) {
+                if (right.size() == 0) continue;
+                for (int k = 0;k < col1;k++) {
+                    if (A[i][k] != 0 && right.count(k) > 0) {
                         res[i][j] += A[i][k] * right[k];
                     }
                 }
