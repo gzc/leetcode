e71b35550764c478e01960672aca9020fde1e804
Zhenchao Gan <303542123@qq.com>
2016-08-04 19:23:26 -0700

    290. Word Pattern

--- cpp/281-290/Word Pattern.cpp
+++ cpp/281-290/Word Pattern.cpp
@@ -2,31 +2,32 @@ class Solution {
 public:
     bool wordPattern(string pattern, string str) {
         stringstream ss(str);
+        // bijection, so need two maps
         map<char, string>m;
         map<string, bool>m2;
         
-        for(int i = 0;i < pattern.length();i++)
+        for (int i = 0;i < pattern.length();i++)
         {
             char ch = pattern[i];
             string temp;
             ss >> temp;
             
-            if(temp == "") return false;
+            if (temp == "") return false;
             
-            if(m.count(ch) == 0)
+            if (m.count(ch) == 0)
             {
-                if(m2[temp] == true) return false;
+                if (m2[temp] == true) return false;
                 m[ch] = temp;
                 m2[temp] = true;
             } else {
-                if(m[ch] != temp)
+                if (m[ch] != temp)
                     return false;
             }
         }
         
         string test;
         ss >> test;
-        if(test.length() > 0) return false;
+        if (test.length() > 0) return false;
         
         return true;
     }
