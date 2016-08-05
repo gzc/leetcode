e23230d9639a186b867201ae28342eb149aa7526
Zhenchao Gan <303542123@qq.com>
2016-08-04 19:29:31 -0700

    246. Strobogrammatic Number

--- cpp/241-250/Strobogrammatic Number.cpp
+++ cpp/241-250/Strobogrammatic Number.cpp
@@ -3,11 +3,9 @@ class Solution {
 public:
     bool isStrobogrammatic(string num) {
         unordered_map<char, char> m = {{'0', '0'}, {'1', '1'}, {'6','9'}, {'8','8'}, {'9', '6'}};
+        int len(num.length());
         
-        string temp = "";
-        int len = num.length();
-        
-        for(int i = 0;i < (len+1)/2;i++)
+        for (int i = 0;i < (len+1)/2;i++)
         {
             if(m.count(num[i]) == 0) return false;
             if(m[num[i]] != num[len - i - 1]) return false;
