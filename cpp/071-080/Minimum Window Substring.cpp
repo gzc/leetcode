fc218f79379c6bb11063e1af7495a7bc6896f901
Zhenchao Gan <303542123@qq.com>
2016-08-05 21:55:53 -0700

    76. Minimum Window Substring

--- cpp/071-080/Minimum Window Substring.cpp
+++ cpp/071-080/Minimum Window Substring.cpp
@@ -1,52 +1,40 @@
 class Solution {
 public:
     string minWindow(string S, string T) {
-        if (S.empty() || T.empty())
-        {
-            return "";
-        }
-        int count = T.size();
+        if (S.empty() || T.empty()) return "";
+        int needed(T.size());
         int require[128] = {0};
         bool chSet[128] = {false};
-        for (int i = 0; i < count; ++i)
+        for (int i = 0; i < needed; ++i)
         {
             require[T[i]]++;
             chSet[T[i]] = true;
         }
-        int i = -1;
-        int j = 0;
-        int minLen = INT_MAX;
-        int minIdx = 0;
-        while (i < (int)S.size() )
-        {
-            if (count)
-            {
-                i++;
-                require[S[i]]--;
-                if (chSet[S[i]] && require[S[i]] >= 0)
-                {
-                    count--;
+        int left(0), right(-1);
+        int minLen(INT_MAX), minIdx(0);
+        while (right < (int)S.size()) {
+            if (needed) {
+                char ch = S[++right];
+                require[ch]--;
+                if (chSet[ch] && require[ch] >= 0) {
+                    needed--;
                 }
             }
-            else
-            {
-                if (minLen > i - j + 1)
-                {
-                    minLen = i - j + 1;
-                    minIdx = j;
+            else {
+                char ch = S[left];
+                int temp_length = right-left+1;
+                if (minLen > temp_length) {
+                    minLen = temp_length;
+                    minIdx = left;
                 }
-                require[S[j]]++;
-                if (chSet[S[j]] && require[S[j]] > 0)
-                {
-                    count++;
+                require[ch]++;
+                if (chSet[ch] && require[ch] > 0) {
+                    needed++;
                 }
-                j++;
+                left++;
             }
         }
-        if (minLen == INT_MAX)
-        {
-            return "";
-        }
+        if (minLen == INT_MAX) return "";
         return S.substr(minIdx, minLen);
     }
 };
