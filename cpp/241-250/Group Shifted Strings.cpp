e6a4e81da32549af5074d82a04a23cce4f988e34
Zhenchao Gan <303542123@qq.com>
2016-08-05 21:01:42 -0700

    249. Group Shifted Strings

--- cpp/241-250/Group Shifted Strings.cpp
+++ cpp/241-250/Group Shifted Strings.cpp
@@ -2,14 +2,14 @@ class Solution {
     
     bool pattern_match(const string &s1, const string &s2)
     {
-        if(s1.length() != s2.length()) return false;
+        if (s1.length() != s2.length()) return false;
         int v(-1);
-        for(int i = 0;i < s1.length();i++)
+        for (int i = 0;i < s1.length();i++)
         {
             int temp = (s2[i]-s1[i])%26;
-            if(temp < 0) temp += 26;
-            if(v < 0) v= temp;
-            if(temp != v) return false;
+            if (temp < 0) temp += 26;
+            if (v < 0) v= temp;
+            if (temp != v) return false;
         }
         return true;
     }
@@ -18,20 +18,20 @@ public:
     vector<vector<string>> groupStrings(vector<string>& strings) {
         vector<vector<string>> result;
         
-        for(int i = 0;i < strings.size();i++)
+        for (int i = 0;i < strings.size();i++)
         {
             string cur = strings[i];
             bool match(false);
-            for(int j = 0;j < result.size();j++)
+            for (int j = 0;j < result.size();j++)
             {
-                if(pattern_match(cur, result[j][0]))
+                if (pattern_match(cur, result[j][0]))
                 {
                     result[j].push_back(cur);
                     match = true;
                     break;
                 }
             }
-            if(!match)
+            if (!match)
             {
                 vector<string> pattern;
                 pattern.push_back(cur);
@@ -39,7 +39,7 @@ public:
             }
         }
         
-        for(int i = 0;i < result.size();i++)
+        for (int i = 0;i < result.size();i++)
             sort(result[i].begin(), result[i].end());
         return result;
     }
