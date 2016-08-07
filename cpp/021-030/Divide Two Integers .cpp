6eaae90a195853ff8db835239b5cb6f2d6dd24b1
Zhenchao Gan <303542123@qq.com>
2016-08-06 22:13:02 -0700

    29. Divide Two Integers

--- cpp/021-030/Divide Two Integers .cpp
+++ cpp/021-030/Divide Two Integers .cpp
@@ -2,22 +2,26 @@ class Solution {
 public:
     int divide(long long dividend, long long divisor) {
         if(dividend == INT_MIN && divisor == -1) return INT_MAX;
-        long long result(0);
-        long long sign(1);
-        if(dividend*divisor < 0) sign = -1;
+        long long result(0), sign(1);
+        if (dividend > 0 && divisor < 0) sign = -1;
+        if (dividend < 0 && divisor > 0) sign = -1;
         dividend = abs(dividend);
         divisor = abs(divisor);
         
-        while(dividend >= divisor) {
-            long long tmp = divisor;
-            long long factor = 1;
-            
-            while(dividend >= tmp) {
-                dividend -= tmp;
-                result += factor;
-                tmp = tmp << 1;
-                factor = factor << 1;
+        long long arrays[35] = {0};
+        arrays[0] = divisor;
+        int index = 0;
+        while (arrays[index] < dividend) {
+            arrays[index+1] = arrays[index] + arrays[index];
+            index++;
+        }
+        
+        while (index >= 0) {
+            if (dividend >= arrays[index]) {
+                result += (1 << index);
+                dividend -= arrays[index];
             }
+            index--;
         }
         
         return sign*result;
