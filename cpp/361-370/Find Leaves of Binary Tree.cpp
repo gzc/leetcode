ea22aa0c22da23a2b26d400d5ffec6272bab541a
Zhenchao Gan <303542123@qq.com>
2016-08-09 12:27:15 -0700

    366. Find Leaves of Binary Tree

--- cpp/361-370/Find Leaves of Binary Tree.cpp
+++ cpp/361-370/Find Leaves of Binary Tree.cpp
@@ -10,31 +10,21 @@
 class Solution {
     
     int dfs(TreeNode* root, vector<vector<int>> &myvec) {
-        
-        if(!root) return 0;
-        
+        if (root == nullptr) return 0;
         int ldepth = dfs(root->left, myvec);
         int rdepth = dfs(root->right, myvec);
-        
         int depth = 1 + max(ldepth, rdepth);
-        
-        if(myvec.size() < depth) {
+        if (myvec.size() < depth) {
             myvec.resize(myvec.size()+1);
         }
-        
         myvec[depth-1].push_back(root->val);
-        
         return depth;
-        
     }
     
 public:
     vector<vector<int>> findLeaves(TreeNode* root) {
-        
         vector<vector<int>> myvec;
-        
         dfs(root, myvec);
-        
         return myvec;
     }
 };
