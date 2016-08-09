7d87db4c07ef8fbd32dee6d18e188c8152cc86eb
Zhenchao Gan <303542123@qq.com>
2016-08-08 22:24:56 -0700

    213. House Robber II

--- cpp/211-220/House Robber II.cpp
+++ cpp/211-220/House Robber II.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         
-        if(nums.size() == 0)
+        if (nums.size() == 0)
             return 0;
-        if(nums.size() == 1)
+        if (nums.size() == 1)
             return nums[0];
 
-        int pre1 = 0, cur1 = 0;
-        for(int i = 0; i < nums.size() - 1; ++ i)
+        int pre1(0), cur1(0);
+        for (int i = 0; i < nums.size() - 1; ++ i)
         {
             int temp = pre1;
             pre1 = cur1;
             cur1 = max(temp + nums[i], pre1);
         }
 
-        int pre2 = 0, cur2 = 0;
-        for(int i = 1; i < nums.size(); ++ i)
+        int pre2(0), cur2(0);
+        for (int i = 1; i < nums.size(); ++ i)
         {
             int temp = pre2;
             pre2 = cur2;
@@ -24,6 +24,5 @@ public:
         }
 
         return max(cur1, cur2);
-        
     }
 };
