7bda9b9ba7a3d9896d9f03d8fef11d9d4d51fa7b
Zhenchao Gan <303542123@qq.com>
2016-08-08 20:46:25 -0700

    93. Restore IP Addresses

--- cpp/091-100/Restore IP Addresses.cpp
+++ cpp/091-100/Restore IP Addresses.cpp
@@ -1,60 +1,24 @@
 class Solution {
-    
-    bool Valid(string s)
-    {
-        if(s.length() == 1){
-            return true;
-        }
-        else if(s.length() == 2){
-            if(s >= "10" && s <= "99") 
-                return true;
-        }
-        else if(s.length() == 3){
-            if(s >= "100" && s <= "255") 
-                return true;
-        }
-        return false;
-    }
-    
-    void help(string s, int level, int index, vector<string>& r, vector<string> temp)
-    {
-        
-        if(level == 4 && index == s.length()) {
-            string line = temp[0];
-            for(int i = 1;i < 4;i++)
-            {
-                line += '.';
-                line += temp[i];
-            }
-            r.push_back(line);
-        } else if(level == 4 && index < s.length()) {
-            return;
-        }
-        
-        for(int i = 0;i < 3;i++)
-        {
-            if(index + i + 1 > s.length())
-                return;
-            string tmp = s.substr(index,i+1);
-
-            if( Valid(tmp) ){
-                temp.push_back(tmp);
-                help(s, level+1, index+i+1, r, temp);
-                temp.pop_back();
-            }
-        }
-    }
-    
 public:
     vector<string> restoreIpAddresses(string s) {
-        vector<string> results;
-        vector<string> temp;
-        if(s.length() > 12 && s.length() < 4)
-            return results;
+        vector<string> ret;
+        string ans;
         
-        help(s, 0, 0, results, temp);
+        for (int a=1; a<=3; a++)
+        for (int b=1; b<=3; b++)
+        for (int c=1; c<=3; c++)
+        for (int d=1; d<=3; d++)
+            if (a+b+c+d == s.length()) {
+                int A = stoi(s.substr(0, a));
+                int B = stoi(s.substr(a, b));
+                int C = stoi(s.substr(a+b, c));
+                int D = stoi(s.substr(a+b+c, d));
+                if (A <= 255 && B <= 255 && C <= 255 && D <= 255)
+                    // 000000 => false
+                    if ( (ans=to_string(A)+"."+to_string(B)+"."+to_string(C)+"."+to_string(D)).length() == s.length()+3)
+                        ret.push_back(ans);
+            }    
         
-        return results;
+        return ret;
     }
-    
 };
