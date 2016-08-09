20787cc9d12a7922532444185eec558adc8e5168
Zhenchao Gan <303542123@qq.com>
2016-08-08 21:57:29 -0700

    283. Move Zeroes

--- cpp/281-290/Move Zeroes.cpp
+++ cpp/281-290/Move Zeroes.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int first(0);
-        for(int i = 0;i < nums.size(); i++)
-            if(nums[i] != 0) swap(nums[i],nums[first++]);
+        for (int i = 0;i < nums.size(); i++)
+            if (nums[i] != 0)
+                swap(nums[i],nums[first++]);
     }
 };
