1c10f1a3282f5ba61f0d49644f59680ce008a66c
Zhenchao Gan <303542123@qq.com>
2016-08-08 22:42:45 -0700

    162. Find Peak Element

--- cpp/161-170/Find Peak Element.cpp
+++ cpp/161-170/Find Peak Element.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
+    int findPeakElement(const vector<int> &num) 
+    {
+        int low(0), high(num.size()-1);
         
-        int i(0),j(nums.size()-1);
-        
-        while(i < j)
+        while(low < high)
         {
-            int mid = (i+j) / 2;
-            
-            if(i == mid)
-                return (nums[i] > nums[j] ? i : j);
-                
-            if(nums[mid] > nums[mid-1])
-                i = mid;
-            else
-                j = mid-1;
+            int mid1 = (low+high)/2;
+            int mid2 = mid1+1;
+            num[mid1] < num[mid2] ?  low = mid2 : high = mid1;
         }
-        
+        return low;
     }
 };
