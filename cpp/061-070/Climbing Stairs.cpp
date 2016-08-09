731aa3397aad24bbe833df6d16dffdddc8be8f02
Zhenchao Gan <303542123@qq.com>
2016-08-09 12:17:15 -0700

    70. Climbing Stairs

--- cpp/061-070/Climbing Stairs.cpp
+++ cpp/061-070/Climbing Stairs.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
+        vector<int> dp(n, 0);
+        dp[0] = 1;
+        dp[1] = 2;
         
-        int* a = new int[n];
-        a[0] = 1;
-        a[1] = 2;
-        
-        for(int i =2;i < n;i++)
+        for (int i =2;i < n;i++)
         {
-            a[i] = a[i-1]+a[i-2];
+            dp[i] = dp[i-1]+dp[i-2];
         }
         
-        return a[n-1];
-        
+        return dp[n-1];
     }
 };
