179ac748ef45a45b066dfc73ac48397009ebb220
Zhenchao Gan <303542123@qq.com>
2016-08-08 22:00:22 -0700

    1. Two Sum

--- cpp/001-010/Two Sum.cpp
+++ cpp/001-010/Two Sum.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<int> twoSum(vector<int> &numbers, int target) {
         unordered_map<int, int> m;
-        vector<int> r;
-        for(int i = 0;i < numbers.size();i++) {
+        vector<int> result;
+        for (int i = 0;i < numbers.size();i++) {
             int remain = target - numbers[i];
-            if(m.count(remain) > 0) {
-                r.push_back(m[remain]);
-                r.push_back(i);
-                return r;
+            if (m.count(remain) > 0) {
+                result.push_back(m[remain]);
+                result.push_back(i);
+                return result;
             }
             m[numbers[i]] = i;
         }
-        return r;
+        return result;
     }
 };
