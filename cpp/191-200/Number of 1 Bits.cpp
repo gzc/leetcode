d2d7fdd642aa20b02871da7559faa81179ba2507
Zhenchao Gan <303542123@qq.com>
2016-08-10 14:22:06 -0700

    191. Number of 1 Bits

--- cpp/191-200/Number of 1 Bits.cpp
+++ cpp/191-200/Number of 1 Bits.cpp
@@ -1,29 +1,11 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-
-using namespace std;
-
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        
-        int r = 0;
-        
-        while(n > 0)
-        {
+        int hamming_weight(0);
+        while (n > 0) {
             n = n & (n-1);
-            r++;
+            hamming_weight++;
         }
-        
-        return r;
+        return hamming_weight;
     }
-    
 };
-
-int main()
-{
-    Solution s;
-    
-    cout << s.hammingWeight(11);
-}
