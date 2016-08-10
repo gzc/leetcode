da0bfe8d01422c85857c2c7f2ae7fa5b2eedfcac
Zhenchao Gan <303542123@qq.com>
2016-08-09 22:12:21 -0700

    143. Reorder List

--- cpp/141-150/Reorder List.cpp
+++ cpp/141-150/Reorder List.cpp
@@ -2,8 +2,8 @@ class Solution {
     
     ListNode* reverse(ListNode *head)
     {
-        ListNode *tmp = head->next;
-        ListNode *p = nullptr;
+        ListNode* tmp = head->next;
+        ListNode* p = nullptr;
         head->next = NULL;
         while (tmp)
         {
@@ -17,12 +17,12 @@ class Solution {
     
 public:
     void reorderList(ListNode *head) {
-        if(!head || !head->next) return;
+        if (!head || !head->next) return;
         
         ListNode *head1 = head;
         ListNode *head2 = head->next;
         
-        while(head2 && head2->next) {
+        while (head2 && head2->next) {
             head1 = head1->next;
             head2 = head2->next->next;
         }
@@ -33,7 +33,7 @@ public:
         head2 = reverse(head2);
 
         ListNode *tmp = head1 -> next;
-        while(head2){
+        while (head2){
             head1 -> next = head2;
             head1 = head2;
             head2 = tmp;
