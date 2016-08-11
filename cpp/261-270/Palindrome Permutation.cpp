23536bef15716f3b820341e7830051b5adafdeb0
Zhenchao Gan <303542123@qq.com>
2016-08-10 22:58:20 -0700

    266. Palindrome Permutation

--- cpp/261-270/Palindrome Permutation.cpp
+++ cpp/261-270/Palindrome Permutation.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     bool canPermutePalindrome(string s) {
-        
-        int ch[257] = {0};
-        
-        for(int i = 0;i < s.length();i++)
-        {
+        int ch[128] = {0};
+        for (int i = 0;i < s.length();i++) 
             ch[s[i]]++;
-        }
         
         int odd(0), even(0);
-        
-        for(int i = 0;i < 256;i++)
-        {
-            if(ch[i] % 2 == 0 && ch[i] > 0) even++;
-            if(ch[i] > 0 && ch[i] % 2 == 1) odd++;
+        for (int i = 0;i < 256;i++) {
+            if (ch[i] > 0) {
+                ch[i] % 2 == 1 ? odd++ : even++;
+            }
         }
         
-        if(odd >= 2) return false;
-        if(odd == 1)
-        {
-            if(s.length() % 2 == 1) return true;
+        if (odd >= 2) return false;
+        if (odd == 1) {
+            if (s.length() % 2 == 1) return true;
             return false;
         }
         return true;
-        
     }
 };
