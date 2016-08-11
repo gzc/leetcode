a8f686504477589987187a04c559f3da7b244c52
Zhenchao Gan <303542123@qq.com>
2016-08-11 16:39:34 -0700

    252. Meeting Rooms

--- cpp/251-260/Meeting Rooms.cpp
+++ cpp/251-260/Meeting Rooms.cpp
@@ -10,7 +10,8 @@
 class Solution {
 public:
     bool canAttendMeetings(vector<Interval>& intervals) {
-        sort(intervals.begin(), intervals.end(), compare);
+        auto comp = [&](const Interval &a, const Interval &b) { return a.start < b.start; };
+        sort(intervals.begin(), intervals.end(), comp);
         int n = intervals.size();
         for (int i = 0; i < n - 1; i++)
             if (overlap(intervals[i], intervals[i + 1]))
@@ -18,9 +19,6 @@ public:
         return true;
     }
 private:
-    static bool compare(Interval& interval1, Interval& interval2) {
-        return interval1.start < interval2.start;
-    }
     bool overlap(Interval& interval1, Interval& interval2) {
         return interval1.end > interval2.start;
     }
