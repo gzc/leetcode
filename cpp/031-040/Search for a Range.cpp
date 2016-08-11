90ec1476753e49b53222ae99dc5a5f3582e61819
Zhenchao Gan <303542123@qq.com>
2016-08-11 15:35:32 -0700

    34. Search for a Range

--- cpp/031-040/Search for a Range.cpp
+++ cpp/031-040/Search for a Range.cpp
@@ -1,49 +1,17 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <stack>
-#include <queue>
-
-using namespace std;
-
 class Solution {
 public:
-    vector<int> searchRange(int A[], int n, int target) {
-        
-        int left = 0;
-        int right = n-1;
-        vector<int>result;
-        
-        while(left <= right)
-        {
-            int mid = (left+right)/2;
-            if(target < A[mid])
-                right = mid-1;
-            else if(target > A[mid])
-                left = mid+1;
-            else
-            {
-                int l(mid),r(mid);
-                while(l > 0 && A[l] == A[l-1]) l--;
-                while(r < n-1 && A[r] == A[r+1]) r++;
-                result.push_back(l);
-                result.push_back(r);
-                return result;
-            }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        std::vector<int>::iterator low,up;
+        low =std::lower_bound (nums.begin(), nums.end(), target);
+        up = std::upper_bound (nums.begin(), nums.end(), target); 
+        vector<int> result;
+        if (low == up) {
+            result.push_back(-1);
+            result.push_back(-1);
+        } else {
+            result.push_back(low - nums.begin());
+            result.push_back(up - nums.begin() - 1);
         }
-        result.push_back(-1);
-        result.push_back(-1);
         return result;
     }
-    
 };
-
-int main()
-{
-    Solution s;
-    int a[] = {5, 7, 7, 8, 8, 10};
-
-    vector<int>v = s.searchRange(a, 6, 8);
-    cout << v[0] << " " << v[1];
-    return 0;
-}
