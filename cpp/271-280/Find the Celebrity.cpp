fa662e59c5f8f690dc41d1736243b24b86810bcd
Zhenchao Gan <303542123@qq.com>
2016-08-11 15:24:57 -0700

    277. Find the Celebrity

--- cpp/271-280/Find the Celebrity.cpp
+++ cpp/271-280/Find the Celebrity.cpp
@@ -4,19 +4,16 @@ bool knows(int a, int b);
 class Solution {
 public:
     int findCelebrity(int n) {
-        int i(0),j(n-1);
+        int i(0), j(n-1);
         
-        while(i < j)
-        {
-            if(knows(i, j)) i++;
+        while (i < j) {
+            if (knows(i, j)) i++;
             else j--;
         }
         
-        for(int k = 0;k < n;k++)
-        {
-            if(k == i) continue;
-            if(!knows(k, i)) return -1;
-            if(knows(i, k)) return -1;
+        for(int k = 0;k < n;k++) {
+            if (k == i) continue;
+            if (knows(i, k) || !knows(k, i)) return -1;
         }
         return i;
     }
