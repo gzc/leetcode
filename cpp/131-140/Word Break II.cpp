a754487e0d84c916cd99b8b9fcf58e76af5cf5ea
Zhenchao Gan <303542123@qq.com>
2016-08-11 22:02:07 -0700

    140. Word Break II

--- cpp/131-140/Word Break II.cpp
+++ cpp/131-140/Word Break II.cpp
@@ -5,21 +5,20 @@ class Solution {
 public:
     
     vector<string> wordBreak(string s, unordered_set<string> &dict) {
-        if(m.count(s) > 0) return m[s];
+        if (m.count(s) > 0) return m[s];
         vector<string> words;
         int len = s.length();
-        for(int i = len-1;i >= 0;i--)
-        {
-            string last = s.substr(i,len-i);
-            unordered_set<string>::const_iterator got = dict.find (last);
-            if ( got != dict.end() ) {
-                if(i == 0)
-                    words.push_back(last);
+        for(int i = 1;i <= len;i++) {
+            string first = s.substr(0, i);
+            unordered_set<string>::const_iterator got = dict.find(first);
+            if (got != dict.end()) {
+                if(i == len)
+                    words.push_back(first);
                 else {
-                    string remain = s.substr(0,i);
+                    string remain = s.substr(i);
                     vector<string> remainVec = wordBreak(remain, dict);
-                    for(int k = 0;k < remainVec.size();k++)
-                        words.push_back(remainVec[k] + " " + last);
+                    for (int k = 0;k < remainVec.size();k++)
+                        words.push_back(first + " " + remainVec[k]);
                 }
             }
         }
