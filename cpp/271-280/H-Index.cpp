53a6b38e86466524a33d8bae6a781b6c7a17184a
Zhenchao Gan <303542123@qq.com>
2016-08-11 17:18:02 -0700

    274. H-Index

--- cpp/271-280/H-Index.cpp
+++ cpp/271-280/H-Index.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        
         sort(citations.begin(), citations.end());
-        
         int h = citations.size();
-        
-        int i = 0;
-        for(int i = 0;i < citations.size();i++)
-        {
-            if(citations[i] >= h)
+        for (int i = 0;i < citations.size();i++) {
+            if (citations[i] >= h)
                 break;
             h--;
         }
-        
         return h;
     }
 };
