64a297332491640ef809ec7fc2d262d7720740e1
Zhenchao Gan <303542123@qq.com>
2016-08-11 21:40:54 -0700

    139. Word Break

--- cpp/131-140/Word Break.cpp
+++ cpp/131-140/Word Break.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     bool wordBreak(string s, unordered_set<string> &dict) {
-        
         vector<bool> p(s.length()+1, false);
         p[0]=true;
-        
-        for(int i = 0;i < s.length();i++)
-        {
-            for(int j = 0;j <= i;j++)
-            {
-                if(p[j] == true)
-                {
+        for(int i = 0;i < s.length();i++) {
+            for(int j = 0;j <= i;j++) {
+                if(p[j] == true) {
                     string tmp = s.substr(j,i+1-j);
-                    if ( dict.find (tmp) != dict.end() )
-                    {
+                    if (dict.find (tmp) != dict.end()) {
                         p[i+1] = true;
                         break;
                     }
@@ -21,7 +15,5 @@ public:
             }
         }
         return p[s.length()];
-        
     }
-    
 };
