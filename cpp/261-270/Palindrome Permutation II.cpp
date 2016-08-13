72d77852f58d24bac702cc5bdd44e148b4e3416d
Zhenchao Gan <303542123@qq.com>
2016-08-12 21:24:16 -0700

    267. Palindrome Permutation II

--- cpp/261-270/Palindrome Permutation II.cpp
+++ cpp/261-270/Palindrome Permutation II.cpp
@@ -1,10 +1,10 @@
 class Solution {
     
-    const static int M = 256;
+    const static int M = 128;
     
     int chs[M] = {0};
     
-    void help(vector<string>& result, string temp, string &s, int cur)
+    void help(vector<string>& result, string &temp, const string &s, int cur)
     {
         int mid = s.length() / 2;
         if(s.length() % 2 == 0 && cur == mid) { result.push_back(temp); return;}
@@ -33,22 +33,20 @@ class Solution {
 public:
     vector<string> generatePalindromes(string s) {
         vector<string> result;
-        for(int i = 0;i < s.length();i++)
+        for (int i = 0;i < s.length();i++)
             chs[s[i]]++;
         
         int odd(0),even(0);
-        for(int i = 0;i < M;i++)
-        {
+        for (int i = 0;i < M;i++) {
             if(chs[i] == 0) continue;
             if(chs[i] % 2 == 0) even++;
             else odd++;
         }
         
-        if(odd >= 2) return result;
-        if(s.length() % 2 == 0 && odd > 0) return result;
+        if (odd >= 2) return result;
+        if (s.length() % 2 == 0 && odd > 0) return result;
         
         string tmp(s.length(), 0);
-        
         help(result, tmp, s, 0);
         return result;
     }
