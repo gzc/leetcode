e616b0136003ef29f21af864032b2e8fe20a86d1
Zhenchao Gan <303542123@qq.com>
2016-08-12 21:34:48 -0700

    118. Pascal's Triangle

--- cpp/111-120/Pascal's Triangle.cpp
+++ cpp/111-120/Pascal's Triangle.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     vector<vector<int> > generate(int numRows) {
-        
         vector<vector<int> > result;
         if (numRows <= 0) return result;        
-
         for(int i = 0; i < numRows; i++) {
             vector<int> row(i+1);
             row[0] = row[i] = 1;
-
             for(int j = 1; j < i; j++) 
                 row[j] = result[i-1][j-1] + result[i-1][j];
-
             result.push_back(row);
         }
+        return result;
     }
 };
