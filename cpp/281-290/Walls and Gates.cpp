5457ab03d946ddee30198d43e99c268e626054af
Zhenchao Gan <303542123@qq.com>
2016-08-13 16:54:34 -0700

    286. Walls and Gates

--- cpp/281-290/Walls and Gates.cpp
+++ cpp/281-290/Walls and Gates.cpp
@@ -1,21 +1,15 @@
-struct point
-{
+struct point {
     int i;
     int j;
     int v;
-    point(int _i, int _j, int _v):i(_i),j(_j),v(_v){}
+    point(int _i, int _j, int _v):i(_i), j(_j), v(_v) { }
 };
 
-class mycomparison
-{
-    bool reverse;
+class mycomparison {
 public:
-    mycomparison(const bool& revparam=false)
-    {reverse=revparam;}
-    bool operator() (const struct point& lhs, const struct point &rhs) const
-    {
-        if (reverse) return (lhs.v > rhs.v);
-        else return (lhs.v < rhs.v);
+    mycomparison() { }
+    bool operator() (const point& lhs, const point &rhs) const {
+        return lhs.v > rhs.v;
     }
 };
 
@@ -25,41 +19,32 @@ class Solution {
     
 public:
     void wallsAndGates(vector<vector<int>>& rooms) {
-        
-        typedef priority_queue<point, vector<point>, mycomparison> mytype;
-        mytype myqueue(mycomparison(true));
+        priority_queue<point, vector<point>, mycomparison> myqueue;
         
         int height = rooms.size();
-        if(height == 0) return;
+        if (height == 0) return;
         int width = rooms[0].size();
-        if(width == 0) return;
+        if (width == 0) return;
         
-        for(int j = 0;j < height;j++)
-            for(int i = 0;i < width;i++)
-                if(rooms[j][i] == 0)
+        for (int j = 0;j < height;j++)
+            for (int i = 0;i < width;i++)
+                if (rooms[j][i] == 0)
                     myqueue.push(point(i, j, 0));
         
-        while(!myqueue.empty())
-        {
-            struct point p = myqueue.top();
+        while (!myqueue.empty()) {
+            point p = myqueue.top();
             myqueue.pop();
-            
-            for(int m = 0;m <= 3;m++)
-            {
+            for (int m = 0;m <= 3;m++) {
                 int ox = p.i + cors[m].first;
                 int oy = p.j + cors[m].second;
                 int ov = p.v;
-                if(ox >= 0 && ox < width && oy >= 0 && oy < height)
-                {
-                    if(rooms[oy][ox] == INT_MAX)
-                    {
+                if (ox >= 0 && ox < width && oy >= 0 && oy < height) {
+                    if(rooms[oy][ox] == INT_MAX) {
                         rooms[oy][ox] = p.v + 1;
                         myqueue.push(point(ox,oy,rooms[oy][ox]));
                     }
                 }
             }
-            
         }
-        
     }
 };
