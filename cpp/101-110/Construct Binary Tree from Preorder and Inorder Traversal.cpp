1e77390090a12f248085439d380eb264e830305b
Zhenchao Gan <303542123@qq.com>
2016-08-13 09:58:29 -0700

    105. Construct Binary Tree from Preorder and Inorder Traversal

--- cpp/101-110/Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ cpp/101-110/Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -1,40 +1,18 @@
 class Solution {
-    
-    TreeNode *help(vector<int>::iterator first, vector<int>::iterator last, vector<int>::iterator pfirst, vector<int>::iterator plast)
-    {
-        int mid = *pfirst;
-        TreeNode *node = new TreeNode(mid);
-        
-        if(first == last)
-            return node;
-        
-        vector<int>::iterator inorder_min = find(first, last, mid);
-        vector<int>::iterator porder_break;
-        TreeNode *left,*right;
-        if(inorder_min == first)
-        {
-            left = NULL;
-            right = help(first+1, last, pfirst+1, plast);
-        } else if(inorder_min == last)
-        {
-            right = NULL;
-            left = help(first,last-1,pfirst+1,plast);
-        } else
-        {
-            int diff = inorder_min-first;
-            left = help(first, inorder_min-1, pfirst+1, pfirst+diff);
-            right = help(inorder_min+1, last, pfirst+diff+1,plast);
-        }
 
-        node->left = left;
-        node->right = right;
-        return node;
+TreeNode* helper(vector<int>& preorder,int i,int j,vector<int>& inorder,int ii,int jj) {
+        if(i >= j) return nullptr;
+        int mid = preorder[i];
+        auto f = find(inorder.begin() + ii,inorder.begin() + jj,mid);
+        int dis = f - inorder.begin() - ii;
+        TreeNode* root = new TreeNode(mid);
+        root -> left = helper(preorder,i + 1,i + 1 + dis,inorder,ii,ii + dis);
+        root -> right = helper(preorder,i + 1 + dis,j,inorder,ii + dis + 1,jj);
+        return root;
     }
-    
+
 public:
-    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
-        if(inorder.size() == 0)
-            return NULL;
-        return help(inorder.begin(), inorder.end()-1, preorder.begin(), preorder.end()-1);
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        return helper(preorder,0,preorder.size(),inorder,0,inorder.size());
     }
 };
