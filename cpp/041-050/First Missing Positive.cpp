1051a3a839667d22da5d8ba09286246b66fe8bcb
Zhenchao Gan <303542123@qq.com>
2016-08-14 14:57:46 -0700

    41. First Missing Positive

--- cpp/041-050/First Missing Positive.cpp
+++ cpp/041-050/First Missing Positive.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& A) {
-        
-        if(A.empty()) return 1;
+    int firstMissingPositive(vector<int>& nums) {
+        if (nums.empty()) return 1;
         int i(0);
         
-        while(i < A.size()) {
-            if(A[i] != i+1 && A[i] > 0 && A[i] <= A.size() && A[i] != A[A[i]-1]) {
-                swap(A[i], A[A[i]-1]);
+        while (i < nums.size()) {
+            if (nums[i] != i+1 && nums[i] > 0 && nums[i] < nums.size() && nums[i] != nums[nums[i]-1]) {
+                swap(nums[i], nums[nums[i]-1]);
                 continue;
             }
             i++;
         }
         
-        for(int i = 0;i < A.size();i++)
-            if(A[i] != i+1)
+        for (int i = 0;i < nums.size();i++)
+            if(nums[i] != i+1)
                 return i+1;
-        return A.back()+1;
+        return nums.back()+1;
     }
 };
