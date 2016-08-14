42289a25048a2f77d6d66046d7e61ad530a427e9
Zhenchao Gan <303542123@qq.com>
2016-08-14 09:21:52 -0700

    98. Validate Binary Search Tree

--- cpp/091-100/Validate Binary Search Tree.cpp
+++ cpp/091-100/Validate Binary Search Tree.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        
         stack<TreeNode*>s;
         TreeNode *p = root;
         int prev(INT_MIN);
         bool first(true);
         
-        while(p || !s.empty())
-        {
-            while(p)
-            {
+        while (p!= nullptr || !s.empty()) {
+            while (p != nullptr) {
                 s.push(p);
                 p = p->left;
             }
-                
             p = s.top();
             s.pop();
-            if(first) {
+            if (first) {
                 prev = p->val;
                 first = false;
+            } else if (p->val <= prev) {
+                return false;
             }
-            else if(p->val <= prev) return false;
             prev = p->val;
             p = p->right;
         }
-        
         return true;
     }
 };
