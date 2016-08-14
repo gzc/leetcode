b79a89c59649c0fa41001b6c1b8e3cb60135b493
Zhenchao Gan <303542123@qq.com>
2016-08-14 15:15:18 -0700

    287. Find the Duplicate Number

--- cpp/281-290/Find the Duplicate Number.cpp
+++ cpp/281-290/Find the Duplicate Number.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        
         int n = nums.size();
-        int left = 1, right = n;
-        
-        while(left < right)
-        {
+        int left(1), right(n);
+        while (left < right) {
             int mid = (left+right)/2;
-            
-            int less(0),large(0), e(0);
-            for(int i = 0;i < n;i++)
-            {
-                if(nums[i] < mid) less++;
-                else if(nums[i] > mid) large++;
-                else e++;
+            int less(0), duplicate(0);
+            for (int i = 0;i < n;i++) {
+                if (nums[i] < mid) less++;
+                else if(nums[i] == mid) duplicate++;
             }
-            if(e > 1) return mid;
-            if(less > mid - 1) right = mid - 1;
-            else left = mid + 1;
+            if (duplicate > 1) return mid;
+            less > mid - 1 ? right = mid - 1 : left = mid + 1;
         }
-        
+        return left;
     }
-    
 };
