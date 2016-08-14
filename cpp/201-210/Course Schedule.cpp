2a19ac2bc8899356ec6fde90811078f2a63efafd
Zhenchao Gan <303542123@qq.com>
2016-08-13 17:15:59 -0700

    207. Course Schedule

--- cpp/201-210/Course Schedule.cpp
+++ cpp/201-210/Course Schedule.cpp
@@ -1,25 +1,21 @@
 struct Node {
     int state;
     vector<int> edges;
-    Node(int x) : state(x){}
+    Node(int x) : state(x) { }
 };
  
 class Solution {
-    
+
     vector<struct Node>  Nodes;
     
-    bool DFS_VISIT(int u)
-    {
+    bool dfs_visit(int u) {
         Nodes[u].state = 1;
-        for(int i = 0;i < Nodes[u].edges.size();i++)
-        {
+        for (int i = 0;i < Nodes[u].edges.size();i++) {
             int num = Nodes[u].edges[i];
-            if(Nodes[num].state == 0)
-            {
-                bool fff = DFS_VISIT(num);
+            if(Nodes[num].state == 0) {
+                bool fff = dfs_visit(num);
                 if(!fff) return false;
-            } else if(Nodes[num].state == 1)
-            {
+            } else if(Nodes[num].state == 1) {
                 return false;
             }
         }
@@ -28,24 +24,20 @@ class Solution {
     }
     
 public:
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
-        for(int i = 0;i < numCourses;i++)
+    bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
+        Nodes.clear();
+        for (int i = 0;i < numCourses;i++)
             Nodes.push_back(Node(0));
+        for (int i = 0;i < prerequisites.size();i++)
+            Nodes[prerequisites[i].first].edges.push_back(prerequisites[i].second);
         
-        for(int i = 0;i < prerequisites.size();i++)
-            Nodes[prerequisites[i][0]].edges.push_back(prerequisites[i][1]);
-        
-        for(int i = 0;i < numCourses;i++)
-        {
+        for (int i = 0;i < numCourses;i++) {
             vector<int> edges = Nodes[i].edges;
             if(edges.empty()) continue;
             if(Nodes[i].state != 0) continue;
-            bool fff = DFS_VISIT(i);
+            bool fff = dfs_visit(i);
             if(!fff) return false;
         }
-        
         return true;
     }
-    
 };
