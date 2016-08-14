7482fd6f33c94ed616a0e3ca74c4a7003ce59203
Zhenchao Gan <303542123@qq.com>
2016-08-14 09:36:34 -0700

    99. Recover Binary Search Tree

--- cpp/091-100/Recover Binary Search Tree.cpp
+++ cpp/091-100/Recover Binary Search Tree.cpp
@@ -1,43 +1,32 @@
 class Solution {
 public:
     void recoverTree(TreeNode* root) {
-        
         stack<TreeNode*>s;
         TreeNode *p = root;
-        TreeNode *prev(NULL);
+        TreeNode *prev(nullptr);
         bool fff(false);
+        TreeNode *n1(nullptr), *n2(nullptr);
         
-        TreeNode *n1(NULL),*n2(NULL),*n3(NULL);
-        
-        while(p || !s.empty())
-        {
-            while(p)
-            {
+        while (p || !s.empty()) {
+            while (p) {
                 s.push(p);
                 p = p->left;
             }
-                
             p = s.top();
             s.pop();
-
-            if(prev && prev->val > p->val)
-            {
-                if(!fff){
+            if (prev && prev->val > p->val) {
+                n2 = p;
+                if (!fff){
                     fff = true;
                     n1 = prev;
-                    n3 = p;
                 } else {
-                    n2 = p;
                     swap(n1->val, n2->val);
                     return;
                 }
             }
-            
             prev = p;
             p = p->right;
         }
-        
-        if(!n2) swap(n1->val, n3->val);
-        
+        swap(n1->val, n2->val);
     }
 };
