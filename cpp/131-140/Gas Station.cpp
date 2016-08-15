194713c018e1c3867c4ebe4dab80fc6cb1ef047f
Zhenchao Gan <303542123@qq.com>
2016-08-14 17:14:17 -0700

    134. Gas Station

--- cpp/131-140/Gas Station.cpp
+++ cpp/131-140/Gas Station.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        
         int totalSum(0), start(0), now(0);
-        
-        for(int i = 0;i < gas.size();i++)
-        {
+        for (int i = 0;i < gas.size();i++) {
             int tmp = gas[i] - cost[i];
             totalSum += tmp;
             now += tmp;
-            if(now < 0)
-            {
+            if(now < 0) {
                 start = i+1;
                 now = 0;
             }
         }
-        
         return totalSum >= 0 ? start : -1;
     }
 };
