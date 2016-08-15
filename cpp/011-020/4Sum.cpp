886941520b6104c8ceb90bc901404c525edacdfc
Zhenchao Gan <303542123@qq.com>
2016-08-15 08:28:03 -0700

    18. 4Sum

--- cpp/011-020/4Sum.cpp
+++ cpp/011-020/4Sum.cpp
@@ -1,36 +1,18 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-
-using namespace std;
-
 class Solution {
     
 public:
     vector<vector<int> > fourSum(vector<int> &num, int target) {
-        
         vector<vector<int>>result;
-        if(num.size() < 4) return result;
-        
+        if (num.size() < 4) return result;
         sort(num.begin(), num.end());
         
-        int buf1(INT_MAX),buf2(INT_MAX),buf3(INT_MAX),buf4(INT_MAX);
-        
-        for(int i = 0;i < num.size()-3;)
-        {
-            
-            for(int j = i+1;j < num.size()-2;)
-            {
-                
+        for (int i = 0;i < num.size()-3;) {
+            for (int j = i+1;j < num.size()-2;) {
                 int l(j+1), r(num.size()-1);
                 int remain = num[i] + num[j];
-                
-                while(l < r)
-                {
+                while(l < r) {
                     int tmp = num[l] + num[r];
-                    
-                    if(tmp + remain == target)
-                    {
+                    if(tmp + remain == target) {
                         int a[4] = {num[i], num[j], num[l], num[r]};
                         vector<int>v(a, a+4);
                         result.push_back(v);
@@ -39,26 +21,11 @@ public:
                     }
                     else if(tmp + remain > target) do {r--;}while(l < r && num[r+1] == num[r]);
                     else do {l++;}while(l < r && num[l-1] == num[l]);
-                    
                 }
-                
                 do {j++;}while(j < num.size() && num[j-1] == num[j]);
             }
-            
             do {i++;}while(i < num.size() && num[i-1] == num[i]);
         }
-        
         return result;
     }
-    
 };
-
-int main()
-{
-    Solution s;
-    int a[] = {-1,0,1,2,-1,-4};
-    vector<int> v(a, a+6);
-    cout << s.fourSum(v, -1).size();
-    
-    return 0;
-}
