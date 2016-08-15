81ea9a30584486ec394213964cdb242745fd9650
Zhenchao Gan <303542123@qq.com>
2016-08-14 17:11:25 -0700

    260. Single Number III

--- cpp/251-260/Single Number III.cpp
+++ cpp/251-260/Single Number III.cpp
@@ -4,8 +4,7 @@ public:
         int diff = accumulate(nums.begin(), nums.end(), 0, bit_xor<int>());
         diff &= -diff;
         vector<int> res(2, 0);
-        for(int e : nums)
-        {
+        for (int e : nums) {
             if(e & diff)
                 res[0] ^= e;
             else
