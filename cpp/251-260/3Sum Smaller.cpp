acbea30221d68fe6e5bb436482b3b114bcb16eea
Zhenchao Gan <303542123@qq.com>
2016-08-15 08:23:48 -0700

    259. 3Sum Smaller

--- cpp/251-260/3Sum Smaller.cpp
+++ cpp/251-260/3Sum Smaller.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int threeSumSmaller(vector<int>& nums, int target) {
-        if(nums.size() < 3) return 0;
+        if (nums.size() < 3) return 0;
         sort(nums.begin(), nums.end());
         int result(0);
-        for(int i = 0;i < nums.size()-2;i++)
-        {
-            int j(i+1),k(nums.size()-1);
+        for (int i = 0;i < nums.size()-2;i++) {
+            int j(i+1), k(nums.size()-1);
             int remain = target - nums[i];
-            
-            while(j < k)
-            {
-                if(nums[j] + nums[k] >= remain)
-                {
+            while(j < k) {
+                if(nums[j] + nums[k] >= remain) {
                     k--;
                 } else {
                     result += (k - j);
@@ -20,7 +16,6 @@ public:
                 }
             }
         }
-        
         return result;
     }
 };
