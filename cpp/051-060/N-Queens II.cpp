4802c4fbacba804b4550701ceb660ec1c6952a85
Zhenchao Gan <303542123@qq.com>
2016-08-15 22:10:47 -0700

    52. N-Queens II

--- cpp/051-060/N-Queens II.cpp
+++ cpp/051-060/N-Queens II.cpp
@@ -5,25 +5,20 @@ class Solution {
     bool *diff;
     int n;
     
-    bool valid(int i, int j)
-    {
-        if(!cols[j] && !add[i+j] && !diff[i-j+n])
+    bool valid(int i, int j) {
+        if (!cols[j] && !add[i+j] && !diff[i-j+n])
             return true;
         return false;
     }
     
-    void help(int i, int &res)
-    {
-        if(i == n)
-        {
+    void help(int i, int &res) {
+        if (i == n) {
             res++;
             return;
         }
 
-        for(int j = 0;j < n;j++)
-        {
-            if(valid(i, j))
-            {
+        for (int j = 0;j < n;j++) {
+            if(valid(i, j)) {
                 cols[j] = add[i+j] = diff[i-j+n] = true;
                 help(i+1, res);
                 cols[j] = add[i+j] = diff[i-j+n] = false;
@@ -33,13 +28,14 @@ class Solution {
     
 public:
     int totalNQueens(int n) {
-
         cols = new bool[n];
         add = new bool[2*n];
         diff = new bool[2*n];
+        memset(cols, 0, n);
+        memset(add, 0, 2*n);
+        memset(diff, 0, 2*n);
         this->n = n;
         int res = 0;
-        
         help(0, res);
         return res;
     }
