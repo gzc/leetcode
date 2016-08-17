5e5ecaf02b3ff69b4da8a293aa8a6f8c012333a1
Zhenchao Gan <303542123@qq.com>
2016-08-16 22:36:33 -0700

    32. Longest Valid Parentheses

--- cpp/031-040/Longest Valid Parentheses.cpp
+++ cpp/031-040/Longest Valid Parentheses.cpp
@@ -1,53 +1,18 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <stack>
-#include <queue>
-
-using namespace std;
-
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        
-        int *p = new int[s.size()];
-        memset(p, 0, sizeof(int)*s.size());
-        stack<pair<char, int> >_stack;
-        
-        for(int i = 0;i < s.size();i++)
-        {
-            char c = s[i];
-            if(_stack.empty()) _stack.push(make_pair(c, i));
-            else {
-                if(c == '(') _stack.push(make_pair(c, i));
-                else if(_stack.top().first == '(') {
-                    pair<char, int> tmp = _stack.top();
-                    _stack.pop();
-                    p[tmp.second] = 1;
-                    p[i] = 1;
-                }
+        stack<int> stk;
+        stk.push(-1);
+        int maxL(0);
+        for (int i = 0;i < s.size();i++) {
+            int t = stk.top();
+            if (t != -1 && s[i] == ')' && s[t]=='(') {
+                stk.pop();
+                maxL = max(maxL,i-stk.top());
             }
+            else
+                stk.push(i);
         }
-        
-        int r(0);
-        for(int i = 1;i < s.size();i++)
-        {
-            if(p[i] > 0)
-                p[i] = p[i]+p[i-1];
-            r = max(r, p[i]);
-        }
-        
-        return r;
-        
+        return maxL;
     }
-    
-    
 };
-
-int main()
-{
-    Solution s;
-
-    cout << s.longestValidParentheses("(()");
-    return 0;
-}
