3fb250761c2383c54fbb8b48a80f849d68582685
Zhenchao Gan <303542123@qq.com>
2016-08-18 21:50:04 -0700

    190. Reverse Bits

--- cpp/181-190/Reverse Bits.cpp
+++ cpp/181-190/Reverse Bits.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        
-        uint32_t r(0);
-        
-        for(int i = 0;i < 32;i++)
-            r += ((n >> i) & 1) * (1 << (31-i));
-        
-        return r;
+        uint32_t result(0);
+        for(int i = 0; i < 32; i++)
+            result = (result<<1) + (n>>i &1);
+        return result;
     }
-    
 };
