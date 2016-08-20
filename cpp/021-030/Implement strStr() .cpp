8e80af28878fa8b28d5547de3d7b671dc7178735
Zhenchao Gan <303542123@qq.com>
2016-08-20 14:22:44 -0700

    28. Implement strStr()

--- cpp/021-030/Implement strStr() .cpp
+++ cpp/021-030/Implement strStr() .cpp
@@ -1,49 +1,15 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <stack>
-#include <queue>
-
-using namespace std;
-
 class Solution {
 public:
-    
-    int BF_match(char *text,char *pattern)
-    {
-        size_t n = strlen(text);
-        size_t m = strlen(pattern);
-        int i;
-        int end = n-m;
-        
-        for(i = 0; i <= end; i++)
-        {
-            int j;
-            char *p_text = text + i;
-            for(j = 0;j < m;j++)
-            {
-                if(*(p_text+j) == *(pattern+j))
-                    continue;
-                else
+    int strStr(string haystack, string needle) {
+        if (needle.empty()) return 0;
+        int m = haystack.length(), n = needle.length();
+        for (int i = 0; i < m - n + 1; i++) {
+            int j = 0;
+            for (; j < n; j++)
+                if (haystack[i + j] != needle[j])
                     break;
-            }
-            if(j == m)
-                return i;
+            if (j == n) return i;
         }
         return -1;
     }
-    
-    int strStr(char *haystack, char *needle) {
-        return BF_match(haystack, needle);
-    }
-    
 };
-
-int main()
-{
-    Solution s;
-
-    cout << s.BF_match("hddskah", "ska");
-    
-    return 0;
-}
