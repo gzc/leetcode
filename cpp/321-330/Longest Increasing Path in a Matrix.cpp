1b59cc4eb94d81056b8e56691f5d69b0573047c5
Zhenchao Gan <303542123@qq.com>
2016-08-20 22:53:02 -0700

    329. Longest Increasing Path in a Matrix

--- cpp/321-330/Longest Increasing Path in a Matrix.cpp
+++ cpp/321-330/Longest Increasing Path in a Matrix.cpp
@@ -4,7 +4,7 @@ class Solution {
         
         pair<int, int> pos[4] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
         
-        for(int k = 0;k < 4;k++) {
+        for (int k = 0;k < 4;k++) {
             int ni = i + pos[k].first;
             int nj = j + pos[k].second;
             if(ni < 0 || nj < 0 || ni >= matrix.size() || nj >= matrix[0].size()) continue;
@@ -20,21 +20,17 @@ class Solution {
 public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         int m = matrix.size();
-        if(m == 0) return 0;
+        if (m == 0) return 0;
         int n = matrix[0].size();
         vector<vector<bool>> visited(m,vector<bool>(n, false));
         vector<vector<int>> lens(m,vector<int>(n, 1));
         
-        int ri(0), rj(0), _max(0);
-        for(int i = 0;i < m;i++) {
-            for(int j = 0;j < n;j++) {
-                if(!visited[i][j]) {
+        int _max(0);
+        for (int i = 0;i < m;i++) {
+            for (int j = 0;j < n;j++) {
+                if (!visited[i][j]) {
                     dfsvisit(i, j, visited, lens, matrix);
-                }
-                if(lens[i][j] > _max) {
-                    _max = lens[i][j];
-                    ri = i;
-                    rj = j;
+                    _max = max(_max, lens[i][j]);
                 }
             }
         }
