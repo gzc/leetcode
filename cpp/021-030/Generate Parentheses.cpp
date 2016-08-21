30f6fe16ec93cf990282419e5cde6f0c376684ae
Zhenchao Gan <303542123@qq.com>
2016-08-20 17:24:58 -0700

    22. Generate Parentheses

--- cpp/021-030/Generate Parentheses.cpp
+++ cpp/021-030/Generate Parentheses.cpp
@@ -1,63 +1,25 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <stack>
-
-using namespace std;
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
 class Solution {
     
-    vector<string> r;
-    
-    void help(string s, int n, int m)
-    {
+    void help(string s, int n, int m, vector<string>& result) {
         if(m < n) return;
         
-        if(n == 0)
-        {
-            for(int i = 0;i < m;i++)
-                s += ")";
-            r.push_back(s);
-        }
-        
-        else
-        {
-            if(m == n) help(s+"(", n-1, m);
-            else
-            {
-                help(s+"(", n-1, m);
-                help(s+")", n, m-1);
+        if(n == 0) {
+            string temp(m, ')');
+            s += temp;
+            result.push_back(s);
+        } else {
+            if(m == n) help(s+"(", n-1, m, result);
+            else {
+                help(s+"(", n-1, m, result);
+                help(s+")", n, m-1, result);
             }
         }
     }
     
-    
 public:
     vector<string> generateParenthesis(int n) {
-        
-        help("", n, n);
-        return r;
+        vector<string> result;
+        help("", n, n, result);
+        return result;
     }
 };
-
-int main()
-{
-    Solution s;
-    
-    vector<string>v = s.generateParenthesis(3);
-    
-    for(int i = 0;i < v.size();i++)
-    {
-        cout << v[i] << endl;
-    }
-    cout << v.size();
-    
-    
-    return 0;
-}
