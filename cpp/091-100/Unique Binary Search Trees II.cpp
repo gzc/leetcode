a8cf3d157f44b6b6e3c02e7ae762c6e897e56bad
Zhenchao Gan <303542123@qq.com>
2016-08-21 22:14:54 -0700

    95. Unique Binary Search Trees II

--- cpp/091-100/Unique Binary Search Trees II.cpp
+++ cpp/091-100/Unique Binary Search Trees II.cpp
@@ -1,34 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
     
-    vector<TreeNode *> generate(int beg, int end)
-    {
+    vector<TreeNode *> generate(int beg, int end) {
         vector<TreeNode* > ret;
-        if (beg > end)
-        {
-            ret.push_back(NULL);
+        if (beg > end) {
+            ret.push_back(nullptr);
             return ret;
         }
         
-        for(int i = beg; i <= end; i++)
-        {
+        for (int i = beg; i <= end; i++) {
             vector<TreeNode* > leftTree = generate(beg, i - 1);
             vector<TreeNode* > rightTree = generate(i + 1, end);
-            for(int j = 0; j < leftTree.size(); j++)
-                for(int k = 0; k < rightTree.size(); k++)
-                {
-                    TreeNode *node = new TreeNode(i + 1);
+            for (int j = 0; j < leftTree.size(); j++) {
+                for (int k = 0; k < rightTree.size(); k++) {
+                    TreeNode *node = new TreeNode(i);
                     ret.push_back(node);
                     node->left = leftTree[j];
                     node->right = rightTree[k];              
-                }           
+                }
+            }
         }
-        
         return ret;
     }
 
 public:
 
     vector<TreeNode *> generateTrees(int n) {
-        return generate(0, n - 1);
+        if (n == 0) return vector<TreeNode *>();
+        return generate(1, n);
     }
 };
