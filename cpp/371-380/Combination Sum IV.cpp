2bbc259b8960f136de6d3116f48c5261b1ecd674
Zhenchao Gan <303542123@qq.com>
2016-08-21 22:33:07 -0700

    377. Combination Sum IV

--- cpp/371-380/Combination Sum IV.cpp
+++ cpp/371-380/Combination Sum IV.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        
         vector<int> dp(target+1, 0);
         dp[0] = 1;
         
-        for(int i = 1;i <= target;i++) {
-            for(int k = 0;k < nums.size();k++) {
-                if(i - nums[k] >= 0) {
+        for (int i = 1;i <= target;i++)
+            for (int k = 0;k < nums.size();k++)
+                if (i - nums[k] >= 0)
                     dp[i] += dp[i - nums[k]];
-                }
-            }
-        }
         
         return dp[target];
     }
