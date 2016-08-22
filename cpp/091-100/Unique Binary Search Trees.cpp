3e4f9e0664dade986e0e3c12985836a37a8fa4a9
Zhenchao Gan <303542123@qq.com>
2016-08-21 21:56:44 -0700

    96. Unique Binary Search Trees

--- cpp/091-100/Unique Binary Search Trees.cpp
+++ cpp/091-100/Unique Binary Search Trees.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int numTrees(int n) {
+        vector<int> dp(n+1, 0);
+        dp[0] = dp[1] = 1;
         
-        int *p = new int[n+1];
-        memset(p, 0, 4*(n+1));
-        p[0] = p[1] = 1;
+        for (int i = 2;i <= n;i++)
+            for (int k = 0;k < n;k++)
+                dp[i] += dp[k]*dp[i-k-1];
         
-        for(int i = 2;i <= n;i++)
-            for(int k = 0;k < n;k++)
-                p[i] += p[k]*p[i-k-1];
-        
-        return p[n];
+        return dp[n];
     }
 };
