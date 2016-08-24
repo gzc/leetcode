026efa72f686e5628ff4b78b7e52922d01051738
Zhenchao Gan <303542123@qq.com>
2016-08-24 11:44:03 -0700

    47. Permutations II

--- cpp/041-050/Permutations II.cpp
+++ cpp/041-050/Permutations II.cpp
@@ -1,49 +1,23 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <stack>
-#include <queue>
-
-using namespace std;
-
 class Solution {
     
-    void help(vector<int> v, vector<vector<int>>& result, int n, vector<int> num)
-    {
-        if(v.size() == n) {
-            result.push_back(v);
+    void help(vector<vector<int>>& result, int begin, vector<int> num) {
+        if (begin == num.size()) {
+            result.emplace_back(num);
             return;
         }
-        
-        int i = 0;
-        int j = num.size();
-        while(i < j)
-        {
-            int tmp = num[i];
-            v.push_back(num[i]);
-            num.erase(num.begin()+i);
-            help(v, result, n, num);
-            num.insert(num.begin()+i, tmp);
-            v.pop_back();
-            while(i < j && num[i] == tmp) i++;
+ 
+        for (int i = begin; i < num.size();i++) {
+            if (i != begin && num[i] == num[begin]) continue;
+            swap(num[begin], num[i]);
+            help(result, begin+1, num);
         }
     }
     
 public:
     vector<vector<int> > permuteUnique(vector<int> &num) {
-        vector<int> v;
         vector<vector<int>> result;
         sort(num.begin(), num.end());
-        help(v, result, num.size(), num);
+        help(result, 0, num);
         return result;
     }
-    
 };
-
-int main()
-{
-    Solution s;
-    int a[] = {1,2,3};
-    
-    return 0;
-}
