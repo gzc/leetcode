4aa50e069fe3083690503d9864083d4fc30b942b
Zhenchao Gan <303542123@qq.com>
2016-08-25 21:03:35 -0700

    296. Best Meeting Point

--- cpp/291-300/Best Meeting Point.cpp
+++ cpp/291-300/Best Meeting Point.cpp
@@ -3,29 +3,25 @@ class Solution {
 public:
     int minTotalDistance(vector<vector<int>>& grid) {
         int result(0);
-        vector<int>vx,vy;
+        vector<int>vx, vy;
         
-        for(int i = 0;i < grid.size();i++)
-        {
-            for(int j = 0;j < grid[i].size();j++)
-            {
-                if(grid[i][j] == 1)
-                {
+        for (int i = 0;i < grid.size();i++) {
+            for (int j = 0;j < grid[i].size();j++) {
+                if (grid[i][j] == 1) {
                     vx.push_back(j);
                     vy.push_back(i);
                 }
             }
         }
         
-        sort(vx.begin(), vx.end());
-        sort(vy.begin(), vy.end());
-        int midx = vx[vx.size()/2];
-        int midy = vy[vy.size()/2];
+        int half_num = vx.size()/2;
+        std::nth_element (vx.begin(), vx.begin()+half_num, vx.end());
+        std::nth_element (vy.begin(), vy.begin()+half_num, vy.end());
+        int midx = vx[half_num];
+        int midy = vy[half_num];
         
-        for(int i = 0;i < vx.size();i++)
-        {
+        for (int i = 0;i < vx.size();i++)
             result += abs(vx[i] - midx) + abs(vy[i] - midy);
-        }
         
         return result;
     }
