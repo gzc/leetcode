f32ace8426f682ea6984141468394e7fc6422e5c
Zhenchao Gan <303542123@qq.com>
2016-08-25 20:55:30 -0700

    187. Repeated DNA Sequences

--- cpp/181-190/Repeated DNA Sequences.cpp
+++ cpp/181-190/Repeated DNA Sequences.cpp
@@ -1,12 +1,11 @@
 class Solution {
     
 private:
-    int change(char ch)
-    {
-        if(ch == 'A') return 1;
-        if(ch == 'C') return 2;
-        if(ch == 'G') return 3;
-        return 4;
+    inline int change(char ch) {
+        if (ch == 'A') return 0;
+        if (ch == 'C') return 1;
+        if (ch == 'G') return 2;
+        return 3;
     }
 
 public:
@@ -17,27 +16,21 @@ public:
         
         int tmp(0);
         
-        for(int i = 0;i < 10;i++)
-        {
-            tmp *= 5;
+        for(int i = 0;i < 10;i++) {
+            tmp *= 4;
             tmp += change(s[i]);
         }
+        m[tmp] = 1;
 
-        
-        for(int i = 0;i <= len-10;i++)
-        {
-            if(i != 0){
-                tmp = tmp - change(s[i-1])*1953125;
-                tmp *= 5;
-                tmp += change(s[i+9]);
-            }
-            if(m[tmp] == -1) continue;
+        for (int i = 1; i <= len-10; i++) {
+            tmp = tmp - change(s[i-1])*262144;
+            tmp *= 4;
+            tmp += change(s[i+9]);
+            if (m[tmp] == -1) continue;
             else if(m[tmp] == 1) { results.push_back(s.substr(i,10)); m[tmp] = -1; }
             else m[tmp] = 1;
         }
 
-        
         return results;
     }
-    
 };
