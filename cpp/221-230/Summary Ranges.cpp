f74c0ad7f4a766b8845d5cc1d61de97bf10a1e1a
Zhenchao Gan <303542123@qq.com>
2016-08-28 09:38:41 -0700

    228. Summary Ranges

--- cpp/221-230/Summary Ranges.cpp
+++ cpp/221-230/Summary Ranges.cpp
@@ -2,21 +2,17 @@ class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
         vector<string> res;
-        if (nums.size() == 0) return res;
+        if (nums.empty()) return res;
         
         int i(0);
-        while (i < nums.size())
-        {
-            int start(nums[i]), end(nums[i]);
+        while (i < nums.size()) {
+            int start(nums[i]);
             while (i+1 < nums.size() && nums[i] == nums[i+1]-1)
-            {
-                end++;
                 i++;
-            }
-            if (start == end)
+            if (start == nums[i])
                 res.push_back(to_string(start));
             else
-                res.push_back(to_string(start)+"->"+to_string(end));
+                res.push_back(to_string(start)+"->"+to_string(nums[i]));
             i++;
         }
         
