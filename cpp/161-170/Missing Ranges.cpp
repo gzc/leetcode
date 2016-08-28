42f6d87c2a402cecaf42428e9a322aaa6a5cd596
Zhenchao Gan <303542123@qq.com>
2016-08-28 09:30:31 -0700

    163. Missing Ranges

--- cpp/161-170/Missing Ranges.cpp
+++ cpp/161-170/Missing Ranges.cpp
@@ -1,6 +1,6 @@
 class Solution {
     
-    string get_range(int start, int end) {
+    inline string get_range(int start, int end) {
         return start==end? to_string(start) : to_string(start)+"->"+to_string(end);
     }
     
@@ -9,10 +9,9 @@ public:
     vector<string> findMissingRanges(vector<int>& nums, int lower, int upper) {
         vector<string> result;
         int pre = lower-1;
-        for(int i =0; i <= nums.size(); i++)
-        {
+        for (int i = 0; i <= nums.size(); i++) {
             int cur = (i==nums.size()? upper+1:nums[i]);
-            if(cur-pre>=2)
+            if (cur-pre>=2)
                 result.push_back(get_range(pre+1,cur-1));
             pre = cur;
         }
