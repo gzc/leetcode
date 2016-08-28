57979d210ba0be91925c6f37919346fdc121fb03
Zhenchao Gan <303542123@qq.com>
2016-08-28 10:19:03 -0700

    308. Range Sum Query 2D - Mutable

--- cpp/301-310/Range Sum Query 2D - Mutable.cpp
+++ cpp/301-310/Range Sum Query 2D - Mutable.cpp
@@ -5,29 +5,30 @@ class NumMatrix {
     
 public:
     NumMatrix(vector<vector<int>> &matrix) {
-        if(matrix.size() == 0 || matrix[0].size() == 0) return;
+        if (matrix.empty() || matrix[0].empty()) return;
         
-        for(int i = 0;i < matrix.size();i++) {
+        for (int i = 0; i < matrix.size(); i++) {
             vector<int> temp(matrix[0].size()+1,0);
-            for(int j = 0;j < matrix[0].size();j++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
                 temp[j+1] = temp[j] + matrix[i][j];
             }
-            dp.push_back(temp);
+            dp.emplace_back(temp);
         }
         
         this->matrix = matrix;
     }
     
     void update(int row, int col, int val) {
-        for(int j = col;j < matrix[0].size();j++) {
-            dp[row][j+1] = dp[row][j+1] - matrix[row][col] + val;
+        int diff = val - matrix[row][col];
+        for (int j = col; j < matrix[0].size(); j++) {
+            dp[row][j+1] = dp[row][j+1] + diff;
         }
         matrix[row][col] = val;
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
         int res = 0;
-        for(int i = row1;i <= row2;i++) {
+        for (int i = row1; i <= row2; i++) {
             res += (dp[i][col2+1] - dp[i][col1]);
         }
         return res;
