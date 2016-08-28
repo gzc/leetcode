5ba1e902854e0d490b6a336790d3bfe69c362518
Zhenchao Gan <303542123@qq.com>
2016-08-28 09:43:07 -0700

    303. Range Sum Query - Immutable

--- cpp/301-310/Range Sum Query - Immutable.cpp
+++ cpp/301-310/Range Sum Query - Immutable.cpp
@@ -4,7 +4,7 @@ public:
     NumArray(vector<int> &nums) {
         a = new int[nums.size()+1];
         a[0] = 0;
-        for(int i = 0;i < nums.size();i++) {
+        for (int i = 0; i < nums.size(); i++) {
             a[i+1] = nums[i] + a[i];
         }
     }
@@ -13,3 +13,10 @@ public:
         return a[j+1] - a[i];
     }
 };
+
+
+
+// Your NumArray object will be instantiated and called as such:
+// NumArray numArray(nums);
+// numArray.sumRange(0, 1);
+// numArray.sumRange(1, 2);
