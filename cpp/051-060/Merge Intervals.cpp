006f32c1976b0199119ba05ed1a782b1300e3b4c
Zhenchao Gan <303542123@qq.com>
2016-08-28 09:33:30 -0700

    56. Merge Intervals

--- cpp/051-060/Merge Intervals.cpp
+++ cpp/051-060/Merge Intervals.cpp
@@ -1,18 +1,26 @@
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
 class Solution {
     
 public:
     vector<Interval> merge(vector<Interval> &intervals) {
         if(intervals.size() < 2)
             return intervals;
-        auto comp = [&](Interval m,Interval n) { return m.start < n.start; };
+        auto comp = [&](Interval m, Interval n) { return m.start < n.start; };
         vector<Interval> result;
         sort(intervals.begin(), intervals.end(), comp);
         
         Interval now = intervals[0];
-        for(int i = 1;i < intervals.size();i++)
-        {
+        for (int i = 1;i < intervals.size();i++) {
             Interval then = intervals[i];
-            if(now.end >= then.start) {
+            if (now.end >= then.start) {
                 now.end = max(now.end, then.end);
             } else {
                 result.push_back(now);
