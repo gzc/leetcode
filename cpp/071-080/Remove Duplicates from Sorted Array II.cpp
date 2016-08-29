56d9dc7f205b0d0f46ce70725cdc9c07751c33b1
Zhenchao Gan <303542123@qq.com>
2016-08-28 18:03:58 -0700

    80. Remove Duplicates from Sorted Array II

--- cpp/071-080/Remove Duplicates from Sorted Array II.cpp
+++ cpp/071-080/Remove Duplicates from Sorted Array II.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
-    int removeDuplicates(int A[], int n) {
-        
-        if (n <= 2)
-            return n;
-        int rear = 1;
-        for(int i = 2; i < n; i++){
-            if(!(A[i] == A[rear] && A[i] == A[rear -1])){
-                A[++rear] = A[i];
-            }
-        }
+    int removeDuplicates(vector<int>& nums) {
+        int rear(1), n(nums.size());
+        if (n <= 2) return n;
+        for (int i = 2; i < n; i++)
+            if (!(nums[i] == nums[rear] && nums[i] == nums[rear -1]))
+                nums[++rear] = nums[i];
         return rear+1;
     }
 };
