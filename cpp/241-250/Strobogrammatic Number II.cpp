80aac0ada43a18a38354696137c61bf224b4f312
Zhenchao Gan <303542123@qq.com>
2016-08-28 21:23:58 -0700

    247. Strobogrammatic Number II

--- cpp/241-250/Strobogrammatic Number II.cpp
+++ cpp/241-250/Strobogrammatic Number II.cpp
@@ -1,39 +1,19 @@
 class Solution {
-    
-    map<char, char> m;
-    char chs[5] = {'1','6','9','8','0'};
-    
-    void help(vector<string>& result, string temp, int cur, int n)
-    {
-        // "0" is OK, "0xxxxx" is not
-        if (n > 1 && temp[0] == '0') return;
-        if (cur == (n+1)/2)
-        {
-            result.push_back(temp);
-            return;
-        }
-        
-        for (int i = 0;i <= 4;i++)
-        {
-            temp[cur] = chs[i];
-            temp[n - cur - 1] = m[chs[i]];
-            if (n % 2 == 1 && cur == n/2 && chs[i] != m[chs[i]]) { }  //奇数长度并且是6,9什么都不做
-            else help(result, temp, cur+1, n);
-        }
-    }
-    
 public:
     vector<string> findStrobogrammatic(int n) {
-        m['6'] = '9';
-        m['9'] = '6';
-        m['1'] = '1';
-        m['8'] = '8';
-        m['0'] = '0';
-        
-        vector<string> result;
-        string temp(n, '1');
-        help(result, temp, 0, n);
-        return result;
+        return helper(n , n);
     }
-    
+    vector<string> helper(int m, int n){
+        if(m == 0) return vector<string>({""});
+        if(m == 1) return vector<string>({"0", "1", "8"});
+        vector<string> tmp = helper(m - 2, n), res;
+        for(int i = 0; i < tmp.size(); i++){
+            if(m != n) res.push_back("0" + tmp[i] + "0");
+            res.push_back("1" + tmp[i] + "1");
+            res.push_back("6" + tmp[i] + "9");
+            res.push_back("8" + tmp[i] + "8");
+            res.push_back("9" + tmp[i] + "6");
+        }
+        return res;
+    }
 };
