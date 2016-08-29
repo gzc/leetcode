52199d80a585697c1a9561b7aaba9d54029531b2
Zhenchao Gan <303542123@qq.com>
2016-08-29 16:48:36 -0700

    354. Russian Doll Envelopes

--- cpp/351-360/Russian Doll Envelopes.cpp
+++ cpp/351-360/Russian Doll Envelopes.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int maxEnvelopes(vector<pair<int, int>>& envelopes) {
-        auto Cmp = [](const pair<int, int> &a, const pair<int, int> &b) { 
+        auto Cmp = [&](const pair<int, int> a, const pair<int, int> b) { 
             if(a.first < b.first) return true;
             if(a.first == b.first && a.second > b.second) return true;
             return false;
         };
-        auto Cmp2 = [](const pair<int, int> &a, const pair<int, int> &b) { 
+        auto Cmp2 = [&](const pair<int, int> a, const pair<int, int> b) { 
             return a.second < b.second;
         };
         sort(envelopes.begin(), envelopes.end(), Cmp);
 
         vector<pair<int, int>> res;
-        for(int i=0; i<envelopes.size(); i++) {
+        for (int i=0; i<envelopes.size(); i++) {
             auto it = std::lower_bound(res.begin(), res.end(), envelopes[i], Cmp2);
-            if(it == res.end()) res.push_back(envelopes[i]);
+            if (it == res.end()) res.push_back(envelopes[i]);
             else *it = envelopes[i];
         }
         return res.size();
