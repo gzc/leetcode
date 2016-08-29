dccfeb9d8e42c02ab8a39846ebc999e7eddc68af
Zhenchao Gan <303542123@qq.com>
2016-08-28 17:10:54 -0700

    26. Remove Duplicates from Sorted Array

--- cpp/021-030/Remove Duplicates from Sorted Array .cpp
+++ cpp/021-030/Remove Duplicates from Sorted Array .cpp
@@ -1,40 +1,10 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <stack>
-#include <queue>
-
-using namespace std;
-
 class Solution {
 public:
-    int removeDuplicates(int A[], int n) {
-        
-        if(n <= 1) return n;
-        
-        int cur(1), tmp(A[0]);
-        
-        for(int i = 1;i < n;i++)
-        {
-            if(A[i] == tmp) continue;
-            
-            A[cur] = A[i];
-            cur++;
-            tmp = A[i];
-        }
-        
-        return cur;
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        for (int n : nums)
+            if (i == 0 || n > nums[i-1])
+                nums[i++] = n;
+        return i;
     }
-    
 };
-
-int main()
-{
-    Solution s;
-    
-    int a[3] = {1,1,1};
-    int c = s.removeDuplicates(a, 3);
-    for(int i = 0;i < c;i++) cout << a[i];
-    
-    return 0;
-}
