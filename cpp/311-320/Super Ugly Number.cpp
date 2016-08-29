bba6665670c339dbbea2950ec4b9fd9536d1ef24
Zhenchao Gan <303542123@qq.com>
2016-08-29 16:37:27 -0700

    313. Super Ugly Number

--- cpp/311-320/Super Ugly Number.cpp
+++ cpp/311-320/Super Ugly Number.cpp
@@ -3,13 +3,13 @@ public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
         int k = primes.size();
         vector<int>index(k, 0);
-        vector<int>v(n,INT_MAX);
+        vector<int>v(n, INT_MAX);
         v[0] = 1;
         int val = 1;
         
-        for(int i=1; i<n; i++){
-            for(int j = 0; j< k; j++) v[i] = min(v[i],v[index[j]]*primes[j]);
-            for(int j = 0; j< k; j++) index[j] += (v[i]==v[index[j]]*primes[j]);
+        for (int i=1; i<n; i++){
+            for (int j = 0; j< k; j++) v[i] = min(v[i], v[index[j]]*primes[j]);
+            for (int j = 0; j< k; j++) index[j] += (v[i]==v[index[j]]*primes[j]);
         }
         return v[n-1];
     }
