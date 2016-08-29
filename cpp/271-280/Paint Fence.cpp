6a5c9c8682cecebfad5cbaa173c999bb7f32b170
Zhenchao Gan <303542123@qq.com>
2016-08-28 21:40:49 -0700

    276. Paint Fence

--- cpp/271-280/Paint Fence.cpp
+++ cpp/271-280/Paint Fence.cpp
@@ -7,8 +7,7 @@ public:
         dp[1][0] = k;
         dp[1][1] = k*(k-1);
         
-        for(int i = 2;i < n;i++)
-        {
+        for (int i = 2; i < n; i++) {
             dp[0][i] = dp[1][i-1];
             dp[1][i] = (k-1) * (dp[0][i-1] + dp[1][i-1]);
         }
