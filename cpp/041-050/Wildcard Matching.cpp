1e18767606a0408f60b0c473706aa59bc45e7c9a
Zhenchao Gan <303542123@qq.com>
2016-08-29 21:15:38 -0700

    44. Wildcard Matching

--- cpp/041-050/Wildcard Matching.cpp
+++ cpp/041-050/Wildcard Matching.cpp
@@ -1,19 +1,10 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <stack>
-#include <queue>
-
-using namespace std;
-
 class Solution {
 public:
     bool isMatch(string s, string p) {
         
         int i(0),j(0),ss(0),pp(-1);
         
-        while(i < s.length())
-        {
+        while (i < s.length()) {
             if(s[i] == p[j] || p[j] == '?') {i++;j++;continue;}
             
             else if(p[j] == '*'){ss = i;pp = ++j;continue;}
@@ -23,16 +14,8 @@ public:
             return false;
         }
         
-        while(j < p.length()) if(p[j++] != '*') return false;
+        while (j < p.length()) if(p[j++] != '*') return false;
         return true;
     }
     
 };
-
-int main()
-{
-    Solution s;
-    cout << s.isMatch("ab", "?*");
-
-    return 0;
-}
