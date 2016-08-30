2103c3d632d68920c6d17f4375eba96c939d4db3
Zhenchao Gan <303542123@qq.com>
2016-08-29 21:35:44 -0700

    165. Compare Version Numbers

--- cpp/161-170/Compare Version Numbers.cpp
+++ cpp/161-170/Compare Version Numbers.cpp
@@ -1,72 +1,29 @@
 class Solution {
-    
-    int CMP(string s1, string s2)
-    {
-        while(s1.length() > 1 && s1[0] == '0')
-            s1 = s1.substr(1);
-        while(s2.length() > 1 && s2[0] == '0')
-            s2 = s2.substr(1);
-
-        if(s1.length() > s2.length())
-            return 1;
-        if(s1.length() < s2.length())
-            return -1;
-        
-        int len = s1.length();
-        for(int i = 0;i < len;i++)
-        {
-            if(s1[i] > s2[i])
-                return 1;
-            if(s1[i] < s2[i])
-                return -1;
-        }
-        
-        return 0;
-        
-    }
-    
-    bool zero(string s)
-    {
-        for(int i = 0;i < s.length();i++)
-        {
-            if(s[i] == '0' || s[i] == '.')
-                continue;
-            else
-                return false;
-        }
-        return true;
-    }
-    
 public:
     int compareVersion(string version1, string version2) {
+        int i(0), j(0);
+        int n1(version1.size()), n2(version2.size());
+        int num1(0), num2(0);
         
-        int dot1 = version1.find('.');
-        int dot2 = version2.find('.');
-        
-        if(dot1 > 0 && dot2 > 0){
-            int flag = CMP(version1.substr(0, dot1), version2.substr(0, dot2));
-            if(flag != 0) return flag;
-            else return compareVersion(version1.substr(dot1+1), version2.substr(dot2+1));
+        while (i<n1 || j<n2) {
+            while (i<n1 && version1[i]!='.') {
+                num1 = num1*10+(version1[i]-'0');
+                i++;
+            }
+            
+            while (j<n2 && version2[j]!='.') {
+                num2 = num2*10+(version2[j]-'0');;
+                j++;
+            }
+            
+            if (num1>num2) return 1;
+            else if (num1 < num2) return -1;
+            
+            num1 = 0;
+            num2 = 0;
+            i++;
+            j++;
         }
-        
-        else if(dot1 > 0 && dot2 < 0){
-            int flag = CMP(version1.substr(0, dot1), version2);
-            if(flag != 0) return flag;
-            if(zero(version1.substr(dot1+1))) return 0;
-            else return 1;
-        }
-        
-        else if(dot1 < 0 && dot2 > 0){
-            int flag = CMP(version1, version2.substr(0, dot2));
-            if(flag != 0) return flag;
-            if(zero(version2.substr(dot2+1))) return 0;
-            else return -1;
-        }
-        
-        else {
-            int flag = CMP(version1, version2);
-            return flag;
-        }
- 
+        return 0;
     }
 };
