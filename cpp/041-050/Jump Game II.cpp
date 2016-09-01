e7ddc831c5b850dc55bd7d668d19be25a918daf8
Zhenchao Gan <303542123@qq.com>
2016-08-31 21:48:35 -0700

    45. Jump Game II

--- cpp/041-050/Jump Game II.cpp
+++ cpp/041-050/Jump Game II.cpp
@@ -1,41 +1,22 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <stack>
-#include <queue>
-
-using namespace std;
-
 class Solution {
 public:
     int jump(vector<int>& nums) {
         
-        if(nums.size() < 2) return 0;
+        if (nums.size() < 2) return 0;
         
         int count(0);
         int step(0);
         int nextstep(0);
         
-        for(int i = 0;i < nums.size();i++)
-        {
-            if(i + nums[i] > nextstep) nextstep = i + nums[i];
-            if(i == step && i != nums.size()-1) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (i + nums[i] > nextstep) nextstep = i + nums[i];
+            if (i == step && i != nums.size()-1) {
                 step = nextstep;
                 count++;
             }
         }
         
         return count;
-        
     }
     
 };
-
-int main()
-{
-    Solution s;
-    int a[] = {1,2,3};
-    vector<int>v{a, a+3};
-    cout << s.jump(v);
-    return 0;
-}
