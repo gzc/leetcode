7ddc9d7d6f50537cc541d688215b9985135ae874
Zhenchao Gan <303542123@qq.com>
2016-08-31 21:46:32 -0700

    55. Jump Game

--- cpp/051-060/Jump Game.cpp
+++ cpp/051-060/Jump Game.cpp
@@ -1,21 +1,11 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <stack>
-#include <queue>
-
-using namespace std;
-
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
         
         int maxpos(0);
 
-        for(int i = 0;i < nums.size();i++)
-        {
-            if(i <= maxpos)
-            {
+        for (int i = 0; i < nums.size(); i++) {
+            if (i <= maxpos) {
                 int tmp = i + nums[i];
                 if(tmp >= (nums.size()-1)) return true;
                 maxpos = max(tmp, maxpos);
@@ -26,12 +16,3 @@ public:
     }
     
 };
-
-int main()
-{
-    Solution s;
-    int a[] = {2,5,0,0};
-    vector<int>v{a, a+4};
-    cout << s.canJump(v);
-    return 0;
-}
