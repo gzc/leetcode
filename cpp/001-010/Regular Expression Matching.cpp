c409967e888798d06917f548bb27da24905c109b
Zhenchao Gan <303542123@qq.com>
2016-09-04 10:29:41 -0700

    10. Regular Expression Matching

--- cpp/001-010/Regular Expression Matching.cpp
+++ cpp/001-010/Regular Expression Matching.cpp
@@ -1,15 +1,48 @@
-bool isMatch(const char *s, const char *p) {
-    if (*p == '\0') return *s == '\0';
- 
-    if (*(p+1) != '*') {
-        return ((*p == *s) || (*p == '.' && *s != 0)) && isMatch(s+1, p+1);
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if (p.empty())    return s.empty();
+        
+        if ('*' == p[1])
+            // x* matches empty string or at least one character: x* -> xx*
+            // *s is to ensure s is non-empty
+            return (isMatch(s, p.substr(2)) || !s.empty() && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p));
+        else
+            return !s.empty() && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p.substr(1));
     }
-    
-    // next char is '*'
-    while ((*p == *s) || (*p == '.' && *s != 0)) {
-        if (isMatch(s, p+2)) return true;
-        s++;
+};
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        /**
+         * f[i][j]: if s[0..i-1] matches p[0..j-1]
+         * if p[j - 1] != '*'
+         *      f[i][j] = f[i - 1][j - 1] && s[i - 1] == p[j - 1]
+         * if p[j - 1] == '*', denote p[j - 2] with x
+         *      f[i][j] is true iff any of the following is true
+         *      1) "x*" repeats 0 time and matches empty: f[i][j - 2]
+         *      2) "x*" repeats >= 1 times and matches "x*x": s[i - 1] == x && f[i - 1][j]
+         * '.' matches any single character
+         */
+        int m = s.size(), n = p.size();
+        vector<vector<bool>> f(m + 1, vector<bool>(n + 1, false));
+        
+        f[0][0] = true;
+        for (int i = 1; i <= m; i++)
+            f[i][0] = false;
+        // p[0.., j - 3, j - 2, j - 1] matches empty iff p[j - 1] is '*' and p[0..j - 3] matches empty
+        for (int j = 1; j <= n; j++)
+            f[0][j] = j > 1 && '*' == p[j - 1] && f[0][j - 2];
+        
+        for (int i = 1; i <= m; i++)
+            for (int j = 1; j <= n; j++)
+                if (p[j - 1] != '*')
+                    f[i][j] = f[i - 1][j - 1] && (s[i - 1] == p[j - 1] || '.' == p[j - 1]);
+                else
+                    // p[0] cannot be '*' so no need to check "j > 1" here
+                    f[i][j] = f[i][j - 2] || (s[i - 1] == p[j - 2] || '.' == p[j - 2]) && f[i - 1][j];
+        
+        return f[m][n];
     }
-    
-    return isMatch(s, p+2);
-}
+};
