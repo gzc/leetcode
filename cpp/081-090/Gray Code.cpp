f71860a52181e7bd1ee8c964f5c56a60b661a503
Zhenchao Gan <303542123@qq.com>
2016-09-03 19:01:21 -0700

    89. Gray Code

--- cpp/081-090/Gray Code.cpp
+++ cpp/081-090/Gray Code.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        
-        vector<int> v;
-        v.push_back(0);
-        
-        for(int i = 0;i < n;i++)
-        {
+        vector<int> v{0};
+        for (int i = 0; i < n; i++) {
             int len = v.size();
-            for(int j = len-1;j >= 0;j--)
+            for (int j = len-1; j >= 0; j--)
                 v.push_back(v[j] | (1 << i) );
         }
-        
         return v;
     }
 };
