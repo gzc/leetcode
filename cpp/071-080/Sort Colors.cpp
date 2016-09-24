cb378c8559f25bac51f764862484e7a8298e770a
Zhenchao Gan <303542123@qq.com>
2016-09-24 11:15:09 -0700

    75. Sort Colors

--- cpp/071-080/Sort Colors.cpp
+++ cpp/071-080/Sort Colors.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    void sortColors(int A[], int n) {
-        int i, j;
-        i = j;
-        
-        for(int r = 0; r < n; r++)
-        {
-            if(A[r] == 0)
-            {
-                swap(A[j],A[r]);
-                swap(A[i],A[j]);
+    void sortColors(vector<int>& nums) {
+        int i = 0, j = 0;
+        for (int r = 0; r < nums.size(); r++) {
+            if (nums[r] == 0) {
+                swap(nums[j], nums[r]);
+                swap(nums[i], nums[j]);
                 i++;
                 j++;
-            } else if(A[r] == 1)
-            {
-                swap(A[r],A[j]);
+            } else if (nums[r] == 1) {
+                swap(nums[r], nums[j]);
                 j++;
             }
         }
-        
     }
 };
