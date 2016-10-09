24583d769e8b72ff8683b7b7d181f5ae183b5cf4
Zhenchao Gan <303542123@qq.com>
2016-10-09 16:06:58 -0700

    402. Remove K Digits

---
+++ cpp/401-410/Remove K Digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        string res = "";
+        int n = num.size(), keep = n - k;
+        for (char c : num) {
+            while (k && res.size() && res.back() > c) {
+                res.pop_back();
+                --k;
+            }
+            res.push_back(c);
+        }
+        res.resize(keep);
+        while (!res.empty() && res[0] == '0') res.erase(res.begin());
+        return res.empty() ? "0" : res;
+    }
+};
