d01d607038c112ea9f691255616320d55150cb8b
Zhenchao Gan <303542123@qq.com>
2016-11-11 11:56:49 -0800

    Delete Minimum Moves to Equal Array Elements.cpp

--- 451-460/Minimum Moves to Equal Array Elements.cpp
+++
@@ -1,6 +0,0 @@
-class Solution {
-public:
-    int minMoves(vector<int>& nums) {
-        return accumulate(begin(nums), end(nums), 0) - nums.size() * *min_element(begin(nums), end(nums));
-    }
-};
