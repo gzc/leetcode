499c59398b5114a0d7db9ec33ce846d3421f41c7
Zhenchao Gan <303542123@qq.com>
2016-12-20 22:50:07 -0800

    477. Total Hamming Distance

--- cpp/471-480/Total Hamming Distance.cpp
+++ cpp/471-480/Total Hamming Distance.cpp
@@ -5,7 +5,7 @@ public:
         int totalhammingdistance = 0;
         for (int v : nums) {
             for (int i = 0; i < 32; i++, v/=2) {
-                int key = v % 2;
+                int key = v & 0x1;
                 totalhammingdistance += stats[1-key][i];
                 stats[key][i] ++;
             }
