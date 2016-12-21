d5b2e929f3a19d019bd8e04326490a13a809a6ff
Zhenchao Gan <303542123@qq.com>
2016-12-20 22:48:59 -0800

    477. Total Hamming Distance

---
+++ cpp/471-480/Total Hamming Distance.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int totalHammingDistance(vector<int>& nums) {
+        vector<vector<int>> stats(2, vector<int>(32, 0));
+        int totalhammingdistance = 0;
+        for (int v : nums) {
+            for (int i = 0; i < 32; i++, v/=2) {
+                int key = v % 2;
+                totalhammingdistance += stats[1-key][i];
+                stats[key][i] ++;
+            }
+        }
+        return totalhammingdistance;
+    }
+};
