1ca71381dd034d78755ee72389e6b2225a3af8b3
Zhenchao Gan <303542123@qq.com>
2017-07-29 17:48:15 -0700

    208. Implement Trie (Prefix Tree)

--- cpp/201-210/Implement Trie (Prefix Tree).cpp
+++ cpp/201-210/Implement Trie (Prefix Tree).cpp
@@ -1,43 +1,40 @@
-class TrieNode {
-    
-public:
+struct TrieNode {
     TrieNode *nodes[26];
     bool word;
     // Initialize your data structure here.
-    TrieNode() {
-        word = false;
-        for(int i = 0;i < 26;i++)
-            nodes[i]=NULL;
+    TrieNode(): word(false) {
+        memset(nodes, 0, sizeof(nodes));
     }
 };
 
 class Trie {
+    
 public:
     Trie() {
         root = new TrieNode();
     }
     
     // Inserts a word into the trie.
-    void insert(string s) {
+    void insert(const string& s) {
         TrieNode *tmp = root;
-        for(int i = 0;i < s.length();i++)
-        {
-            int index = s[i] - 'a';
-            if(tmp->nodes[index] == NULL)
+        for(char ch : s) {
+            int index = ch - 'a';
+            if(tmp->nodes[index] == nullptr) {
                 tmp->nodes[index] = new TrieNode();
+            }
             tmp = tmp->nodes[index];
         }
         tmp->word = true;
     }
     
     // Returns if the word is in the trie.
-    bool search(string key) {
+    bool search(const string& key) const {
         TrieNode *tmp = root;
-        for(int i = 0;i < key.length();i++)
-        {
-            int index = key[i] - 'a';
-            if(root->nodes[index] == NULL)
+        for(char ch : key) {
+            int index = ch - 'a';
+            if(tmp->nodes[index] == nullptr) {
                 return false;
+            }
             tmp = tmp->nodes[index];
         }
         return tmp->word;
@@ -45,13 +42,13 @@ public:
     
     // Returns if there is any word in the trie
     // that starts with the given prefix.
-    bool startsWith(string prefix) {
+    bool startsWith(const string& prefix) const {
         TrieNode *tmp = root;
-        for(int i = 0;i < prefix.length();i++)
-        {
-            int index = prefix[i] - 'a';
-            if(tmp->nodes[index] == NULL)
+        for(char ch : prefix) {
+            int index = ch - 'a';
+            if(tmp->nodes[index] == nullptr) {
                 return false;
+            }
             tmp = tmp->nodes[index];
         }
         return true;
