4c0a7ae5b1dea7493e9facd1eb82353a14f00758
Zhenchao Gan <303542123@qq.com>
2017-08-12 21:42:20 -0700

    636. Exclusive Time of Functions

---
+++ cpp/631-640/Exclusive Time of Functions.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    vector<string> split(const string &s, char delim) {
+        vector<string> elems;
+        stringstream ss(s);
+        string item;
+        while (getline(ss, item, delim)) {
+            if (item.length() > 0) {
+                elems.push_back(item);
+            }
+        }
+        return elems;
+    }
+    
+public:
+    vector<int> exclusiveTime(int n, vector<string>& logs) {
+        vector<int> result(n, 0);
+        stack<pair<int, int>> mystack; // pair<id, startTime>
+        for (const string& log : logs) {
+            vector<string> myvec = split(log, ':');
+            int id = stoi(myvec[0]);
+            bool flag = myvec[1] == "start" ? true : false; // true start; false end
+            int time = stoi(myvec[2]);
+            if (flag) {
+                mystack.push({id, time});
+            } else {
+                pair<int, int> currentPair = mystack.top();
+                mystack.pop();
+                int timeSpent = time - currentPair.second + 1;
+                result[currentPair.first] += timeSpent;
+                if (!mystack.empty()) {
+                    pair<int, int> callerPair = mystack.top();
+                    result[callerPair.first] -= timeSpent;
+                }
+            }
+        }
+        return result;
+    }
+};
