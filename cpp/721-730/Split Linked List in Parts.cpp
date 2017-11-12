622a91608edeb6ad9aac4533fd08934cb8e7340e
Zhenchao Gan <303542123@qq.com>
2017-11-12 11:33:35 -0800

    725. Split Linked List in Parts

---
+++ cpp/721-730/Split Linked List in Parts.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* root, int k) {
+        vector<ListNode*> parts(k, nullptr);
+        int len = 0;
+        for (ListNode* node = root; node != nullptr; node = node->next)
+            len++;
+        int n = len / k, r = len % k; // n : minimum guaranteed part size; r : extra nodes spread to the first r parts;
+        ListNode* node = root, *prev = nullptr;
+        for (int i = 0; i < k; i++, r--) {
+            parts[i] = node;
+            for (int j = 0; j < n + (r > 0); j++) {
+                prev = node;
+                node = node->next;
+            }
+            if (prev) prev->next = nullptr;
+        }
+        return parts;
+    }
+};
