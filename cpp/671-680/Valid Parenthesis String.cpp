43bbf6bb0ce5b3ec7ff1959950a33e8c214608fe
Zhenchao Gan <303542123@qq.com>
2017-12-02 23:03:21 -0800

    678. Valid Parenthesis String

---
+++ cpp/671-680/Valid Parenthesis String.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int minLeft = 0, maxLeft = 0;
+        
+        for (char ch : s) {
+            if (ch == '(') {
+                minLeft++;
+                maxLeft++;
+            } else if (ch == ')') {
+                minLeft--;
+                maxLeft--;
+            } else {
+                minLeft--;
+                maxLeft++;
+            }
+            
+            if (maxLeft < 0) {
+                return false;
+            }
+            
+            minLeft = max(0, minLeft);
+        }
+        
+        return minLeft == 0 && maxLeft >= 0;
+    }
+};
