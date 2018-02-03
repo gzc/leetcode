331d6e620da80209905d8c28f782805a69742c96
Zhenchao Gan <303542123@qq.com>
2018-02-02 19:38:49 -0800

    394. Decode String

---
+++ cpp/391-400/Decode String.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    
+    string solve(const string& s, int& i) {
+        string result;
+        while (i < s.length() && s[i] != ']') {
+            int d = 0;
+            if (isdigit(s[i])) {
+                while (isdigit(s[i])) {
+                    d = 10 * d + (s[i] - '0');
+                    i++;
+                }
+            }
+            
+            if (s[i] == '[') {
+                i++;
+                string temp = solve(s, i);
+                while (d-- > 0) result += temp;
+            } else {
+                result += s[i];
+                i++;
+            }
+                       
+        }
+        i++;
+        return result;
+    }
+    
+public:
+    string decodeString(string s) {
+        int i = 0;
+        return solve(s, i);
+    }
+};
