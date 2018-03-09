8157fd17539eaf63632432812a50c60f9a486252
Zhenchao Gan <303542123@qq.com>
2018-03-09 00:31:18 -0800

    556. Next Greater Element III

---
+++ cpp/551-560/Next Greater Element III.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    
+    void nextPermutation(vector<int> &num) {
+        if (num.empty()) return;
+        
+        // in reverse order, find the first number which is in increasing trend (we call it violated number here)
+        int i(0);
+        for (i = num.size()-2; i >= 0; --i)
+            if (num[i] < num[i+1]) break;
+        
+        // reverse all the numbers after violated number
+        reverse(begin(num)+i+1, end(num));
+        
+        // if violated number not found, because we have reversed the whole array, then we are done!
+        if (i == -1) return;
+        
+        // else binary search find the first number larger than the violated number
+        auto itr = upper_bound(num.begin()+i+1, num.end(), num[i]);
+        
+        // swap them, done!
+        swap(num[i], *itr);
+    }
+    
+public:
+    int nextGreaterElement(int n) {
+        vector<int> myvec;
+        int nn = n;
+        while (nn > 0) {
+            int digit = nn % 10;
+            nn /= 10;
+            myvec.emplace(myvec.begin(), digit);
+        }
+        
+        nextPermutation(myvec);
+        int ans = 0;
+        for (int e : myvec) {
+            ans = 10 * ans + e;
+        }
+        
+        if (ans > n) return ans;
+        return -1;
+    }
+};
