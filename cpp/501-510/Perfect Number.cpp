95c134a40f154f7a9e9ab09a383fd1eac8b7d380
Zhenchao Gan <303542123@qq.com>
2018-03-18 16:09:33 -0700

    507. Perfect Number

---
+++ cpp/501-510/Perfect Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        int sum = 1;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i * i != num) {
+                    sum += num / i;
+                }
+
+            }
+        }
+        return sum == num;
+    }
+};
