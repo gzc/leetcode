0d8e103ab4769648f9e61dfe7d2fddcd7d14597e
Zhenchao Gan <303542123@qq.com>
2018-04-12 20:33:37 -0700

    692. Top K Frequent Words

---
+++ cpp/691-700/Top K Frequent Words.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> freq;
+        for(auto w : words){
+            freq[w]++;
+        }
+        
+        auto comp = [&](const pair<string,int>& a, const pair<string,int>& b) {
+            return a.second > b.second || (a.second == b.second && a.first < b.first);
+        };
+        typedef priority_queue< pair<string,int>, vector<pair<string,int>>, decltype(comp) > my_priority_queue_t;
+        my_priority_queue_t  pq(comp);
+        
+        for(auto w : freq ){
+            pq.emplace(w.first, w.second);
+            if(pq.size()>k) pq.pop();
+        }
+        
+        vector<string> output;
+        while(!pq.empty()){
+            output.insert(output.begin(), pq.top().first);
+            pq.pop();
+        }
+        return output;
+    }
+};
