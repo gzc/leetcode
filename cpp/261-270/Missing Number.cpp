850e2043a7cb01f6d3ee9e456a65c369a1282936
Zhenchao Gan <303542123@qq.com>
2018-04-21 22:11:01 -0700

    268. Missing Number

--- cpp/261-270/Missing Number.cpp
+++ cpp/261-270/Missing Number.cpp
@@ -7,3 +7,16 @@ public:
         return v - s;
     }
 };
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            ans ^= i;
+            ans ^= nums[i];
+        }
+        ans ^= nums.size();
+        return ans;
+    }
+};
