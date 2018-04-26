1bee46047d707fdb3ef75809348dab1b19893892
Zhenchao Gan <303542123@qq.com>
2018-04-25 23:45:56 -0700

    201. Bitwise AND of Numbers Range

--- cpp/201-210/Bitwise AND of Numbers Range.cpp
+++ cpp/201-210/Bitwise AND of Numbers Range.cpp
@@ -1,42 +1,22 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <stack>
-#include <queue>
-
-using namespace std;
-
 class Solution {
+    
+    int maxBit(int num) {
+        int ans = 0;
+        while(num > 0) {
+            num = num >> 1;
+            if(ans == 0) ans = 1;
+            else ans *= 2;
+        }
+        return ans;
+    }
+    
 public:
     int rangeBitwiseAnd(int m, int n) {
-        
         if(m == 0) return 0;
         
-        int m_maxbit(0), n_maxbit(0);
-        int tmp(n);
-        while(tmp > 0)
-        {
-            tmp = tmp >> 1;
-            if(n_maxbit == 0) n_maxbit = 1;
-            else n_maxbit *= 2;
-        }
-        tmp = m;
-        while(tmp > 0)
-        {
-            tmp = tmp >> 1;
-            if(m_maxbit == 0) m_maxbit = 1;
-            else m_maxbit *= 2;
-        }
+        int m_maxbit = maxBit(m);
+        int n_maxbit = maxBit(n);
         if(m_maxbit != n_maxbit) return 0;
         return m_maxbit + rangeBitwiseAnd(m - m_maxbit, n - n_maxbit);
     }
-    
 };
-
-int main()
-{
-    Solution s;
-    cout << s.rangeBitwiseAnd(5, 7);
-
-    return 0;
-}
