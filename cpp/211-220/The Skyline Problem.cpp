3d07f669259d29101935d5bc07a8631eb63b812d
Zhenchao Gan <303542123@qq.com>
2018-04-27 19:40:47 -0700

    218. The Skyline Problem

--- cpp/211-220/The Skyline Problem.cpp
+++ cpp/211-220/The Skyline Problem.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-    struct comp{
-        bool operator ()(int a, int b){ return a > b;}
-    };
-    
-    struct mycomparison{
-        bool operator ()(const pair<int, int> &a, const pair<int, int> &b){ return a.first >= b.first;}
-    };
-
     vector<pair<int, int>> getSkyline(vector<vector<int>>& buildings) {
         vector<pair<int, int> > ret;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, mycomparison> points;
-        for(auto e : buildings) {
+        auto cmp = [&](const pair<int, int> &a, const pair<int, int> &b){ return a.first >= b.first; };
+        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> points(cmp);
+        for(const auto& e : buildings) {
             points.push(make_pair(e[0], e[2]));
             points.push(make_pair(e[1], e[2] * -1));
         }
-        multiset<int, comp> sky;
+        multiset<int, std::greater<int>> sky;
         sky.insert(0);
         while (!points.empty()){
             int last_height = *sky.begin();
