3341e1450d2e58d443fc10985c1af815658019d0
Zhenchao Gan <303542123@qq.com>
2018-05-01 23:58:08 -0700

    237. Delete Node in a Linked List

--- cpp/231-240/Delete Node in a Linked List.cpp
+++ cpp/231-240/Delete Node in a Linked List.cpp
@@ -9,12 +9,8 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        
-        ListNode* next = node->next;
-        node->val = next->val;
-        node->next = next->next;
-        delete next;
-        
+        ListNode* nextNode = node->next;
+        *node = *nextNode;
+        delete nextNode;
     }
-    
 };
