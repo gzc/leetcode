df19e541aeda229797328299640c25006c7eee8e
Zhenchao Gan <303542123@qq.com>
2018-05-02 00:49:20 -0700

    48. Rotate Image

--- cpp/041-050/Rotate Image.cpp
+++ cpp/041-050/Rotate Image.cpp
@@ -1,18 +1,9 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <stack>
-#include <queue>
-
-using namespace std;
-
 class Solution {
 public:
     void rotate(vector<vector<int> > &matrix) {
         int n = matrix[0].size() -1;
-        for(int i = 0;i <= n - i;i++){
-            for(int j = i;j <= n - i - 1;j++)
-            {
+        for (int i = 0;i <= n - i;i++) {
+            for (int j = i;j <= n - i - 1;j++) {
                 int tmp = matrix[j][n-i];
                 matrix[j][n-i] = matrix[i][j];
                 matrix[i][j] = matrix[n-j][i];
@@ -21,12 +12,4 @@ public:
             }
         }
     }
-    
 };
-
-int main()
-{
-    Solution s;
-    
-    return 0;
-}
