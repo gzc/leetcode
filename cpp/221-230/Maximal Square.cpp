102192fb3557ac49ba1f653810e6ec58a7cca6eb
Zhenchao Gan <303542123@qq.com>
2018-05-02 19:04:23 -0700

    221. Maximal Square

--- cpp/221-230/Maximal Square.cpp
+++ cpp/221-230/Maximal Square.cpp
@@ -1,44 +1,25 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
+        if (matrix.empty() || matrix.front().empty()) return 0;
+        int m = matrix.size();
+        int n = matrix.front().size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        int maxLen = 0;
         
-        if(matrix.size() <= 0) return 0;
-        
-        vector<int> h(matrix[0].size()+1, 0);
-        int res(0);
-        
-        for(int i = 0;i < matrix.size();i++)
-        {
-            stack<int> s;
-            matrix[i].push_back('0');
-            
-            for(int j = 0;j < matrix[0].size();j++)
-            {
-                if(matrix[i][j] == '0') h[j] = 0;
-                else h[j]++;
-                
-                if(s.empty() || h[j] >= h[s.top()])
-                    s.push(j);
-                
-                else {
-                    while(!s.empty() && h[j] < h[s.top()])
-                    {
-                        int tp = s.top();
-                        s.pop();
-                        int height = h[tp];
-                        int width = (s.empty() ? j : j - s.top() - 1);
-
-                        int side = min(height, width);
-                        res = max(res, side*side);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0;j < n; j++) {
+                if (matrix[i][j] == '1') {
+                    dp[i][j] = 1;
+                    if (i > 0 && j > 0) {
+                        int tmp = min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j]));
+                        dp[i][j] = max(dp[i][j], tmp+1);
                     }
-                    s.push(j);
                 }
+                maxLen = max(maxLen, dp[i][j]);
             }
         }
         
-        return res;
-        
+        return maxLen*maxLen;
     }
-    
-    
 };
