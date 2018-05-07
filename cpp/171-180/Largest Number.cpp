64a625957d63b0fdede2995572ebfac906e455c8
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-06 21:27:24 -0700

    179. Largest Number

--- cpp/171-180/Largest Number.cpp
+++ cpp/171-180/Largest Number.cpp
@@ -1,32 +1,20 @@
-class mycomparison
-{
-  bool reverse;
-public:
-  mycomparison(const bool& revparam=false)
-    {reverse=revparam;}
-  bool operator() (const string& lhs, const string&rhs) const
-  {
-     string s1 = lhs+rhs;
-     string s2 = rhs+lhs;
-     return s1  <= s2;
-  }
-};
-
-
 class Solution {
-    typedef std::priority_queue<string,std::vector<string>,mycomparison> mypq_type;
     
 public:
     string largestNumber(vector<int> &num) {
         
-        mypq_type pq (mycomparison(true));   // greater-than comparison
+        auto cmp = [&] (const string& lhs, const string& rhs) {
+            return lhs+rhs < rhs+lhs;
+        };
+        
+        typedef std::priority_queue<string, std::vector<string>, decltype(cmp)> mypq_type;
+        mypq_type pq (cmp);
         
         for(int i = 0;i < num.size();i++)
             pq.push(to_string(num[i]));
         
         string result("");
-        while(!pq.empty())
-        {
+        while (!pq.empty()) {
             result = result + pq.top();
             pq.pop();
         }
