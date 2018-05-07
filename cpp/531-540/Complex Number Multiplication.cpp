59e607a615c383cabc4c93f45b0f2725f29c7f91
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-06 21:10:01 -0700

    537. Complex Number Multiplication

---
+++ cpp/531-540/Complex Number Multiplication.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string complexNumberMultiply(string a, string b) {
+        int ra, ia, rb, ib;
+        char buff;
+        stringstream aa(a), bb(b), ans;
+        aa >> ra >> buff >> ia >> buff;
+        bb >> rb >> buff >> ib >> buff;
+        ans << ra*rb - ia*ib << "+" << ra*ib + rb*ia << "i";
+        return ans.str();
+    }
+};
