6cc0387a743f6420caea18b0602c7c2008181f38
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-09 22:52:03 -0700

    781. Rabbits in Forest

--- cpp/781-790/Rabbits in Forest.cpp
+++ cpp/781-790/Rabbits in Forest.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int numRabbits(vector<int>& answers) {
-        map<int, int> mymap;
+        unordered_map<int, int> mymap;
         
         int ans = 0;
         for (int e : answers) {
-            if (mymap.count(e) == 0 || mymap[e] == 0) {
+            if (mymap[e] == 0) {
                 ans += (e+1);
                 mymap[e] = e;
             } else {
