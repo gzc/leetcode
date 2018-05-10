12537dcacd27c386a3b8e8ea2ad4d2f7ce561df5
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-09 22:18:15 -0700

    785. Is Graph Bipartite?

--- cpp/781-790/Is Graph Bipartite?.cpp
+++ cpp/781-790/Is Graph Bipartite?.cpp
@@ -1,34 +1,27 @@
 class Solution {
     
-    bool dfs(int node, vector<bool>& visited, vector<bool>& colors, const vector<vector<int>>& graph, bool c) {
-        visited[node] = true;
+    bool dfs(int node, vector<int>& colors, const vector<vector<int>>& graph, int c) {
         colors[node] = c;
-        bool flag = true;
         for (int neighbor : graph[node]) {
-            if (visited[neighbor]) {
-                if (colors[neighbor] == c) {
-                    return false;
-                }
-            } else {
-                flag = dfs(neighbor, visited, colors, graph, !c);
+            if (colors[neighbor] == c) {
+                return false;
+            }
+            if (colors[neighbor] == 0 && !dfs(neighbor, colors, graph, 3 - c)) {
+                return false;
             }
         }
-        return flag;
+        return true;
     }
     
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<bool> colors(n, false);
-        vector<bool> visited(n, false);
+        vector<int> colors(n, 0);
         
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                if(!dfs(i, visited, colors, graph, false)) {
+        for (int i = 0; i < n; i++)
+            if (colors[i] == 0)
+                if(!dfs(i, colors, graph, 1))
                     return false;
-                }
-            }
-        }
         
         return true;
     }
