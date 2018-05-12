02de2c9ee76d9e03a2206238903deb2be852bd4b
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-12 00:44:10 -0700

    223. Rectangle Area

--- cpp/221-230/Rectangle Area.cpp
+++ cpp/221-230/Rectangle Area.cpp
@@ -1,44 +1,19 @@
 class Solution {
-public:
-    int maximalSquare(vector<vector<char>>& matrix) {
-        
-        if(matrix.size() <= 0) return 0;
-        
-        vector<int> h(matrix[0].size()+1, 0);
-        int res(0);
-        
-        for(int i = 0;i < matrix.size();i++)
-        {
-            stack<int> s;
-            matrix[i].push_back('0');
-            
-            for(int j = 0;j < matrix[0].size();j++)
-            {
-                if(matrix[i][j] == '0') h[j] = 0;
-                else h[j]++;
-                
-                if(s.empty() || h[j] >= h[s.top()])
-                    s.push(j);
-                
-                else {
-                    while(!s.empty() && h[j] < h[s.top()])
-                    {
-                        int tp = s.top();
-                        s.pop();
-                        int height = h[tp];
-                        int width = (s.empty() ? j : j - s.top() - 1);
-
-                        int side = min(height, width);
-                        res = max(res, side*side);
-                    }
-                    s.push(j);
-                }
-            }
-        }
-        
-        return res;
-        
+    
+    int overlapLen(int a1, int a2, int b1, int b2) {
+        if (a1 > b1) return overlapLen(b1, b2, a1, a2);
+        int left = b1;
+        int right = min(a2, b2);
+        if (right <= left) return 0;
+        return right - left;
     }
     
-    
+public:
+    int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        int area1 = (C - A) * (D - B);
+        int area2 = (G - E) * (H - F);
+        int width = overlapLen(A, C, E, G);
+        int height = overlapLen(B, D, F, H);
+        return area1 + area2 - width*height;
+    }
 };
