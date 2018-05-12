3923e63f332ef5e4c1ecd6b13decdcdbcca92bbf
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-11 19:54:43 -0700

    131. Palindrome Partitioning

--- cpp/131-140/Palindrome Partitioning.cpp
+++ cpp/131-140/Palindrome Partitioning.cpp
@@ -1,7 +1,7 @@
 class Solution {
     
     void dfs(int index, string& s, vector<string>& path, vector<vector<string> >& ret, vector<vector<bool>> &p) {
-        if(index == s.size()) {
+        if (index == s.size()) {
             ret.push_back(path);
             return;
         }
@@ -20,15 +20,12 @@ public:
         vector<vector<string> > ret;
         if(s.empty()) return ret;
         
-        vector<vector<bool> >p(s.length(), vector<bool>(s.length(), false));
-        for(int i = 0;i < s.length();i++)
-        {
-            for(int j = 0;j < s.length()-i;j++)
-            {
+        vector<vector<bool>>p(s.length(), vector<bool>(s.length(), false));
+        for (int i = 0;i < s.length();i++) {
+            for (int j = 0;j < s.length()-i;j++) {
                 if(i == 0)
                     p[j][j] = true;
-                else
-                {
+                else {
                     if(s[j] == s[j+i])
                         p[j][j+i] = j+1 <= j+i-1 ? p[j+1][j+i-1] : true;
                     else
