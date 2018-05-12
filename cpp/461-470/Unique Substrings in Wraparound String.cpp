11a4d08fee3fea702bfccaf1be646c760e2d123b
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-12 13:48:25 -0700

    467. Unique Substrings in Wraparound String

--- cpp/461-470/Unique Substrings in Wraparound String.cpp
+++ cpp/461-470/Unique Substrings in Wraparound String.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> cnt(26, 0);
         int len = 0;
         for (int i = 0; i < p.size(); ++i) {
-            if (i > 0 && (p[i] == p[i - 1] + 1 || p[i - 1] - p[i] == 25)) {
+            if (i > 0 && (p[i] - p[i-1] + 26) % 26 == 1) {
                 ++len;
             } else {
                 len = 1;
