242ec75a2b149f548ecfdc2b64b0e8d683e4ca7f
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-14 00:37:22 -0700

    414. Third Maximum Number

---
+++ cpp/411-420/Third Maximum Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        set<int> top3;
+        for (int num : nums) {
+            top3.insert(num);
+            if (top3.size() > 3)
+                top3.erase(top3.begin());
+        }
+        return top3.size() == 3 ? *top3.begin() : *top3.rbegin();
+    }
+};
