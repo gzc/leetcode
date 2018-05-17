bdcb7a6ea5fb4d963ad1907cb22f3e6e0b8dfff0
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-16 21:02:07 -0700

    110. Balanced Binary Tree

--- cpp/101-110/Balanced Binary Tree.cpp
+++ cpp/101-110/Balanced Binary Tree.cpp
@@ -1,21 +1,18 @@
 class Solution {
-
-    bool flag;
     
-    int help(TreeNode *root)
-    {
-        if(!root) return 0;
-        int left = 1+help(root->left);
-        int right = 1+help(root->right);
+    int help(TreeNode *root, bool& flag) {
+        if (root == nullptr) return 0;
+        int left = 1 + help(root->left, flag);
+        int right = 1 + help(root->right, flag);
         
-        if(abs(left - right) > 1) flag = false;
+        if (abs(left - right) > 1) flag = false;
         return max(left, right);
     }
     
 public:
     bool isBalanced(TreeNode *root) {
-        flag = true;
-        help(root);
+        bool flag = true;
+        help(root, flag);
         return flag;
     }
 };
