343bee592d2e05d6efb01f80ac4f6d195ac2b7da
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-20 15:45:10 -0700

    73. Set Matrix Zeroes

--- cpp/071-080/Set Matrix Zeroes.cpp
+++ cpp/071-080/Set Matrix Zeroes.cpp
@@ -6,26 +6,20 @@ public:
         int outer = matrix.size();
         int inner = matrix[0].size();
         
-        for(int i = 0; i < outer; i++)
-        {
-            if(matrix[i][0] == 0)
+        for (int i = 0; i < outer; i++) {
+            if (matrix[i][0] == 0)
                 col0 = true;
-            for(int j = 1; j < inner; j++)
-            {
-                if(matrix[i][j] == 0)
-                {
+            for (int j = 1; j < inner; j++) {
+                if (matrix[i][j] == 0) {
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
                 }
             }
         }
         
-        for(int i = outer-1; i >= 0; i--)
-        {
-            for(int j = inner-1; j >= 1; j--)
-            {
-                if(matrix[i][0] == 0 || matrix[0][j] == 0)
-                {
+        for (int i = outer-1; i >= 0; i--) {
+            for (int j = inner-1; j >= 1; j--) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
