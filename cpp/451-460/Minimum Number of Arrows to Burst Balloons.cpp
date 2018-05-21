f08bf49bcfe4fb6ec7ac51f5590ece73340b1424
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-20 20:48:27 -0700

    452. Minimum Number of Arrows to Burst Balloons

--- cpp/451-460/Minimum Number of Arrows to Burst Balloons.cpp
+++ cpp/451-460/Minimum Number of Arrows to Burst Balloons.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int findMinArrowShots(vector<pair<int, int>>& points) {
         int count = 0, arrow = INT_MIN;
-        auto mysort = [] (const pair<int, int> &a, const pair<int, int> &b) {
-            return a.second==b.second?a.first<b.first:a.second<b.second; };
-        sort(points.begin(), points.end(), mysort);
-        for(int i = 0; i<points.size(); i++){
-            if(arrow!=INT_MIN && points[i].first<=arrow){continue;} //former arrow shot points[i] 
+        auto cmp = [&] (const pair<int, int>& a, const pair<int, int>& b) {
+            return a.second == b.second ? a.first < b.first : a.second < b.second;
+        };
+        sort(points.begin(), points.end(), cmp);
+        for (int i = 0; i < points.size(); i++)  {
+            if (arrow != INT_MIN && points[i].first <= arrow) { continue; } //former arrow shot points[i] 
             arrow = points[i].second; // new arrow shot the end of points[i]
             count++;
         }
