3e3cff967fa707e0db029a2ab349491003078f4f
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-20 21:46:00 -0700

    285. Inorder Successor in BST

--- cpp/281-290/Inorder Successor in BST.cpp
+++ cpp/281-290/Inorder Successor in BST.cpp
@@ -10,29 +10,14 @@
 class Solution {
 public:
     TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
-        
-        stack<TreeNode*> s;
-        s.push(root);
-        bool flag(false);
-        
-        while(!s.empty() || root)
-        {
-            while(root && root->left)
-            {
-                s.push(root->left);
-                root = root->left;
-            }
-            
-            root = s.top();
-            s.pop();
-            if(flag) return root;
-            if(root == p) flag = true;
-            
-            if(root->right)
-                s.push(root->right);
-            root = root->right;
+        if (root == nullptr)
+            return nullptr;
+
+        if (root->val <= p->val) {
+            return inorderSuccessor(root->right, p);
+        } else {
+            TreeNode* left = inorderSuccessor(root->left, p);
+            return (left != nullptr) ? left : root;
         }
-        
-        return nullptr;
     }
 };
