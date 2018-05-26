3a887153f3efcf7a3ff45cd51927294fce3d5d0a
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-25 20:34:40 -0700

    248. Strobogrammatic Number III

--- cpp/241-250/Strobogrammatic Number III.cpp
+++ cpp/241-250/Strobogrammatic Number III.cpp
@@ -4,24 +4,20 @@ class Solution {
     map<char, char> m;
     char chs[5] = {'1','6','9','8','0'};
     
-    bool check(string low, string high)
-    {
+    bool check(const string& low, const string& high) {
         if (high.length() > low.length()) return true;
         if (high.length() < low.length()) return false;
         return high >= low;
     }
     
-    void help(string temp, int cur, int n, string low, string high)
-    {
+    void help(string temp, int cur, int n, const string& low, const string& high) {
         if (n >= 2 && temp[0] == '0') return;
-        if (cur == (n+1)/2)
-        {
+        if (cur == (n+1)/2) {
             if (check(low, temp) && check(temp, high)) {r++;}
             return;
         }
         
-        for (int i = 0;i <= 4;i++)
-        {
+        for (int i = 0; i <= 4; i++) {
             temp[cur] = chs[i];
             temp[n - cur - 1] = m[chs[i]];
             if (n % 2 == 1 && cur == n/2 && chs[i] != m[chs[i]]) {}
@@ -39,10 +35,9 @@ public:
         
         int n1 = low.length();
         int n2 = high.length();
-        for (int n = n1;n <= n2;n++)
-        {
+        for (int n = n1; n <= n2; n++) {
             string temp(n,'1');
-            help(temp,0,n,low,high);
+            help(temp, 0, n, low, high);
         }
         
         return r;
