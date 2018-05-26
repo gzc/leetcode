691a19a29f46e842bae5358ac738d0986da656da
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-26 10:56:09 -0700

    373. Find K Pairs with Smallest Sums

--- cpp/371-380/Find K Pairs with Smallest Sums.cpp
+++ cpp/371-380/Find K Pairs with Smallest Sums.cpp
@@ -2,19 +2,16 @@ class Solution {
     
     typedef pair<int, vector<int>::iterator> mm;
     
-private:
-    struct mycompare{
-        bool operator()(const mm& p1, const mm& p2){
-            return p1.first + *(p1.second) > p2.first + *(p2.second);
-        }
-    };
 public:
     vector<pair<int, int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         int size = min(k, (int)(nums1.size())*int(nums2.size()));
         vector<pair<int, int>> res(size);
         if (nums1.empty() || nums2.empty()) return res;
         
-        priority_queue<mm, vector<mm>, mycompare> pq;
+        auto cmp = [&] (const mm& p1, const mm& p2) {
+            return p1.first + *(p1.second) > p2.first + *(p2.second);
+        };
+        priority_queue<mm, vector<mm>, decltype(cmp)> pq(cmp);
         
         // Given nums1 = [1,7,11], nums2 = [2,4,6],  k = 3
         // {1, iter}  {7, iter}  {11, iter}
