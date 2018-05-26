43bfb6e619523205735a840ab10c29912a7b8da7
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-25 21:50:37 -0700

    269. Alien Dictionary

--- cpp/261-270/Alien Dictionary.cpp
+++ cpp/261-270/Alien Dictionary.cpp
@@ -1,37 +1,27 @@
 struct node {
-    int starttime;
-    int finishtime;
     char ch;
     int color;
     set<int> edges;
 };
 
-bool myfunction (const node& i, const node& j) { return (i.finishtime > j.finishtime); }
-
 class Solution {
-    
-    bool flag;
-    int mytime;
-    
     node nodes[26];
     
-    void dfs_visit(int u) {
-        if(nodes[u].color == 1) { flag = true; return; }
+    bool dfs_visit(int u, string& result) {
+        if(nodes[u].color == 1) { return false; }
         nodes[u].color = 1;
-        nodes[u].starttime = mytime++;
         for(auto it = nodes[u].edges.begin(); it != nodes[u].edges.end(); it++) {
             if(nodes[*it].color != 2)
-                dfs_visit(*it);
+                if (!dfs_visit(*it, result))
+                    return false;
         }
-        nodes[u].finishtime = mytime++;
+        result = nodes[u].ch + result;
         nodes[u].color = 2;
+        return true;
     }
     
 public:
     string alienOrder(vector<string>& words) {
-        string result("");
-        mytime = 0;
-        
         for (int i = 0;i < words.size();i++)
             for (int j = 0;j < words[i].length();j++)
                 nodes[words[i][j] - 'a'].ch = words[i][j];
@@ -49,16 +39,11 @@ public:
             }
         }
         
+        string result("");
         for (int i = 0;i < 26;i++)
             if (nodes[i].color == 0 && nodes[i].ch >= 'a')
-                dfs_visit(i);
-        
-        if (flag) return "";
-        sort(nodes, nodes+26, myfunction);
-        
-        for (int i = 0;i < 26;i++)
-            if (nodes[i].ch >= 'a')
-                result += nodes[i].ch;
+                if (!dfs_visit(i, result))
+                    return "";
 
         return result;
     }
