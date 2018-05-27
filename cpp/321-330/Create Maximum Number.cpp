f016053d8ece1cb533f1dcf38817267cfb84ef0d
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-27 12:30:40 -0700

    321. Create Maximum Number

--- cpp/321-330/Create Maximum Number.cpp
+++ cpp/321-330/Create Maximum Number.cpp
@@ -2,7 +2,7 @@ class Solution {
     
     vector<int> merge(vector<int> nums1, vector<int> nums2) {
         vector<int> ans;
-        int i(0),j(0);
+        int i = 0, j = 0;
         while(i < nums1.size() && j < nums2.size()) {
             if(greater(nums1, nums2, i, j)) {
                 ans.push_back(nums1[i]);
@@ -13,16 +13,16 @@ class Solution {
             }
         }
         if(i == nums1.size())
-        while(j < nums2.size())
-        ans.push_back(nums2[j++]);
+            while(j < nums2.size())
+                ans.push_back(nums2[j++]);
         else if(j == nums2.size())
-        while(i < nums1.size())
-        ans.push_back(nums1[i++]);
+            while(i < nums1.size())
+                ans.push_back(nums1[i++]);
         
         return ans;
     }
     
-    vector<int> getMax(vector<int> nums, int t) {
+    vector<int> getMax(const vector<int>& nums, int t) {
         stack<int> ans;
         vector<int> res;
         for(int i = 0;i < nums.size();i++) {
@@ -38,11 +38,10 @@ class Solution {
         return res;
     }
     
-    bool greater(vector<int> &nums1, vector<int> &nums2, int i, int j) {
-        int len = min(nums1.size(), nums2.size());
-        while(i < nums1.size() && j < nums2.size()) {
-            if(nums1[i] > nums2[j]) return true;
-            if(nums1[i] < nums2[j]) return false;
+    bool greater(const vector<int> &nums1, const vector<int> &nums2, int i, int j) {
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] > nums2[j]) return true;
+            if (nums1[i] < nums2[j]) return false;
             i++;
             j++;
         }
@@ -55,17 +54,14 @@ public:
         int len2 = nums2.size();
         vector<int> res;
         
-        int start = max(0, k-len2);
+        int start = max(0, k - len2);
         int end = min(k, len1) + 1;
         
-        for(int i = start;i < end;i++) {
+        for (int i = start; i < end; i++) {
             vector<int> tmp = merge(getMax(nums1, i), getMax(nums2, k - i));
-            for(auto e : tmp)
-            if(res.size() == 0) res = tmp;
-            if(greater(tmp, res, 0, 0)) res = tmp;
+            if (greater(tmp, res, 0, 0)) res = tmp;
         }
         
         return res;
     }
-    
 };
