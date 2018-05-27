a2147a22f7d587dfedfc70df41f1c1641b1aba91
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-26 23:42:56 -0700

    282. Expression Add Operators

--- cpp/281-290/Expression Add Operators.cpp
+++ cpp/281-290/Expression Add Operators.cpp
@@ -1,22 +1,18 @@
 class Solution {
     
-    void help(vector<string>& result, string& num, int& target, int index, char op, int pv, int nv, string res)
-    {
-        if(index == num.size() && nv == target)
-        {
+    void help(vector<string>& result, string& num, int& target, int index, char op, int pv, int nv, string res) {
+        if (index == num.size() && nv == target) {
             result.push_back(res);
             return;
         }
         
-        for(int i = 1;i <= num.size()-index;i++)
-        {
+        for (int i = 1; i <= num.size()-index; i++) {
             string curnum = num.substr(index, i);
             int v = stol(curnum);
-            if(curnum.length() != to_string(v).length()) continue;
+            if (curnum.length() != to_string(v).length()) continue;
             help(result, num, target, index+i, '+', v, nv+v, res+'+'+curnum);
             help(result, num, target, index+i, '-', v, nv-v, res+'-'+curnum);
             help(result, num, target, index+i, op, pv*v, op == '+'? nv - pv + pv*v : (op == '-'? nv + pv - pv*v : pv*v), res+'*'+curnum);
-            
         }
     }
     
@@ -25,11 +21,10 @@ public:
         
         vector<string> result;
         
-        for(int i = 1;i <= num.size();i++)
-        {
+        for (int i = 1; i <= num.size(); i++) {
             string curnum = num.substr(0, i);
             int v = stol(curnum);
-            if(curnum.length() != to_string(v).length()) continue;
+            if (curnum.length() != to_string(v).length()) continue;
             help(result, num, target, i, '!', v, v, curnum);
         }
         
