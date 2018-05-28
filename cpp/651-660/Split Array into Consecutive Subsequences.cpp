bf3920b395660315105138bc56f50658537c171f
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-27 18:15:20 -0700

    659. Split Array into Consecutive Subsequences

--- cpp/651-660/Split Array into Consecutive Subsequences.cpp
+++ cpp/651-660/Split Array into Consecutive Subsequences.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    bool isPossible(vector<int>& nums) {
-        map<int, int> mymap;
-        for (int num : nums) mymap[num]++;
-        while (true) {
-            if (mymap.empty()) return true;
-            pair<int, int> mypair = *mymap.begin();
-            int temp = mypair.first;
-            mymap[temp]--;
-            int pick = 1;
-            if (mymap[temp] == 0) mymap.erase(temp);
-        
-            while (true) {
-                temp++;
-                auto it = mymap.find(temp);
-                if (pick < 3) {
-                    if (it == mymap.end() || mymap[temp] == 0) return false;
-                } else {
-                    if (it == mymap.end()) break;
-                    auto itt = mymap.find(temp-1);
-                    int numm = 0;
-                    if (itt != mymap.end()) numm = mymap[temp-1];
-                    if (mymap[temp] == 0 || mymap[temp] <= numm) break;
-                }
-                mymap[temp]--;
-                if (mymap[temp] == 0) mymap.erase(temp);
-                pick++;
-            }
-            if (pick < 3) return false;
-        }
-    }
+	bool isPossible(vector<int>& nums) {
+		unordered_map<int, priority_queue<int, vector<int>, std::greater<int>>> backs;
+
+		// Keep track of the number of sequences with size < 3
+		int need_more = 0;
+
+		for (int num : nums) {
+			if (! backs[num - 1].empty()) {	
+                // There exists a sequence that ends in num-1
+				// Append 'num' to this sequence
+				// Remove the existing sequence
+				// Add a new sequence ending in 'num' with size incremented by 1 
+				int count = backs[num - 1].top();
+				backs[num - 1].pop();
+				backs[num].push(++count);
+
+				if (count == 3)
+					need_more--;
+			} else {	
+                // There is no sequence that ends in num-1
+				// Create a new sequence with size 1 that ends with 'num'
+				backs[num].push(1);
+				need_more++;
+			}
+		}
+		return need_more == 0;
+	}
 };
