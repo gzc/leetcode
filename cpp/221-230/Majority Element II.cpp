6fb035c5635ae8679b83988f4f72dbcf9f492d6a
Zhenchao Gan <zhenchaogan@gmail.com>
2018-05-28 18:56:03 -0700

    229. Majority Element II

--- cpp/221-230/Majority Element II.cpp
+++ cpp/221-230/Majority Element II.cpp
@@ -1,57 +1,34 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        
-        int n1,n2,c1(-1),c2(-1);
-        vector<int>res;
-        
-        for(auto e : nums)
-        {
-            if(c1 < 0)
-            {
-                n1 = e;
-                c1 = 1;
-                continue;
-            }
-            
-            if(c2 < 0 && e != n1)
-            {
-                n2 = e;
-                c2 = 1;
-                continue;
-            }
-            
-            if(e == n1) c1++;
-            else if(e == n2) c2++;
-            else {
-                if(c1 < c2)
-                {
-                    c1--;
-                    if(c1 == 0){
-                        n1 = e;
-                        c1 = 1;
-                    }
-                } else {
-                    c2--;
-                    if(c2 == 0){
-                        n2 = e;
-                        c2 = 1;
-                    }
-                }
+        int cnt1 = 0, cnt2 = 0, a = 0, b = 1;
+
+        for(int n: nums){
+            if (a == n) {
+                cnt1++;
+            } else if (b == n) {
+                cnt2++;
+            } else if (cnt1 == 0) {
+                a = n;
+                cnt1 = 1;
+            } else if (cnt2 == 0){
+                b = n;
+                cnt2 = 1;
+            } else {
+                cnt1--;
+                cnt2--;
             }
         }
-        
-        int count = nums.size()/3;
-        c1 = c2 = 0;
-        for(auto e : nums)
-        {
-            if(e == n1) c1++;
-            if(e == n2) c2++;
+
+        cnt1 = cnt2 = 0;
+        for (int n: nums){
+            if (n == a)   cnt1++;
+            else if (n == b)  cnt2++;
         }
-        
-        if(c1 > count) res.push_back(n1);
-        if(c2 > count) res.push_back(n2);
-        
+
+        vector<int> res;
+        if (cnt1 > nums.size()/3)   res.push_back(a);
+        if (cnt2 > nums.size()/3)   res.push_back(b);
         return res;
     }
 };
