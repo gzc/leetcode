83ac36bcb59636661bfc5dd5449d9497aa1ee2e6
Zhenchao Gan <zhenchaogan@gmail.com>
2018-06-27 16:38:13 -0700

    859. Buddy Strings

---
+++ cpp/851-860/Buddy Strings.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool buddyStrings(string A, string B) {
+        if (A.length() != B.length()) return false;
+        if (A == B && set<char>(A.begin(), A.end()).size() < A.size()) return true;
+        vector<int> dif;
+        for (int i = 0; i < A.length(); ++i) if (A[i] != B[i]) dif.push_back(i);
+        return dif.size() == 2 && A[dif[0]] == B[dif[1]] && A[dif[1]] == B[dif[0]];
+    }
+};
