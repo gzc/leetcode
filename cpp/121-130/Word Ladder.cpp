e6e7286eb506a119928eea78001e8add58a97470
Zhenchao Gan <zhenchaogan@gmail.com>
2018-06-27 16:09:45 -0700

    127. Word Ladder

--- cpp/121-130/Word Ladder.cpp
+++ cpp/121-130/Word Ladder.cpp
@@ -1,26 +1,27 @@
 class Solution {
-    public:
-        int ladderLength(string start, string end, unordered_set<string> &dict) {
-            unordered_map<string, int> dis; // store the distance from start to the current word
-            queue<string> q; // FIFO for bfs purpose
-            dis[start] = 1;
-            q.push(start);
-            while (!q.empty()) {
-                string word = q.front();
-                q.pop();
-                if (word == end) break;
-                for (int i = 0; i < word.size(); i++) {
-                    for (int j = 0; j < 26; j++) {
-                        string newWord = word;
-                        newWord[i] = 'a' + j;
-                        if (dict.count(newWord) > 0 && dis.count(newWord) == 0) {
-                            dis[newWord] = dis[word] + 1;
-                            q.push(newWord);
-                        }
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> dict(begin(wordList), end(wordList));
+        unordered_map<string, int> dis; // store the distance from start to the current word
+        queue<string> q; // FIFO for bfs purpose
+        dis[beginWord] = 1;
+        q.push(beginWord);
+        while (!q.empty()) {
+            string word = q.front();
+            q.pop();
+            if (word == endWord) break;
+            for (int i = 0; i < word.size(); i++) {
+                for (int j = 0; j < 26; j++) {
+                    string newWord = word;
+                    newWord[i] = 'a' + j;
+                    if (dict.count(newWord) > 0 && dis.count(newWord) == 0) {
+                        dis[newWord] = dis[word] + 1;
+                        q.push(newWord);
                     }
                 }
             }
-            return dis[end];
         }
-        
+        if (dis.count(endWord) == 0) return 0;
+        return dis[endWord];
+    }
 };
