9ee11f9e3483c9e9133f4ae40aab20ea96335f49
Zhenchao Gan <zhenchaogan@gmail.com>
2018-06-27 23:08:43 -0700

    378. Kth Smallest Element in a Sorted Matrix

--- cpp/371-380/Kth Smallest Element in a Sorted Matrix.cpp
+++ cpp/371-380/Kth Smallest Element in a Sorted Matrix.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-        int m(matrix.size()), n(matrix[0].size());
-        priority_queue<int> pq;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                pq.emplace(matrix[i][j]);
-                if (pq.size() > k) {
-                    pq.pop();
-                }
-            }
-        }
-        return pq.top();
-    }
+	int kthSmallest(vector<vector<int>>& matrix, int k) {
+		int n = matrix.size();
+		int L = matrix[0][0], R = matrix[n - 1][n - 1];
+		while (L < R) {
+			int mid = L + ((R - L) >> 1);
+			int temp = search_lower_than_mid(matrix, mid);
+			if (temp < k) L = mid + 1;
+			else R = mid;
+		}
+		return L;
+	}
+	int search_lower_than_mid(const vector<vector<int>>& matrix, int x) {
+        int n = matrix.size();
+		int i = n - 1, j = 0, cnt = 0;
+		while (i >= 0 && j < n) {
+			if (matrix[i][j] <= x) {
+				j++;
+				cnt += i + 1;
+			}
+			else i--;
+		}
+		return cnt;
+	}
 };
