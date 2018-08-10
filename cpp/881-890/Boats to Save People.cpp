ae31226cd9bc2169dc9557aacd4e147c8e904292
Zhenchao Gan <zhenchaogan@gmail.com>
2018-08-09 20:08:23 -0700

    885. Boats to Save People

---
+++ cpp/881-890/Boats to Save People.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        
+        int i = 0, j = people.size() - 1;
+        int ans = 0;
+        
+        while (i <= j) {
+            if (people[i] + people[j] <= limit) {
+                i++;
+            }
+            j--;
+            ans++;
+        }
+        
+        return ans;
+    }
+};
