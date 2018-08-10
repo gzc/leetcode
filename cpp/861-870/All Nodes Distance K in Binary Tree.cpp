9d64cccdc09c58cccac4c5f76f8a74309c7e5fd9
Zhenchao Gan <zhenchaogan@gmail.com>
2018-08-09 20:54:37 -0700

    863. All Nodes Distance K in Binary Tree

---
+++ cpp/861-870/All Nodes Distance K in Binary Tree.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> ans;   
+    map<TreeNode*, TreeNode*> parent;  // son=>parent  
+    set<TreeNode*> visit;    //record visied node
+    
+    void findParent(TreeNode* node){
+        if(!node ) return;
+        if( node->left ){
+            parent[node->left] = node;
+            findParent(node->left);
+        }
+        if( node->right){
+            parent[node->right] = node;
+            findParent(node->right);
+        }
+    }
+    
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int K) {
+        if( !root ) return {};
+        
+        findParent(root);
+        dfs(target, K );
+        return ans;
+    }
+    void dfs( TreeNode* node, int K){
+        if( visit.find(node) != visit.end() )
+            return;
+        visit.insert(node);
+        if( K == 0 ){
+            ans.push_back(node->val);
+            return;
+        }
+        if( node->left ){
+            dfs(node->left, K-1);
+        }
+        if( node->right){
+            dfs(node->right, K-1);
+        }
+        TreeNode* p = parent[node];
+        if( p )
+            dfs(p, K-1);
+    }
+};
