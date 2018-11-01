6ee1e36729f716902dd140f877ba4033fc4bc9a7
Zhenchao Gan <zhenchaogan@gmail.com>
2018-10-31 21:13:44 -0700

    932. Beautiful Array

---
+++ cpp/931-940/Beautiful Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> beautifulArray(int N) {
+        vector<int> res = {1};
+        while (res.size() < N) {
+            vector<int> tmp;
+            for (int i : res) if (i * 2 - 1 <= N) tmp.push_back(i * 2 - 1);
+            for (int i : res) if (i * 2 <= N) tmp.push_back(i * 2);
+            res = tmp;
+        }
+        return res;
+    }
+};
+
