f9c2cb844a44cd22fc46f2979ed6a82bf4ebbf17
Zhenchao Gan <zhenchaogan@gmail.com>
2018-11-17 13:18:21 -0800

    115. Distinct Subsequences

--- cpp/111-120/Distinct Subsequences.cpp
+++ cpp/111-120/Distinct Subsequences.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    int numDistinct(string S, string T) {
+    int numDistinct(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+
+        vector<vector<int>> dp(n+1, vector<int>(m + 1, 0));
         
-        int n = S.length();
-        int m = T.length();
+        for(int k = 0; k <= n; k++)
+            dp[k][0] = 1;
         
-        int p[n+1][m+1];
-        memset(p, 0, sizeof(p));
-        
-        for(int k = 0;k <= n;k++)
-            p[k][0] = 1;
-        
-        for(int i = 1;i <= n;i++)
-        {
-            for(int j = 1;j <= m;j++)
-            {
-                p[i][j] = p[i-1][j];
-                if(S[i-1] == T[j-1])
-                    p[i][j] += p[i-1][j-1];
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                dp[i][j] = dp[i-1][j];
+                if(s[i-1] == t[j-1])
+                    dp[i][j] += dp[i-1][j-1];
             }
         }
         
-        return p[n][m];
+        return dp[n][m];
     }
-    
 };
