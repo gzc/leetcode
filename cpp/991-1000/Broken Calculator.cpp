acf5f35f3460e75925cb2bb18ec0575833e9185c
Zhenchao Gan <zhenchaogan@gmail.com>
2019-02-18 14:21:39 -0800

    991. Broken Calculator

---
+++ cpp/991-1000/Broken Calculator.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    map<int, int> cache;
+public:
+    int brokenCalc(int X, int Y) {
+        int ans = 0;
+        while (Y > X) {
+            ans++;
+            if (Y % 2 == 1)
+                Y++;
+            else
+                Y /= 2;
+        }
+
+        return ans + X - Y;
+    }
+};
