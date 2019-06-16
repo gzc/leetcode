7930138029aa726505f11bdf08b17d8c55854149
Zhenchao Gan <zhenchaogan@gmail.com>
2019-06-15 21:49:39 -0700

    1092. Shortest Common Supersequence

--- cpp/1001-10000/1091-1100/Shortest Common Supersequence.cpp
+++ cpp/1001-10000/1091-1100/Shortest Common Supersequence.cpp
@@ -1,6 +1,7 @@
 class Solution {
     
-    string lcs(const string& str1, const string& str2) {
+public:
+    string shortestCommonSupersequence(string str1, string str2) {
         int m = str1.size();
         int n = str2.size();
         vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
@@ -9,8 +10,12 @@ class Solution {
 
         for (int i = 0; i <= m; i++) {
             for (int j = 0; j <= n; j++) {
-                if (i == 0 || j == 0) {
+                if (i == 0) {
                     dp[i][j] = 0;
+                    from[{i, j}] = {0, j-1};
+                } else if (j == 0) {
+                    dp[i][j] = 0;
+                    from[{i,j}] = {i-1, 0};
                 } else if (str1[i-1] == str2[j-1]) {
                     dp[i][j] = dp[i-1][j-1] + 1;
                     from[{i, j}] = {i-1, j-1};
@@ -29,58 +34,20 @@ class Solution {
 
         string ans;
 
-        while (i > 0 && j > 0) {
+        while (!(i == 0 && j == 0)) {
             auto p = from[{i, j}];
 
-            if (dp[i][j] == dp[p.first][p.second] + 1) {
+            if (i == p.first + 1 && j == p.second + 1) {
+                ans = str1[i-1] + ans;
+            } else if (i == p.first) {
+                ans = str2[j-1] + ans;
+            } else {
                 ans = str1[i-1] + ans;
             }
 
             i = p.first;
             j = p.second;
         }
-
-        return ans;
-    }
-    
-public:
-    string shortestCommonSupersequence(string str1, string str2) {
-        string temp = lcs(str1, str2);
-
-        map<int, string> mymap;
-
-        int j = 0;
-        for (int i = 0; i < temp.size(); i++) {
-            char ch = temp[i];
-            string s;
-            while (str1[j] != ch) {
-                s = s + str1[j];
-                j++;
-            }
-            j++;
-            mymap[i] = s;
-        }
-        mymap[temp.size()] = str1.substr(j);
-
-        j = 0;
-        for (int i = 0; i < temp.size(); i++) {
-            char ch = temp[i];
-            string s;
-            while (str2[j] != ch) {
-                s = s + str2[j];
-                j++;
-            }
-            j++;
-            mymap[i] += s;
-        }
-        mymap[temp.size()] += str2.substr(j);
-
-        string ans;
-        for (int i = 0; i < temp.size(); i++) {
-            ans += mymap[i];
-            ans = ans + temp[i];
-        }
-        ans += mymap[temp.size()];
 
         return ans;
     }
