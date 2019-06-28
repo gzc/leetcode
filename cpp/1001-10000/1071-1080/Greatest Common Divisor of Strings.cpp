76ee9e58b68a32078c6ef6847f63e6316fa62a4a
Zhenchao Gan <zhenchaogan@gmail.com>
2019-06-27 21:27:53 -0700

    1071. Greatest Common Divisor of Strings

---
+++ cpp/1001-10000/1071-1080/Greatest Common Divisor of Strings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    
+    bool IsValid(const string& str, const string& candidate) {
+        for (int i = 0; i < str.length(); i += candidate.length()) {
+            if (str.substr(i, candidate.length()) == candidate) {
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+    
+public:
+    string gcdOfStrings(string str1, string str2) {
+        int m = str1.length();
+        int n = str2.length();
+        int gcd = __gcd(m, n);
+        
+        string candidate = str1.substr(0, gcd);
+        
+        if (!IsValid(str1, candidate)) return "";
+        if (!IsValid(str2, candidate)) return "";
+        
+        return candidate;
+    }
+};
