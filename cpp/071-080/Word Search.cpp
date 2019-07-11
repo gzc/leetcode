0fb4af60eeaf6d44972507340d5758de1b9c1552
Zhenchao Gan <zhenchaogan@gmail.com>
2019-07-10 22:00:05 -0700

    79. Word Search

--- cpp/071-080/Word Search.cpp
+++ cpp/071-080/Word Search.cpp
@@ -1,66 +1,31 @@
 class Solution {
-    
-    string::iterator be;
-    
-    bool help(vector<vector<char> > &board, string::iterator it1, string::iterator it2, int ii, int jj)
-    {
-        if(it1 == it2)
+public:
+    bool dfs(vector<vector<char>>& board, const string &word, int i, int j, int ind) {
+        if (ind == word.size()) {
             return true;
-        
-        if(it1 == be)
-        {
-            for(int i = 0;i < board.size();i++)
-            {
-                for(int j = 0;j < board[i].size();j++)
-                {
-                    if(board[i][j] == *it1)
-                    {
-                        board[i][j] = 0;
-                        bool t = help(board, it1+1, it2, i, j);
-                        board[i][j] = *it1;
-                        if(t) return true;
-                    }
-                }
-            }
         }
-        else
-        {
-            for(int d = -1;d <= 1;d = d+2)
-            {
-                if( (ii+d >= 0) && (ii+d <= board.size()-1)  )
-                {
-                    if(board[ii+d][jj] == 0) {}
-                    else if(board[ii+d][jj] == *it1)
-                    {
-                        board[ii+d][jj] = 0;
-                        bool t = help(board, it1+1, it2, ii+d, jj);
-                        board[ii+d][jj] = *it1;
-                        if(t) return true;
-                    }
-                }
-                
-                if( (jj+d >= 0) && (jj+d <= board[ii].size()-1)  )
-                {
-                    if(board[ii][jj+d] == 0) {}
-                    else if(board[ii][jj+d] == *it1)
-                    {
-                        board[ii][jj+d] = 0;
-                        bool t = help(board, it1+1, it2, ii, jj+d);
-                        board[ii][jj+d] = *it1;
-                        if(t) return true;
-                    }
-                }
-                
-            }
+        if (i >= board.size() || j >= board[0].size() || i < 0 || j < 0) return false;
+        if (board[i][j] == 0) return false;
+        if (board[i][j] == word[ind]) {
+            board[i][j] = 0;
+            if (dfs(board, word, i+1, j, ind + 1)) return true;
+            if (dfs(board, word, i, j+1, ind + 1)) return true;
+            if (dfs(board, word, i-1, j, ind + 1)) return true;
+            if (dfs(board, word, i, j-1, ind + 1)) return true;
+            board[i][j] = word[ind];
         }
-        
         return false;
     }
     
-public:
-    bool exist(vector<vector<char> > &board, string word) {
-        be = word.begin();
-        return help(board, word.begin(), word.end(), 0, 0);
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        if (m == 0) return false;
+        int n = board[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, word, i, j, 0)) return true;
+            }
+        }
+        return false;
     }
-    
 };
