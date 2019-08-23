9ae28868f4887d0c2187731ab6cc8fec08d52f65
Zhenchao Gan <zhenchaogan@gmail.com>
2019-08-22 22:36:04 -0700

    309. Best Time to Buy and Sell Stock with Cooldown

--- cpp/301-310/Best Time to Buy and Sell Stock with Cooldown.cpp
+++ cpp/301-310/Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -15,3 +15,22 @@ public:
         return max(sell[n], cold[n]);
     }
 };
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty()) return 0;
+        int buy = -prices[0];
+        int sell = 0;
+        int cooldown = 0;
+        
+        for (int i = 1; i < prices.size(); i++) {
+            int old_cooldown = cooldown;
+            cooldown = max(cooldown, sell);
+            sell = max(sell, buy + prices[i]);
+            buy = max(buy, old_cooldown - prices[i]);
+        }
+        
+        return max(sell, cooldown);
+    }
+};
