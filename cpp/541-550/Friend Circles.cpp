536c02743898f5249f311b66e831f767c3f1a36d
Zhenchao Gan <zhenchaogan@gmail.com>
2019-11-19 23:08:28 -0800

    547. Friend Circles

--- cpp/541-550/Friend Circles.cpp
+++ cpp/541-550/Friend Circles.cpp
@@ -1,73 +1,27 @@
-class UF {
-    
-private:
-    vector<int> parent;
-    vector<int> rank;
-    int count;
-    int N;
-    
-    bool validate(int p) {
-        return (p >= 0 && p < N);
-    }
-    
-public:
-    
-    UF(int N) : parent(N), rank(N, 0), N(N), count(N) {
-        for (int i = 0; i < N; i++) {
-            parent[i] = i;
-        }
-    }
-    
-    int find(int p) {
-        if (!validate(p)) return -1;
-        while (p != parent[p]) {
-            parent[p] = parent[parent[p]];    // path compression by halving
-            p = parent[p];
-        }
-        return p;
-    }
-    
-    int getCount() const {
-        return count;
-    }
-    
-    bool connected(int p, int q) {
-        return find(p) == find(q);
-    }
-    
-    void Union(int p, int q) {
-        int rootP = find(p);
-        int rootQ = find(q);
-        if (rootP == rootQ) return;
-        
-        // make root of smaller rank point to root of larger rank
-        if (rank[rootP] < rank[rootQ]) parent[rootP] = rootQ;
-        else if (rank[rootP] > rank[rootQ]) parent[rootQ] = rootP;
-        else {
-            parent[rootQ] = rootP;
-            rank[rootP]++;
-        }
-        count--;
-    }
-    
-    ~UF() {}
-};
-
-
 class Solution {
 public:
-    int findCircleNum(vector<vector<int>>& M) {
-        int n = M.size();
-        UF uf(n);
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (M[i][j]) {
-                    uf.Union(i, j);
-                }
+    void dfs(int i, vector<vector<int>>& M, vector<int>& colors) {
+        colors[i] = 1;
+        int m = M.size();
+        for (int j = 0; j < m; j++) {
+            if (M[i][j] == 0) continue;
+            if (colors[j] == 0) {
+                dfs(j, M, colors);
             }
         }
-        
-        return uf.getCount();
+    }
+    
+    int findCircleNum(vector<vector<int>>& M) {
+        int m = M.size();
+        if (m == 0) return 0;
+        int res = 0;
+        vector<int> colors(m, 0);
+        for (int i = 0; i < m; i++) {
+            if (colors[i] == 0) {
+                res++;
+                dfs(i, M, colors);
+            }
+        }
+        return res;
     }
 };
