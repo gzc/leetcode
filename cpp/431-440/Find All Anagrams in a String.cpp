9a592cca5eac0a0257cfe0a716ed8ac0c550ba37
Zhenchao Gan <zhenchaogan@gmail.com>
2019-11-28 17:02:32 -0800

    438. Find All Anagrams in a String

---
+++ cpp/431-440/Find All Anagrams in a String.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        unordered_map<char, int> mymap;
+        int psize = p.size();
+        vector<int> ans;
+        
+        for (char ch : p) mymap[ch]++;
+        int kinds = mymap.size();
+        
+        for (int i = 0; i < s.size(); i++) {
+            char ch = s[i];
+            mymap[ch]--;
+            
+            if (mymap[ch] == 0) kinds--;
+            
+            if (i >= psize) {
+                char poped_ch = s[i - psize];
+                int times = ++mymap[poped_ch];
+                if (times > 0) kinds++;
+            }
+            
+            if (kinds == 0) ans.push_back(i - psize + 1);
+        }
+        
+        return ans;
+    }
+};
