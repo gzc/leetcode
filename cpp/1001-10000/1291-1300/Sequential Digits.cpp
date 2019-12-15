68f925b4c9e41f2dec8f90ef1e795c368a2e9b2d
Zhenchao Gan <zhenchaogan@gmail.com>
2019-12-14 23:06:44 -0800

    1291. Sequential Digits

---
+++ cpp/1001-10000/1291-1300/Sequential Digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        string tmp = "123456789";
+        
+        vector<int> res;
+        for (int i = 0; i < 9; i++) {
+            for (int j = i+1; j <= 9; j++) {
+                int len = j - i;
+                string str = tmp.substr(i, len);
+                long v = stol(str);
+                if (v >= low && v <= high) res.push_back(v);
+            }
+        }
+        
+        sort(res.begin(), res.end());
+        return res;
+    }
+};
