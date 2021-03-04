5f69d99b38be072cefd3828355489d3eb2f65cad
Zhenchao Gan <zhenchaogan@gmail.com>
2021-03-04 00:02:42 -0800

    295. Find Median from Data Stream

--- cpp/291-300/Find Median from Data Stream.cpp
+++ cpp/291-300/Find Median from Data Stream.cpp
@@ -1,21 +1,28 @@
 class MedianFinder {
-    std::priority_queue<long, std::vector<long>, std::less<long> > large;
-    std::priority_queue<long, std::vector<long>, std::greater<long> > small;
+    priority_queue<int, vector<int>, std::less<int>> maximum_heap;
+    priority_queue<int, vector<int>, std::greater<int>> minimum_heap;
 public:
-
+    /** initialize your data structure here. */
+    MedianFinder() {
+        
+    }
+    
     void addNum(int num) {
-        small.push(num);
-        large.push(small.top());
-        small.pop();
-        if (small.size() < large.size()) {
-            small.push(large.top());
-            large.pop();
+        minimum_heap.push(num);
+        
+        maximum_heap.push(minimum_heap.top());
+        minimum_heap.pop();
+        
+        if (minimum_heap.size() < maximum_heap.size()) {
+            minimum_heap.push(maximum_heap.top());
+            maximum_heap.pop();
         }
     }
-
+    
     double findMedian() {
-        return small.size() > large.size()
-               ? small.top()
-               : (small.top() + large.top()) / 2.0;
+        if (maximum_heap.size() == minimum_heap.size()) {
+            return (minimum_heap.top()+maximum_heap.top()) / 2.0;
+        }
+        return minimum_heap.top();
     }
 };
