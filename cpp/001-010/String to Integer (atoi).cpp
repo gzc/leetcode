a4a16465724d11fa28a4acca4e5f1dd8afdbee29
Zhenchao Gan <zhenchaogan@gmail.com>
2021-03-04 00:09:29 -0800

    8. String to Integer (atoi)

--- cpp/001-010/String to Integer (atoi).cpp
+++ cpp/001-010/String to Integer (atoi).cpp
@@ -1,44 +1,33 @@
 class Solution {
 public:
     int myAtoi(string str) {
-        int r(0);
-        int sign(0);
-        
-        while(str[0] == ' ')
-            str.erase(0, 1);
-        
-        while(str[0] == '+' | str[0] == '-')
-        {
-            if(sign != 0) return 0;
-            else if(str[0] == '+') sign = 1;
-            else sign = -1;
+        while (str[0] == ' ') {
             str.erase(0, 1);
         }
-        if(sign == 0) sign = 1;
         
-        while(str[0] == '0')
+        int sign = 1;
+        if (str[0] == '+' || str[0] == '-') {
+            if (str[0] == '-') sign = -1;
             str.erase(0, 1);
+        }
         
-        size_t len = str.size();
-        
-        for(int i = 0;i < len;i++)
-        {
-            if(!isdigit(str[i])) break;
+        int result = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (!isdigit(str[i])) {
+                break;
+            }
             int v = str[i] - '0';
 
-            if(r > INT_MAX/10)
-            {
-                if(sign == 1) return INT_MAX;
-                if(sign == -1) return INT_MIN;
-            } else if(r == INT_MAX/10)
-            {
-                if(sign == 1 && v > 7) return INT_MAX;
-                if(sign == -1 && v > 8) return INT_MIN;
+            if (result > INT_MAX/10) {
+                if (sign == 1) return INT_MAX;
+                if (sign == -1) return INT_MIN;
+            } else if (result == INT_MAX/10) {
+                if (sign == 1 && v >= 7) return INT_MAX;
+                if (sign == -1 && v >= 8) return INT_MIN;
             }
-            r = 10*r + v;
-
+            result = 10 * result + v;
         }
         
-        return r*sign;
+        return result * sign;
     }
 };
