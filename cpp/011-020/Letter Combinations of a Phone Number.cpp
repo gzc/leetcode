5cd853710c36aa4743925c9bc775779875c58b26
Zhenchao Gan <zhenchaogan@gmail.com>
2021-03-04 00:18:53 -0800

    17. Letter Combinations of a Phone Number

--- cpp/011-020/Letter Combinations of a Phone Number.cpp
+++ cpp/011-020/Letter Combinations of a Phone Number.cpp
@@ -1,28 +1,24 @@
 class Solution {
     
-    string symbols[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-    
 public:
     vector<string> letterCombinations(string digits) {
+        string symbols[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         vector<string> result;
-        if (digits.empty()) return result;
-        result.emplace(result.end(), "");
+        if (digits.empty()) {
+            return result;
+        }
+        result.push_back("");
         
-        for (int i = 0;i < digits.length();i++)
-        {
-            int num = digits[i]-'0';
-            int current_combinations = result.size();
-            for (int j = 0;j < current_combinations;j++)
-            {
-                string s = result[j];
-                string letters = symbols[num];
-                for (int k = 0;k < letters.length();k++)
-                {
-                    string tmp = s+letters[k];
-                    result.emplace(result.end(), tmp);
+        for (char ch : digits) {
+            int num = ch - '0';
+            vector<string> new_result;
+            for (const string& s : result) {
+                for (char symbol : symbols[num]) {
+                    string tmp = s + symbol;
+                    new_result.push_back(tmp);
                 }
             }
-            result.erase(result.begin(), result.begin()+current_combinations);
+            result.swap(new_result);
         }
         
         return result;
