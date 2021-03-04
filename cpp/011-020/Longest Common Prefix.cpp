0fc75e4ec8e4109c302f86bdeabdffdd1883ccaf
Zhenchao Gan <zhenchaogan@gmail.com>
2021-03-04 00:16:24 -0800

    14. Longest Common Prefix

--- cpp/011-020/Longest Common Prefix.cpp
+++ cpp/011-020/Longest Common Prefix.cpp
@@ -1,47 +1,25 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <map>
-
-using namespace std;
-
 class Solution {
 public:
-    string longestCommonPrefix(vector<string> &strs) {
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) {
+            return "";
+        }
+        string prefix = strs[0];
         
-        int len(0);
-        string r("");
-        
-        if(strs.size() == 0) return r;
-        
-        for(;;)
-        {
-            char ch;
-            if(len == strs[0].length()) return r;
-            else ch = strs[0][len];
-            for(int i = 1;i < strs.size();i++)
-            {
-                if(len+1 > strs[i].length())
-                    return r;
-                if(strs[i][len] != ch)
-                    return r;
+        for (int i = 1; i < strs.size(); i++) {
+            const string& str = strs[i];
+            
+            int k = 0;
+            while (k < min(prefix.length(), str.length())) {
+                if (prefix[k] != str[k]) {
+                    break;
+                }
+                k++;
             }
-            r += ch;
-            len++;
+            
+            prefix = prefix.substr(0, k);
         }
         
-        return r;
+        return prefix;
     }
-    
 };
-
-int main()
-{
-    Solution s;
-    vector<string> v;
-    v.push_back("aaa");
-    v.push_back("ab");
-    s.longestCommonPrefix(v);
-    
-    return 0;
-}
