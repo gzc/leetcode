dd415204f18cef132dbffd67f569d2a2757fe248
Zhenchao Gan <zhenchaogan@gmail.com>
2021-03-04 00:08:37 -0800

    7. Reverse Integer

--- cpp/001-010/Reverse Integer.cpp
+++ cpp/001-010/Reverse Integer.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int reverse(int x) {
-        long long int y(x), reversed_value(0);
-        if (y < 0) y = -y;
+        // INT_MAX: 2147483647
+        // INT_MIN: -2147483648
+        int result = 0;
         
-        while (y > 0) {
-            int remain = y%10;
-            reversed_value = 10*reversed_value + remain;
-            y = (y-remain)/10;
+        while (x != 0) {
+            int mod = x % 10;
+            
+            if (result > 214748364 || (result == 214748364 && mod > 7)) {
+                return 0;
+            }
+            if (result < -214748364 || (result == -214748364 && mod == -9)) {
+                return 0;
+            }
+            
+            result = 10 * result + mod;
+            x /= 10;
         }
         
-        if (x < 0) reversed_value = -reversed_value;
-        if (x > 0 && reversed_value > INT_MAX) return 0;
-        if (x < 0 && reversed_value < INT_MIN) return 0;
-        return reversed_value;
+        return result;
     }
 };
