9a04096497ad7dbc4098815837b47dd52a1f239a
Zhenchao Gan <zhenchaogan@gmail.com>
2021-03-04 00:23:50 -0800

    24. Swap Nodes in Pairs

--- cpp/021-030/Swap Nodes in Pairs.cpp
+++ cpp/021-030/Swap Nodes in Pairs.cpp
@@ -1,71 +1,15 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-#include <stack>
-#include <queue>
-
-using namespace std;
-
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
-
 class Solution {
 public:
-    ListNode *swapPairs(ListNode *head) {
-        
-        if(!head || !head->next) return head;
-        ListNode *result = head->next, *_head(result);
-        
-        while(1)
-        {
-            ListNode *tmp = _head->next;
-            _head -> next = head;
-            if(!tmp)
-            {
-                head->next = NULL;
-                break;
-            }
-            if(tmp->next)
-                head->next = tmp->next;
-            else
-            {
-                head->next = tmp;
-                break;
-            }
-            head = tmp;
-            _head = head->next;
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
         }
         
-        return result;
+        ListNode* new_head = head->next;
+        
+        head->next = swapPairs(new_head->next);
+        new_head->next = head;
+        
+        return new_head;
     }
-    
 };
-
-int main()
-{
-    Solution s;
-    
-    ListNode *n1 = new ListNode(1);
-    ListNode *n2 = new ListNode(2);
-    ListNode *n3 = new ListNode(3);
-    ListNode *n4 = new ListNode(4);
-    ListNode *n5 = new ListNode(10);
-    n1->next = n2;
-    n2->next = n3;
-    n3->next = n4;
-    n4->next = n5;
-    
-    ListNode *head = s.swapPairs(n1);
-    while(head)
-    {
-        cout << head->val << endl;
-        head = head->next;
-    }
-    
-    
-    return 0;
-}
