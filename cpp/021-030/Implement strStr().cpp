bc9ff2f4e656f6b173e58eb463833682d6a53d0d
Zhenchao Gan <zhenchaogan@gmail.com>
2021-03-04 01:23:53 -0800

    28. Implement strStr()

--- cpp/021-030/Implement strStr().cpp
+++ cpp/021-030/Implement strStr().cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if (needle.empty()) return 0;
         int m = haystack.length(), n = needle.length();
         for (int i = 0; i < m - n + 1; i++) {
             int j = 0;
-            for (; j < n; j++)
-                if (haystack[i + j] != needle[j])
+            for (; j < n; j++) {
+                if (haystack[i + j] != needle[j]) {
                     break;
-            if (j == n) return i;
+                }
+            }
+            if (j == n) {
+                return i;
+            }
         }
         return -1;
     }
