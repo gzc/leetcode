3c7e3980fadf3d5f7f15b2d22b108208aeab400d
Zhenchao Gan <zhenchaogan@gmail.com>
2021-03-04 20:17:02 -0800

    29. Divide Two Integers

--- cpp/021-030/Divide Two Integers .cpp
+++ cpp/021-030/Divide Two Integers .cpp
@@ -1,29 +1,85 @@
+/*
+ * Solution 1:
+ * Use long type
+ */
 class Solution {
 public:
-    int divide(long long dividend, long long divisor) {
-        if(dividend == INT_MIN && divisor == -1) return INT_MAX;
-        long long result(0), sign(1);
-        if (dividend > 0 && divisor < 0) sign = -1;
-        if (dividend < 0 && divisor > 0) sign = -1;
-        dividend = abs(dividend);
-        divisor = abs(divisor);
-        
-        long long arrays[35] = {0};
-        arrays[0] = divisor;
-        int index = 0;
-        while (arrays[index] < dividend) {
-            arrays[index+1] = arrays[index] + arrays[index];
-            index++;
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN && divisor == -1) {
+            return INT_MAX;
+        }
+        int sign = 1;
+        if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)) {
+            sign = -1;
+        }
+
+        long new_dividend = abs((long)dividend);
+        long new_divisor = abs((long)divisor);
+        long result = 0;
+
+        int power = 0;
+        while (new_divisor <= new_dividend / 2) {
+            new_divisor *= 2;
+            power++;
         }
         
-        while (index >= 0) {
-            if (dividend >= arrays[index]) {
-                result += (1 << index);
-                dividend -= arrays[index];
+        while (power >= 0) {
+            if (new_dividend >= new_divisor) {
+                result += (1 << power);
+                new_dividend -= new_divisor;
             }
-            index--;
+            power--;
+            new_divisor /= 2;
         }
         
-        return sign*result;
+        return (long)sign * result;
+    }
+};
+
+/*
+ * Solution 2:
+ * Don't use long type
+ */
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        if (dividend == INT_MIN) {
+            if (divisor == -1) {
+                return INT_MAX;
+            } 
+            if (divisor > 0) {
+                return -1 + divide(dividend+divisor, divisor);
+            } else {
+                return 1 + divide(dividend-divisor, divisor);
+            }
+        }
+        if (divisor == INT_MIN) {
+            return dividend == INT_MIN ? 1 : 0;
+        }
+        int sign = 1;
+        if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)) {
+            sign = -1;
+        }
+
+        int new_dividend = abs(dividend);
+        int new_divisor = abs(divisor);
+        int result = 0;
+
+        int power = 0;
+        while (new_divisor <= new_dividend / 2) {
+            new_divisor *= 2;
+            power++;
+        }
+        
+        while (power >= 0) {
+            if (new_dividend >= new_divisor) {
+                result += (1 << power);
+                new_dividend -= new_divisor;
+            }
+            power--;
+            new_divisor /= 2;
+        }
+        
+        return sign * result;
     }
 };
