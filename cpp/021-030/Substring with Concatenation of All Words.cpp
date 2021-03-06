93130a9424d493f2eda76a1107e0d36b1eb95a34
Zhenchao Gan <zhenchaogan@gmail.com>
2021-03-06 00:09:52 -0800

    30. Substring with Concatenation of All Words
    LeetCode 30. Substring with Concatenation of All Words

--- cpp/021-030/Substring with Concatenation of All Words.cpp
+++ cpp/021-030/Substring with Concatenation of All Words.cpp
@@ -1,75 +1,36 @@
-#include <iostream>
-#include <unordered_map>
-#include <vector>
-using namespace std;
-
 class Solution {
-    
 public:
-    vector<int> findSubstring(string S, vector<string> &L) {
-        
-        int wl = L[0].length();
-        int wc = L.size();
-        int boundary = S.length() - wl;
+    vector<int> findSubstring(string s, vector<string>& words) {
         vector<int> res;
         unordered_map<string, int> dict;
-        for(auto e : L) dict[e]++;
-        
-        for(int k = 0;k < wl;k++)
-        {
-            unordered_map<string, int> records;
-            int count(0),left(k);
-            
-            for(int i = k;i <= boundary;i += wl)
-            {
-                string str = S.substr(i, wl);
-                if(dict.count(str))
-                {
-                    records[str]++;
-                    if(records[str] > dict[str])
-                    {
-                        while(records[str] > dict[str])
-                        {
-                            string s = S.substr(left, wl);
-                            left += wl;
-                            records[s]--;
-                            if(records[s] < dict[s]) count--;
-                        }
-                    } else {
-                        count++;
+        int len = words[0].size();
+        int n = s.length(), m = words.size();
+        for (const string& word : words) {
+            dict[word]++;
+        }
+        for (int i = 0; i < len; i++) {
+            int cnt = 0;
+            unordered_map<string, int> copy = dict;
+            for (int j = i; j <= n - len; j += len) {
+                string cur = s.substr(j, len);
+                copy[cur]--;
+                if (copy[cur] >= 0) {
+                    cnt++;
+                }
+                
+                int pop_start = j - m * len;
+                if (pop_start >= 0) {
+                    string pop_word = s.substr(pop_start, len);
+                    copy[pop_word]++;
+                    if (copy[pop_word] > 0) {
+                        cnt--;
                     }
-                    
-                    if(count == wc)
-                    {
-                        res.push_back(left);
-                        string s = S.substr(left, wl);
-                        records[s]--;
-                        left += wl;
-                        count--;
-                    }
-                } else {
-                    records.clear();
-                    count = 0;
-                    left = i + wl;
+                }
+                if (cnt == m) {
+                    res.push_back(pop_start + len);
                 }
             }
         }
-        
         return res;
     }
-    
-    
 };
-
-int main()
-{
-    Solution s;
-
-    string a[3] = {"a", "b", "a"};
-    vector<string> ss(a,a+3);
-    vector<int> v = s.findSubstring("abababab", ss);
-    
-    for(int i = 0;i < v.size();i++)
-        cout << v[i] << endl;
-    return 0;
-}
