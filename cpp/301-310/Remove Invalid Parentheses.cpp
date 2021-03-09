37b4706f6833ac94edb214d4f8d7d8547bc89ae0
Zhenchao Gan <zhenchaogan@gmail.com>
2021-03-09 00:05:39 -0800

    301. Remove Invalid Parentheses

--- cpp/301-310/Remove Invalid Parentheses.cpp
+++ cpp/301-310/Remove Invalid Parentheses.cpp
@@ -1,52 +1,129 @@
+// Solution 1: BFS
 class Solution {
     
-    int check(const string &s) {
+    int GetErrors(const string &s) {
         stack<char> mystack;
-        int num(0);
-        for (auto e : s) {
-            if (e == '(') mystack.push(e);
-            else if (e == ')') {
-                if(mystack.empty()) num++;
-                else mystack.pop();
+        int errors = 0;
+        for (char ch : s) {
+            if (ch == '(') {
+                mystack.push(ch);
+            } else if (ch == ')') {
+                if (mystack.empty()) {
+                    errors++;
+                } else {
+                    mystack.pop();
+                }
             }
         }
-        return num + mystack.size();
+        return errors + mystack.size();
     }
     
 public:
     vector<string> removeInvalidParentheses(string s) {
         queue<string> myqueue;
-        map<string, bool> mymap;
+        unordered_set<string> visited;
         myqueue.push(s);
-        mymap[s] = true;
-        vector<string> result;
+        visited.insert(s);
         
-        while(!myqueue.empty()) {
-            string str = myqueue.front();
-            int num1 = check(str);
-            if (num1 == 0) break;
-            myqueue.pop();
-            int i = 0;
-            while(i < str.length()) {
-                if(str[i] != '(' && str[i] != ')') { i++; continue;}
-                string tmp = str.substr(0,i) + str.substr(i+1);
-                int num2 = check(tmp);
-                if(mymap.count(tmp) == 0 && num2 < num1) {
-                    myqueue.push(tmp);
-                    mymap[tmp] = true;
+        int errors = GetErrors(s);
+        while (!myqueue.empty() && errors > 0) {
+            int queue_size = myqueue.size();
+            for (int k = 0; k < queue_size; k++) {
+                string str = myqueue.front();
+                myqueue.pop();
+                int i = 0;
+                while (i < str.length()) {
+                    if (str[i] != '(' && str[i] != ')') {
+                        i++;
+                        continue;
+                    }
+                    string tmp = str.substr(0,i) + str.substr(i+1);
+                    if (visited.count(tmp) == 0) {
+                        int current_errors = GetErrors(tmp);
+                        if (current_errors < errors) {
+                            myqueue.push(tmp);
+                            visited.insert(tmp);
+                        }
+                    }
+    
+                    while (i+1 < str.length() && str[i] == str[i+1]) i++;
+                    i++;
                 }
-                while(i+1 < str.length() && str[i] == str[i+1]) i++;
-                i++;
             }
+            
+            errors--;
         }
         
-        int len = myqueue.front().length();
-        while(!myqueue.empty() && myqueue.front().length() == len) {
+        vector<string> result;
+        while (!myqueue.empty()) {
             string tmp = myqueue.front();
             myqueue.pop();
             result.push_back(tmp);
         }
         
         return result;
+    }
+};
+
+
+// Solution 2: DFS
+class Solution {
+    
+    int GetErrors(const string &s) {
+        stack<char> mystack;
+        int errors = 0;
+        for (char ch : s) {
+            if (ch == '(') {
+                mystack.push(ch);
+            } else if (ch == ')') {
+                if (mystack.empty()) {
+                    errors++;
+                } else {
+                    mystack.pop();
+                }
+            }
+        }
+        return errors + mystack.size();
+    }
+    
+public:
+    
+    void dfs(const string& s, int ind, int validLen, int leftCnt, int rightCnt, string cur, unordered_set<string>& visited, unordered_set<string>& res_set) {
+        if (cur.size() == validLen) {
+            if (leftCnt == rightCnt) {
+                res_set.insert(cur);
+            }
+            return;
+        }
+        if (ind >= s.size()) {
+            return;
+        }
+        
+        string key = to_string(ind) + cur;
+        if (visited.count(key) > 0) return;
+        visited.insert(key);
+        
+        if (s[ind] == '(' || s[ind] == ')') {
+            if (cur.length() + (s.size() - ind) > validLen) {
+                dfs(s, ind+1, validLen, leftCnt, rightCnt, cur, visited, res_set);
+            }
+            if (s[ind] == '(') {
+                if (leftCnt < validLen / 2) {
+                    dfs(s, ind+1, validLen, leftCnt+1, rightCnt, cur + '(', visited, res_set);
+                }
+            } else if (leftCnt > rightCnt) {
+                dfs(s, ind+1, validLen, leftCnt, rightCnt+1, cur + ')', visited, res_set);
+            }
+        } else {
+            dfs(s, ind+1, validLen, leftCnt, rightCnt, cur + s[ind], visited, res_set);
+        }
+    }
+    
+    vector<string> removeInvalidParentheses(string s) {
+        int validLen = s.size() - GetErrors(s);
+        unordered_set<string> visited;
+        unordered_set<string> res_set;
+        dfs(s, 0, validLen, 0, 0, "", visited, res_set);
+        return vector<string>(res_set.begin(), res_set.end());
     }
 };
