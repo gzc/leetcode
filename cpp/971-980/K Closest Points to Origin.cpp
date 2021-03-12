77569d657d69e1d05d91f60b02df3407edd6aeaf
Zhenchao Gan <zhenchaogan@gmail.com>
2021-03-11 22:07:06 -0800

    LeetCode 973. K Closest Points to Origin

--- cpp/971-980/K Closest Points to Origin.cpp
+++ cpp/971-980/K Closest Points to Origin.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
         auto cmp = [&](const vector<int>& a, const vector<int>& b) {
-            return a[0]*a[0]+a[1]*a[1] < b[0]*b[0]+b[1]*b[1]; 
+            return a[0] * a[0] + a[1] * a[1] <  b[0] * b[0] + b[1] * b[1]; 
         };
         
-        sort(points.begin(), points.end(), cmp);
-        vector<vector<int>> ans(points.begin(), points.begin()+K);
+        nth_element(points.begin(), points.begin() + K, points.end(), cmp);
+        // sort(points.begin(), points.end(), cmp);
+        
+        vector<vector<int>> ans(points.begin(), points.begin() + K);
         return ans;
     }
 };
