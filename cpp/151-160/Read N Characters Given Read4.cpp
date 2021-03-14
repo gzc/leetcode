c299936b0761a5e3a143088ec3a6f88f44b660e2
Zhenchao Gan <zhenchaogan@gmail.com>
2021-03-14 14:34:40 -0700

    LeetCode 157. Read N Characters Given Read4

--- cpp/151-160/Read N Characters Given Read4.cpp
+++ cpp/151-160/Read N Characters Given Read4.cpp
@@ -1,25 +1,26 @@
-// Forward declaration of the read4 API.
-int read4(char *buf);
+/**
+ * The read4 API is defined in the parent class Reader4.
+ *     int read4(char *buf4);
+ */
 
 class Solution {
 public:
-    /**
-     * @param buf Destination buffer
-     * @param n   Maximum number of characters to read
-     * @return    The number of characters read
-     */
     int read(char *buf, int n) {
-        int hasread(0);
-        int nn = n;
-        
-        while (n > 0) {
-            int temp = read4(buf);
-            if(temp == 0) break;
-            hasread += temp;
-            n -= temp;
-            buf += temp;
+        char cache[4];
+        int ind = 0;
+        while (true) {
+            int cache_ind = 0;
+            int cache_cnt = read4(cache);
+            if (cache_cnt == 0) {
+                break;
+            }
+            while (cache_cnt > 0 && ind < n) {
+                buf[ind] = cache[cache_ind];
+                cache_ind++;
+                ind++;
+                cache_cnt--;
+            }
         }
-        
-        return min(hasread, nn);
+        return ind;
     }
 };
