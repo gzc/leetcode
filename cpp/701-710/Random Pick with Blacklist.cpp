cd38e91c3b7bc43ca44fc873bcda1b87f76fe93a
Zhenchao Gan <zhenchaogan@gmail.com>
2021-03-18 20:44:53 -0700

    710. Random Pick with Blacklist

--- cpp/701-710/Random Pick with Blacklist.cpp
+++ cpp/701-710/Random Pick with Blacklist.cpp
@@ -1,3 +1,25 @@
+// Solution 1: Binary Search. O(log)
+class Solution {
+    vector<int> v;
+    std::random_device rd;
+    int mod;
+public:
+    Solution(int N, vector<int> blacklist) {
+        mod = N - blacklist.size();
+        sort(blacklist.begin(), blacklist.end());
+        v = blacklist;
+        v.push_back(N);
+        for (int i = 0; i < v.size(); i++) v[i] -= i;
+    }
+    
+    int pick() {
+        int index = rd() % mod;
+        auto it = upper_bound(v.begin(), v.end(), index);
+        return index + (it - v.begin());
+    }
+};
+
+// Solution 2: hash_map; O(1)
 class Solution {
     random_device rd;
     unordered_map<int, int> updated_index;
