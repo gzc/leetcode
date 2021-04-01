f17848fca4360a47ca9ff5a59eb28441e6ced499
Zhenchao Gan <zhenchaogan@gmail.com>
2021-04-01 00:29:46 -0700

    658. Find K Closest Elements

--- cpp/651-660/Find K Closest Elements.cpp
+++ cpp/651-660/Find K Closest Elements.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        
-        auto cmp = [](const pair<int, int> &a, const pair<int, int> &b) {
-            return a.second > b.second || (a.second == b.second && a.first > b.first);
-        };
-
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)> pq(cmp);
-        
-        for (int e : arr) {
-            int v = abs(e - x);
-            pq.push({e, v});
+        auto it = lower_bound(arr.begin(), arr.end(), x);
+        int idx = it - arr.begin();
+        if (idx == arr.size()) {
+            idx--;
+        } else if (idx > 0) {
+            if (abs(arr[idx - 1] - x) <= abs(arr[idx] - x)) {
+                idx = idx - 1;
+            }
         }
         
-        vector<int> res;
-        for (int i = 0; i < k; i++) {
-            pair<int, int> e = pq.top();
-            pq.pop();
-            res.push_back(e.first);
+        int l = idx - 1;
+        int r = idx + 1;
+        for (int i = 2; i <= k; i++) {
+            if (l < 0) {
+                r++;
+            } else if (r >= arr.size()) {
+                l--;
+            } else {
+                if (abs(arr[l] - x) <= abs(arr[r] - x)) {
+                    l--;
+                } else {
+                    r++;
+                }
+            }
         }
-        sort(res.begin(), res.end());
-        return res;
+        
+        return vector<int>(arr.begin() + l + 1, arr.begin() + r);
     }
 };
