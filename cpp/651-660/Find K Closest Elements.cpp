f67a4fc2b8b6f1466a3bdc957cf7903e1b78f76c
Zhenchao Gan <zhenchaogan@gmail.com>
2021-04-01 00:55:12 -0700

    658. Find K Closest Elements

--- cpp/651-660/Find K Closest Elements.cpp
+++ cpp/651-660/Find K Closest Elements.cpp
@@ -1,3 +1,4 @@
+// Solution 1.
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
@@ -28,5 +29,28 @@ public:
         }
         
         return vector<int>(arr.begin() + l + 1, arr.begin() + r);
+    }
+};
+
+
+
+// Solution 2.
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int left = 0;
+        int right = arr.size() - k;
+        
+        while (left < right) {
+            int mid = (left + right) / 2;
+            
+            if (x - arr[mid] > arr[mid + k] - x) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        
+        return vector<int>(arr.begin() + left, arr.begin() + left + k);
     }
 };
