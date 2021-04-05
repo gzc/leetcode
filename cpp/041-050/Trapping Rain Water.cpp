d183bbcfba664cf92e2aeb8a0606b46f59ad921a
Zhenchao Gan <zhenchaogan@gmail.com>
2021-04-04 20:49:40 -0700

    42. Trapping Rain Water

--- cpp/041-050/Trapping Rain Water.cpp
+++ cpp/041-050/Trapping Rain Water.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int result(0),i(0),j(height.size()-1);
-        int maxleft(0), maxright(0);
-        while(i < j)
-        {
+        int i = 0;
+        int j = height.size() - 1;
+        int maxleft = 0, maxright = 0;
+        int res = 0;
+        while(i < j) {
             if(height[i] <= height[j]) {
-                if(height[i] > maxleft) maxleft = height[i];
-                else result += (maxleft - height[i]);
+                if (height[i] > maxleft) maxleft = height[i];
+                else res += (maxleft - height[i]);
                 i++;
             } else {
-                if(height[j] > maxright) maxright = height[j];
-                else result += (maxright - height[j]);
+                if (height[j] > maxright) maxright = height[j];
+                else res += (maxright - height[j]);
                 j--;
             }
         }
-        return result;
+        return res;
     }
 };
