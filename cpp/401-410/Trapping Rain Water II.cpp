6335e178f5b49b380f164f58abe95f956dc4a238
Zhenchao Gan <zhenchaogan@gmail.com>
2021-04-04 22:14:55 -0700

    407. Trapping Rain Water II

--- cpp/401-410/Trapping Rain Water II.cpp
+++ cpp/401-410/Trapping Rain Water II.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
     int trapRainWater(vector<vector<int>>& heightMap) {
-        if (heightMap.empty()) return 0;
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> que;
         int row = heightMap.size(), col = heightMap[0].size();
         vector<vector<bool>> visited(row, vector<bool>(col, false));
-        int ans = 0, Max = INT_MIN;
+        int minimum = INT_MAX;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if (!(i==0 || i==row-1 || j==0 || j==col-1)) continue;
-                que.push(make_pair(heightMap[i][j], i*col+j));
-                visited[i][j] = true, Max = min(Max, heightMap[i][j]);
+                que.push({heightMap[i][j], i * col + j});
+                visited[i][j] = true;
+                minimum = min(minimum, heightMap[i][j]);
             }
         }
-        vector<vector<int>> dir{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        
+        pair<int, int> dir[4] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        int ans = 0;
         while (!que.empty()) {
-            auto val = que.top(); que.pop();
-            int height = val.first, x = val.second/col, y = val.second%col;
-            Max = max(Max, height);
-            for (auto d: dir) {
-                int x2 = x + d[0], y2 = y + d[1];
-                if (x2>=row || x2<0 || y2<0 || y2>=col || visited[x2][y2]) continue;
-                visited[x2][y2] = true;
-                if(heightMap[x2][y2] < Max) ans += Max - heightMap[x2][y2];
-                que.push(make_pair(heightMap[x2][y2], x2*col+y2));
+            pair<int, int> val = que.top();
+            que.pop();
+            int height = val.first;
+            int x = val.second / col;
+            int y = val.second % col;
+            minimum = max(minimum, height);
+            for (const pair<int, int>& d: dir) {
+                int nx = x + d.first;
+                int ny = y + d.second;
+                if (nx >= row || nx < 0 || ny < 0 || ny >= col || visited[nx][ny]) continue;
+                visited[nx][ny] = true;
+                if (heightMap[nx][ny] < minimum) {
+                    ans += minimum - heightMap[nx][ny];
+                }
+                que.push({heightMap[nx][ny], nx * col + ny});
             }
         }
         return ans;
