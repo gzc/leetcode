847b1fac53709f7c7b30710ab0afc83e8f1b7653
Zhenchao Gan <zhenchaogan@gmail.com>
2021-04-11 23:54:58 -0700

    680. Valid Palindrome II

--- cpp/671-680/Valid Palindrome II.cpp
+++ cpp/671-680/Valid Palindrome II.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
     bool validPalindrome(string s) {
-        for (int i = 0, j = s.size() - 1; i < j; i++, j--)
+        for (int i = 0, j = s.size() - 1; i < j; i++, j--) {
             if (s[i] != s[j]) {
                 int i1 = i, j1 = j - 1, i2 = i + 1, j2 = j;
-                while (i1 < j1 && s[i1] == s[j1]) {i1++; j1--;};
-                while (i2 < j2 && s[i2] == s[j2]) {i2++; j2--;};
-                return i1 >= j1 || i2 >= j2;
+                while (i1 < j1 && s[i1] == s[j1]) {
+                    i1++;
+                    j1--;
+                };
+                if (i1 >= j1) {
+                    return true;
+                }
+                while (i2 < j2 && s[i2] == s[j2]) {
+                    i2++;
+                    j2--;
+                };
+                if (i2 >= j2) {
+                    return true;
+                }
+                
+                return false;
             }
+        }
+        
         return true;
     }
 };
