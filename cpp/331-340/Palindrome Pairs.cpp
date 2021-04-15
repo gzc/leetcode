52b114760c4bb2257d577d905f93c26bc7298785
Zhenchao Gan <zhenchaogan@gmail.com>
2021-04-14 18:22:09 -0700

    336. Palindrome Pairs

--- cpp/331-340/Palindrome Pairs.cpp
+++ cpp/331-340/Palindrome Pairs.cpp
@@ -58,3 +58,103 @@ public:
         return ans;
     }
 };
+
+
+/*
+ * Trie Solution
+*/
+class Trie {
+    bool isPalindrome(const string& s) {
+        int left = 0;
+        int right = s.size() - 1;
+        while (left < right) {
+            if (s[left] == s[right]) {
+                left++;
+                right--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    struct TrieNode {
+        TrieNode *nodes[26];
+        bool isWord;
+        vector<int> indices;
+        int ind;
+        TrieNode(): isWord(false) {
+            memset(nodes, 0, sizeof(nodes));
+        }
+    };
+    
+    TrieNode* root_;
+public:
+    Trie() {
+        root_ = new TrieNode();
+    }
+    
+    void insert(string& s, int wordIndex) {
+        TrieNode* tmp = root_;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            int ind = s[i]-'a';
+            if (tmp->nodes[ind] == nullptr) {
+                tmp->nodes[ind] = new TrieNode();
+            }
+            if (isPalindrome(s.substr(i))) {
+                tmp->indices.push_back(wordIndex);
+            }
+            tmp = tmp->nodes[ind];
+        }
+        tmp->indices.push_back(wordIndex);
+        tmp->ind = wordIndex;
+        tmp->isWord = true;
+    }
+    
+    void findPalindrome(const vector<string>& words, int wordIndex, vector<vector<int>>& res) {
+        TrieNode* root = root_;
+        int m = words[wordIndex].size();
+        bool consume_whole_word = true;
+        for (int j = 0; j < m; j++) {
+            int ind = words[wordIndex][j] - 'a';
+            if (root->isWord && isPalindrome(words[wordIndex].substr(j))) {
+                if (root->ind != wordIndex) {
+                    res.push_back({wordIndex, root->ind});
+                }
+            }
+            root = root->nodes[ind];
+            if (root == nullptr) {
+                consume_whole_word = false;
+                break;
+            }
+        }
+        if (consume_whole_word) {
+            for (int index : root->indices) {
+                if (index != wordIndex) {
+                    res.push_back({wordIndex, index});
+                }
+            }
+        }
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> palindromePairs(vector<string>& words) {
+        int n = words.size();
+        vector<vector<int>> res;
+        Trie trie;
+        for (int i = 0; i < n; i++) {
+            string current = words[i];
+            reverse(current.begin(), current.end());
+            trie.insert(current, i);
+        }
+       
+        for (int i = 0; i < n; i++) {
+            trie.findPalindrome(words, i, res);
+        }
+        
+        return res;
+    }
+};
