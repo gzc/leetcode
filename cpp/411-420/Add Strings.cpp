8234a13b5d92e753538d5f8c605b2e654b5b4d97
Zhenchao Gan <zhenchaogan@gmail.com>
2021-04-18 02:18:19 -0700

    415. Add Strings

--- cpp/411-420/Add Strings.cpp
+++ cpp/411-420/Add Strings.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int length = max(num1.length(), num2.length());
-        string result(length, '0');
+        string result;
         int carry = 0;
-        int i1 = num1.length()-1;
-        int i2 = num2.length()-1;
-        
-        for (int i = length-1 ; i >= 0 ; i--) {
-            int v1 = i1 >= 0? num1.at(i1) - '0' : 0;
-            int v2 = i2 >= 0? num2.at(i2) - '0' : 0;
+        int i1 = num1.length() - 1;
+        int i2 = num2.length() - 1;
+        while (i1 >= 0 || i2 >= 0) {
+            int v1 = i1 >= 0 ? num1[i1] - '0' : 0;
+            int v2 = i2 >= 0 ? num2[i2] - '0' : 0;
             int sum = v1 + v2 + carry;
-            result[i] = sum%10 + '0';
-            carry = sum/10;
-            i1--; i2--;
+            result.push_back(sum % 10 + '0');
+            carry = sum / 10;
+            i1--;
+            i2--;
         }
-        if (carry)  result = '1' + result;
+        
+        if (carry) {
+            result.push_back('1');
+        }
+        reverse(begin(result), end(result));
         return result;
     }
 };
