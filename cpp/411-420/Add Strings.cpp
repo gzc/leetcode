52f6394099abc6406c2541eaa61fa08ffbc9501c
Zhenchao Gan <zhenchaogan@gmail.com>
2021-04-18 02:28:42 -0700

    415. Add Strings

--- cpp/411-420/Add Strings.cpp
+++ cpp/411-420/Add Strings.cpp
@@ -5,7 +5,7 @@ public:
         int carry = 0;
         int i1 = num1.length() - 1;
         int i2 = num2.length() - 1;
-        while (i1 >= 0 || i2 >= 0) {
+        while (i1 >= 0 || i2 >= 0 || carry > 0) {
             int v1 = i1 >= 0 ? num1[i1] - '0' : 0;
             int v2 = i2 >= 0 ? num2[i2] - '0' : 0;
             int sum = v1 + v2 + carry;
@@ -15,9 +15,6 @@ public:
             i2--;
         }
         
-        if (carry) {
-            result.push_back('1');
-        }
         reverse(begin(result), end(result));
         return result;
     }
