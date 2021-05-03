db635231fe289e800b09e6b8fdd01dec2a537de3
Zhenchao Gan <zhenchaogan@gmail.com>
2021-05-02 22:21:39 -0700

    1851. Minimum Interval to Include Each Query

--- cpp/1001-10000/1851-1860/Minimum Interval to Include Each Query.cpp
+++ cpp/1001-10000/1851-1860/Minimum Interval to Include Each Query.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
         int m = queries.size();
 
-        sort(intervals.begin(), intervals.end(), [](const vector<int>& lhs, const vector<int>& rhs){
+        sort(intervals.begin(), intervals.end(),[](const vector<int>& lhs, const vector<int>& rhs){
             return lhs[1] - lhs[0] < rhs[1] - rhs[0];
         });
         
@@ -14,13 +14,14 @@ public:
         
         vector<int> ans(m, -1);
         for (const vector<int>& interval : intervals) {
-            auto it1 = myset.lower_bound({interval[0], 0});
-            auto it2 = myset.upper_bound({interval[1], 99999999});
+            auto it = myset.lower_bound({interval[0], 0});
             
-            while (it1 != it2 && it1 != myset.end()) {
-                ans[it1->second] = interval[1] - interval[0] + 1;
-                it1 = myset.erase(it1);
+            while (it != myset.end() && it->first <= interval[1]) {
+                ans[it->second] = interval[1] - interval[0] + 1;
+                it = myset.erase(it);
             }
+            
+            if (myset.empty()) break;
         }
         
         return ans;
