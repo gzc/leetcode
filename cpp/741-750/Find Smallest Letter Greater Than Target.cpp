6facb742c491e4b0c9f78ddfecff7df0f86d4298
Zhenchao Gan <zhenchaogan@gmail.com>
2021-09-16 18:03:54 -0700

    744. Find Smallest Letter Greater Than Target

--- cpp/741-750/Find Smallest Letter Greater Than Target.cpp
+++ cpp/741-750/Find Smallest Letter Greater Than Target.cpp
@@ -6,3 +6,28 @@ public:
         return *it;
     }
 };
+
+
+
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        if (target >= letters.back()) {
+            return letters.front();
+        }
+        int i = 0;
+        int j = letters.size() - 1;
+        
+        while (i < j) {
+            int mid = (i + j) / 2;
+            if (target < letters[mid]) {
+                j = mid;
+            } else if (target >= letters[mid]) {
+                i = mid + 1;
+            }
+        }
+        
+        return letters[i];
+    }
+};
