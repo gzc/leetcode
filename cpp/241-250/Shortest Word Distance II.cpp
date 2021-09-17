5f15f46f27499a0d71980dd8ecbe2c75c1747caf
Zhenchao Gan <zhenchaogan@gmail.com>
2021-09-16 21:11:27 -0700

    244. Shortest Word Distance II

--- cpp/241-250/Shortest Word Distance II.cpp
+++ cpp/241-250/Shortest Word Distance II.cpp
@@ -4,30 +4,29 @@ class WordDistance {
     
 public:
     WordDistance(vector<string>& words) {
-        for(int i = 0;i < words.size();i++)
+        for (int i = 0; i < words.size(); i++) {
             m[words[i]].push_back(i);
+        }
     }
     
     int shortest(string word1, string word2) {
-        vector<int> v1 = m[word1];
-        vector<int> v2 = m[word2];
-        int i(0),j(0),res(INT_MAX);
-        while(i < v1.size() && j < v2.size())
-        {
+        const vector<int>& v1 = m[word1];
+        const vector<int>& v2 = m[word2];
+        int i = 0;
+        int j = 0;
+        int res = INT_MAX;
+        
+        while (i < v1.size() && j < v2.size()) {
             int i1 = v1[i];
             int i2 = v2[j];
             res = min(abs(i1-i2), res);
-            if(i1 > i2)
+            if(i1 > i2) {
                 j++;
-            else
+            } else {
                 i++;
+            }
         }
+
         return res;
     }
 };
-
-
-// Your WordDistance object will be instantiated and called as such:
-// WordDistance wordDistance(words);
-// wordDistance.shortest("word1", "word2");
-// wordDistance.shortest("anotherWord1", "anotherWord2");
