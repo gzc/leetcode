f9c1add79ffe7c3cded327ddff0ee2898e4e8909
Zhenchao Gan <zhenchaogan@gmail.com>
2021-09-22 17:47:25 -0700

    149. Max Points on a Line

--- cpp/141-150/Max Points on a Line.cpp
+++ cpp/141-150/Max Points on a Line.cpp
@@ -1,48 +1,31 @@
-/**
- * Definition for a point.
- * struct Point {
- *     int x;
- *     int y;
- *     Point() : x(0), y(0) {}
- *     Point(int a, int b) : x(a), y(b) {}
- * };
- */
-int gcd(int a, int b)
-{
-    if(a == 0) return b;
-    return gcd(b%a, a);
-}
-
 class Solution {
 public:
-    int maxPoints(vector<Point> &points) {
-        if(points.size() <= 2)
+    int maxPoints(vector<vector<int>> &points) {
+        if (points.size() <= 2) {
             return points.size();
+        }
         
         int maxNum = 0;
         
-        for(int i = 0;i < points.size();i++)
-        {
-            int localmax(0),overlap(0);
-            map<pair<int, int>,int> statistic;
-            for(int j = i+1;j < points.size();j++)
-            {
-                //if(j == i) continue;
-                if(points[j].x==points[i].x && points[j].y==points[i].y) {
+        for (int i = 0; i < points.size(); i++) {
+            int localmax = 0, overlap = 0;
+            map<pair<int, int>, int> statistic;
+            for (int j = i + 1; j < points.size(); j++) {
+                if (points[j][0] == points[i][0] && points[j][1] == points[i][1]) {
                     overlap++;
                     continue;
                 } else {
-                    int x = points[j].x - points[i].x;
-                    int y = points[j].y - points[i].y;
-                    int divide = gcd(x,y);
+                    int x = points[j][0] - points[i][0];
+                    int y = points[j][1] - points[i][1];
+                    int divide = __gcd(x,y);
                     x /= divide;
                     y /= divide;
                     pair<int, int>p(x,y);
-                    statistic[p] += 1;
-                    localmax=max(statistic[p], localmax);
+                    statistic[p]++;
+                    localmax = max(statistic[p], localmax);
                 }
             }
-            maxNum=max(maxNum, localmax+overlap+1);
+            maxNum=max(maxNum, localmax + overlap + 1);
         }
         
         return maxNum;
