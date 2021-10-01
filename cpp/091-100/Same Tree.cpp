918472a349f79c2ae1c082c698f40e942a701867
Zhenchao Gan <zhenchaogan@gmail.com>
2021-10-01 16:21:00 -0700

    15. Same Tree

--- cpp/091-100/Same Tree.cpp
+++ cpp/091-100/Same Tree.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if (p ＝＝ nullptr && q == nullptr) return true;
+        if (p == nullptr && q == nullptr) return true;
         if ( (p && !q) || (q && !p) ) return false;
         if (p->val != q->val) return false;
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
