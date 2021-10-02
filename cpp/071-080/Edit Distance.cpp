42fb22dcf6b15953d87e06e988b0efe3e83303b4
Zhenchao Gan <zhenchaogan@gmail.com>
2021-10-01 21:49:59 -0700

    72. Edit Distance

--- cpp/071-080/Edit Distance.cpp
+++ cpp/071-080/Edit Distance.cpp
@@ -4,21 +4,22 @@ public:
         int m = word1.length();
         int n = word2.length();
         
-        vector<vector<int> > p(m+1, vector<int>(n+1));
+        vector<vector<int>> p(m+1, vector<int>(n+1));
 
         for (int i = 0;i <= m;i++)
             p[i][0] = i;
         for (int j = 0;j <= n;j++)
             p[0][j] = j;
         
-        for (int i = 1;i <= m;i++)
-        {
-            for (int j = 1;j <= n;j++)
-            {
-                int f(0);
-                if (word1[i-1] != word2[j-1])
-                    f = 1;
-                p[i][j] = min(p[i-1][j-1]+f, 1+min(p[i][j-1], p[i-1][j]));
+        for (int i = 1;i <= m;i++) {
+            for (int j = 1;j <= n;j++) {
+                
+                if (word1[i-1] == word2[j-1]) {
+                    p[i][j] = p[i-1][j-1];
+                } else {
+                    p[i][j] = min(1 + p[i-1][j-1], 1+min(p[i][j-1], p[i-1][j]));
+                }
+                
             }
         }
         
