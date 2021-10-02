8304ae34912ece881c712618c74c8b704d959517
Zhenchao Gan <zhenchaogan@gmail.com>
2021-10-02 13:02:37 -0700

    69. Sqrt(x)

--- cpp/061-070/Sqrt.cpp
+++ cpp/061-070/Sqrt.cpp
@@ -16,3 +16,27 @@ public:
         return -1;
     }
 };
+
+
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0) return 0;
+        int left = 1, right = x/2;
+        
+        while (left < right) {
+            int mid = left + (right - left)/2;
+            
+            if ((long)mid * (long)mid <= (long)x && (long)(mid+1)*(long)(mid+1) > (long)x) {
+                return mid;
+            } else if ((long)mid * (long)mid > (long)x) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+};
