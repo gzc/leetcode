119144a94ad68604c9514223e2ad4161bbc9e2a1
Zhenchao Gan <zhenchaogan@gmail.com>
2021-10-03 17:20:56 -0700

    159. Longest Substring with At Most Two Distinct Characters

--- cpp/151-160/Longest Substring with At Most Two Distinct Characters.cpp
+++ cpp/151-160/Longest Substring with At Most Two Distinct Characters.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     int lengthOfLongestSubstringTwoDistinct(string s) {
-        int current(0);
-        int longest(0);
-        int previouspos(0);
-        map<char, int>m;
+        int current = 0;
+        int longest = 0;
+        int previouspos = 0;
+        unordered_map<char, int> m;
         
-        for (int i = 0;i < s.length();i++)
-        {
-            if (current < 2 && m[s[i]] == 0) {
-                m[s[i]] = 1;
+        for (int i = 0; i < s.length(); i++) {
+            m[s[i]]++;
+            if (m[s[i]] == 1) {
                 current++;
-            } else if (m[s[i]] > 0) {
-                m[s[i]]++;
-            } else {
-                longest = max(longest, i-previouspos);
-                m[s[i]] = 1;
-                while (true) {
-                    char ch = s[previouspos];
-                    previouspos++;
-                    m[ch]--;
-                    if(m[ch] == 0) break;
+            }
+
+            while (current > 2) {
+                char ch = s[previouspos];
+                previouspos++;
+                m[ch]--;
+                if (m[ch] == 0) {
+                    current--;
                 }
             }
+            
+            longest = max(longest, i - previouspos + 1);
         }
-        longest = max(longest, (int)s.length()-previouspos);
+        
         return longest;
     }
 };
