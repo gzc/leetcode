a8fcf66fc19e75d64262b1062c8163f023fe2fbd
Zhenchao Gan <zhenchaogan@gmail.com>
2021-12-09 16:23:03 -0800

    1712. Ways to Split Array Into Three Subarrays

--- cpp/1001-10000/1711-1720/Ways to Split Array Into Three Subarrays.cpp
+++ cpp/1001-10000/1711-1720/Ways to Split Array Into Three Subarrays.cpp
@@ -12,6 +12,50 @@ public:
         long ans = 0;
         long mod = 1e9+7;
         int sum_left = 0;
+        int j = 0;
+        int k = 0;
+        for (int i = 0; i < n; i++) {
+            sum_left += nums[i];
+            
+            if (sum_left*3 > sum) break;
+            
+            j = max(j, i+1);
+            while (j < n-1 && ((sums[j] - sums[i]) < sum_left)) j++;
+            
+            int sum_remain = sum - sum_left;
+            k = max(k, j);
+            while (k < n-1) {
+                int l = sums[k] - sums[i];
+                int r = sum_remain - l;
+                if (l > r) {
+                    break;
+                }
+                else k++;
+            }
+            
+            ans += (k - j);
+            
+        }
+        
+        return ans % mod;
+    }
+};
+
+
+class Solution {
+public:
+    int waysToSplit(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> sums;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+            sums.push_back(sum);
+        }
+        
+        long ans = 0;
+        long mod = 1e9+7;
+        int sum_left = 0;
         for (int left = 0; left < n; left++) {
             sum_left += nums[left];
             
