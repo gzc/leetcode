0111718991020875f0dfaca4f74417242e20adc9
Zhenchao Gan <zhenchaogan@gmail.com>
2022-01-02 12:39:00 -0800

    759. Employee Free Time

--- cpp/751-760/Employee Free Time.cpp
+++ cpp/751-760/Employee Free Time.cpp
@@ -1,38 +1,46 @@
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
+/*
+// Definition for an Interval.
+class Interval {
+public:
+    int start;
+    int end;
+
+    Interval() {}
+
+    Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+};
+*/
+
 class Solution {
 public:
-    vector<Interval> employeeFreeTime(vector<vector<Interval>>& schedule) {
-        vector<Interval> result;
-        multiset<pair<int, bool>> myset;
-        for (const vector<Interval>& intervals : schedule) {
-            for (const Interval& interval : intervals) {
-                myset.insert({interval.start, true});
-                myset.insert({interval.end, false});
+    vector<Interval> employeeFreeTime(vector<vector<Interval>> schedule) {
+        map<int, int> mymap;
+        for (const vector<Interval>& s : schedule) {
+            for (const Interval& i : s) {
+                mymap[i.start]++;
+                mymap[i.end]--;
             }
         }
         
-        int count = 0;
-        int lastTime = -1;
-        for (const pair<int, bool>& ele : myset) {
-            if (ele.second) {
-                if (lastTime != -1 && count == 0 && ele.first > lastTime) {
-                    result.push_back(Interval(lastTime, ele.first));
-                }
-                count++;
-            } else {
-                count--;
+        int last = -1;
+        int cnt = 0;
+        vector<Interval> ans;
+        
+        for (const auto& pair : mymap) {
+            int pcnt = cnt;
+            cnt += pair.second;
+            
+            if (pcnt == 0 && last != -1) {
+                Interval in(last, pair.first);
+                ans.push_back(in);
+            } else if (cnt == 0) {
+                last = pair.first;
             }
-            lastTime = ele.first;
         }
         
-        return result;
+        return ans;
     }
 };
